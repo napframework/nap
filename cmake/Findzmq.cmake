df57543a63ab8cede71fb3e13b8ad92c5a8fa3e9
B Korsmit <bmodone@gmail.com>
2016-11-09 17:42:39 +0100

    Adding findZMQ cmake

---
+++ cmake/Findzmq.cmake
@@ -0,0 +1,23 @@
+# - Try to find ZMQ
+# Once done this will define
+# ZMQ_FOUND - System has ZMQ
+# ZMQ_INCLUDE_DIRS - The ZMQ include directories
+# ZMQ_LIBRARIES - The libraries needed to use ZMQ
+# ZMQ_DEFINITIONS - Compiler switches required for using ZMQ
+
+find_path(ZMQ_DIR include/zmq.h HINTS ${CMAKE_CURRENT_LIST_DIR}/../../libzmq)
+
+if(MSVC)
+    find_library(ZMQ_LIBRARIES NAMES libzmq.lib HINTS ${ZMQ_DIR}/bin/Win32/Release/v140/dynamic)
+else()
+    find_library(ZMQ_LIBRARIES NAMES libzmq.dll HINTS ${ZMQ_DIR}/bin/${CMAKE_BUILD_TYPE} ${ZMQ_DIR}/bin/Debug)
+endif()
+
+set(ZMQ_INCLUDE_DIR ${ZMQ_DIR}/include)
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set ZMQ_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(ZMQ DEFAULT_MSG ZMQ_LIBRARIES ZMQ_INCLUDE_DIR)
+
+
