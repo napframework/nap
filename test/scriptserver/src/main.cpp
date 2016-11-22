c5e552731357c00ad3b1b6a93c10a73f253008da
B Korsmit <bmodone@gmail.com>
2016-11-22 17:16:34 +0100

    Updating test

--- test/scriptserver/src/main.cpp
+++ test/scriptserver/src/main.cpp
@@ -1,8 +1,7 @@
-#include <asynctcpserver.h>
 #include <nap.h>
 #include <stdlib.h>
 #include <thread>
-#include <unistd.h>
+//#include <unistd.h>
 
 using namespace nap;
 
@@ -11,6 +10,7 @@ using namespace nap;
 void runScriptServerComponent()
 {
 	Logger::setLevel(Logger::debugLevel());
+	Logger::info("Starting script server");
 
 	Core core;
 	core.initialize();
@@ -20,7 +20,13 @@ void runScriptServerComponent()
 	Entity& secondChild = root.addEntity("SecondChild");
     Entity& secondChildChild = secondChild.addEntity("SecondChildChild");
 
-	RTTI::TypeInfo serverCompType = RTTI::TypeInfo::getByName("nap::JSONRPCServerComponent");
+	std::string serverTypename = "nap::JSONRPCServerComponent";
+	RTTI::TypeInfo serverCompType = RTTI::TypeInfo::getByName(serverTypename);
+	if (!serverCompType.isValid())
+	{
+		Logger::fatal("Type not found: %s", serverTypename.c_str());
+		return;
+	}
 	auto& server = root.addComponent(serverCompType);
 
 	server.getAttribute<bool>("running")->setValue(true);
@@ -32,28 +38,28 @@ void runScriptServerComponent()
 //		Logger::debug("Changing name of root to %s", newName.c_str());
         std::lock_guard<std::mutex> lock(mutex);
         root.setName(newName);
-		sleep(4);
+		std::this_thread::sleep_for(std::chrono::seconds(4));
 	}
 }
 
-void testAsyncTCPServer()
-{
-
-	nap::AsyncTCPServer server(8888);
-
-	server.clientConnected.connect(
-		[&](AsyncTCPClient& client) { std::cout << "Connected: " << client.getIdent() << std::endl; });
-
-	server.clientDisconnected.connect(
-		[&](AsyncTCPClient& client) { std::cout << "Disconnected: " << client.getIdent() << std::endl; });
-
-	server.requestReceived.connect(
-		[&](AsyncTCPClient& client, const std::string& msg) { client.enqueueEvent("Replying to " + msg); });
-
-	while (true) {
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-	}
-}
+//void testAsyncTCPServer()
+//{
+//
+//	nap::AsyncTCPServer server(8888);
+//
+//	server.clientConnected.connect(
+//		[&](AsyncTCPClient& client) { std::cout << "Connected: " << client.getIdent() << std::endl; });
+//
+//	server.clientDisconnected.connect(
+//		[&](AsyncTCPClient& client) { std::cout << "Disconnected: " << client.getIdent() << std::endl; });
+//
+//	server.requestReceived.connect(
+//		[&](AsyncTCPClient& client, const std::string& msg) { client.enqueueEvent("Replying to " + msg); });
+//
+//	while (true) {
+//		std::this_thread::sleep_for(std::chrono::seconds(1));
+//	}
+//}
 
 int main(int argc, char* argv[])
 {
