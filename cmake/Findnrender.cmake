b135efa0ac0aed59146fd1fab9611811d5a2d8cf
Coen Klosters <coen.klosters@gmail.com>
2016-12-02 12:40:02 +0100

    adding nrender module

---
+++ cmake/Findnrender.cmake
@@ -0,0 +1,28 @@
+ find_path(
+        NRENDER_INCLUDE_DIRS
+        NAMES nopengl.h
+        HINTS
+        ${CMAKE_CURRENT_LIST_DIR}/../../nrender/src
+)
+
+mark_as_advanced(NRENDER_INCLUDE_DIRS)
+
+if(NRENDER_INCLUDE_DIRS)
+    set(NRENDER_FOUND TRUE)
+endif()
+
+mark_as_advanced(NRENDER_FOUND)
+mark_as_advanced(NRENDER_CXX_FLAGS)
+
+if(NRENDER_FOUND)
+    if(NOT NRENDER_FIND_QUIETLY)
+        message(STATUS "Found nrender header files in ${NRENDER_INCLUDE_DIRS}")
+        if(DEFINED NRENDER_CXX_FLAGS)
+            message(STATUS "Found nrender C++ extra compilation flags: ${NRENDER_CXX_FLAGS}")
+        endif()
+    endif()
+elseif(NRENDER_FIND_REQUIRED)
+    message(FATAL_ERROR "Could not find nrender, consider adding nrender path to CMAKE_PREFIX_PATH")
+else()
+    message(STATUS "Optional package nrender was not found")
+endif()
