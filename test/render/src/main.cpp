5b86ff1fa457b0d4f89e94912b4f618d08822365
Coen Klosters <coen.klosters@gmail.com>
2016-12-05 16:02:39 +0100

    updating render cmake

--- test/render/src/main.cpp
+++ test/render/src/main.cpp
@@ -1,5 +1,9 @@
+#include <nopengl.h>
 
-
-int main(int argc, char** argv) {
+int main(int argc, char** argv) 
+{
     return 0;
+
+	opengl::WindowSettings settings;
+	opengl::createWindow(settings);
 }
