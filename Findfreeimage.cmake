7631ff7545907ebc68bec63725fa9ca01b58be46
bmod <bmodone@gmail.com>
2016-12-07 14:24:52 +0100

    Fixing up libraries for nrender

--- Findfreeimage.cmake
+++ Findfreeimage.cmake
@@ -6,8 +6,10 @@ find_path(
         ${CMAKE_CURRENT_LIST_DIR}/../thirdparty/FreeImage
 )
 
-set(FREEIMAGE_INCLUDE_DIRS ${FREEIMAGE_DIR}/Dist/x64)
-set(FREEIMAGE_LIBRARIES ${FREEIMAGE_DIR}/Dist/x64/FreeImage.lib)
+if (MSVC)
+    set(FREEIMAGE_INCLUDE_DIRS ${FREEIMAGE_DIR}/msvc/Dist/x64)
+    set(FREEIMAGE_LIBRARIES ${FREEIMAGE_DIR}/msvc/Dist/x64/FreeImage.lib)
+endif()
 
 
 
