192f6c1cb22abe04afa2ac1a93c43ddcd22d14d0
bmod <bmodone@gmail.com>
2016-12-08 14:23:29 +0100

    Fixing up Findassimp.cmake path

--- Findassimp.cmake
+++ Findassimp.cmake
@@ -1,17 +1,24 @@
-find_path(
-        ASSIMP_DIR
-        NAMES include/assimp/scene.h
-        HINTS
-        ${CMAKE_CURRENT_LIST_DIR}/../assimp
-        ${CMAKE_CURRENT_LIST_DIR}/../thirdparty/assimp/msvc
-)
+
+
 
 if (MSVC)
-	set(ASSIMP_INCLUDE_DIRS ${ASSIMP_DIR}/msvc/include)
-	set(ASSIMP_LIBRARIES ${ASSIMP_DIR}/msvc/lib64/assimp.lib)
+	find_path(
+	        ASSIMP_DIR
+	        NAMES include/assimp/scene.h
+	        HINTS
+	        ${CMAKE_CURRENT_LIST_DIR}/../thirdparty/assimp/msvc
+	)
+	set(ASSIMP_INCLUDE_DIRS ${ASSIMP_DIR}/include)
+	set(ASSIMP_LIBRARIES ${ASSIMP_DIR}/lib64/assimp.lib)
 elseif (APPLE)
-	set(ASSIMP_INCLUDE_DIRS ${ASSIMP_DIR}/osx/include)
-	set(ASSIMP_LIBRARIES ${ASSIMP_DIR}/osx/lib/assimp.a)
+	find_path(
+	        ASSIMP_DIR
+	        NAMES include/assimp/scene.h
+	        HINTS
+	        ${CMAKE_CURRENT_LIST_DIR}/../thirdparty/assimp/osx
+	)
+	set(ASSIMP_INCLUDE_DIRS ${ASSIMP_DIR}/include)
+	set(ASSIMP_LIBRARIES ${ASSIMP_DIR}/lib/assimp.a)
 endif()
 
 mark_as_advanced(GLM_INCLUDE_DIRS)
