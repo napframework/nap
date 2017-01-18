f9fd7af8a02875c594c501a9642ff28d37845313
Coen Klosters <coen@naivi.nl>
2017-01-18 10:59:34 +0100

    - started refactor windowcomponent

--- modules/naprender/src/renderwindowcomponent.cpp
+++ modules/naprender/src/renderwindowcomponent.cpp
@@ -1,5 +1,51 @@
 #include "renderwindowcomponent.h"
 
+namespace nap
+{
+	// Constructor
+	RenderWindowComponent::RenderWindowComponent()
+	{
+		// When added request a window
+		added.connect(componentAdded);
+
+		// When removed destroy a window
+		removed.connect(componentRemoved);
+
+		// When visibility changes, hide / show
+		visible.valueChangedSignal.connect(visibilityChanged);
+
+		// Enable edit-ability
+		visible.setFlag(Editable, true);
+	}
+
+
+	void RenderWindowComponent::createWindow()
+	{
+		auto service = getRoot()->getCore().getOrCreateService<RenderService>();
+		mWindow = service->getWindow();
+	}
+
+
+	void RenderWindowComponent::destroyWindow()
+	{
+		auto service = getRoot()->getCore().getOrCreateService<RenderService>();
+		service->destroyWindow(mWindow);
+	}
+
+
+	// Hide / Show window
+	void RenderWindowComponent::onVisibilityChanged(const bool& value)
+	{
+		if (value)
+		{
+			showWindow();
+			return;
+		}
+		hideWindow();
+	}
+
+}
+
 RTTI_DEFINE(nap::RenderWindowComponent)
 
 
--- modules/naprender/src/renderwindowcomponent.h
+++ modules/naprender/src/renderwindowcomponent.h
@@ -1,52 +1,50 @@
 #pragma once
 
-#include <nap.h>
-
+// Local Includes
 #include "renderservice.h"
+
+// External Includes
+#include <nap.h>
 
 namespace nap
 {
+	/**
+	 * 3D render window. Creating a window requests a new 3d window
+	 * with context from the Render Service. Every window is associated
+	 * with it's own drawing context. Note that this window does not own
+	 * the OpenGL render window, it acts as an interface to the window managed 
+	 * by the service
+	 */
 	class RenderWindowComponent : public ServiceableComponent
 	{
 		RTTI_ENABLE_DERIVED_FROM(ServiceableComponent)
-
 	public:
-		RenderWindowComponent()
-		{
+		/**
+		 * Constructor
+		 */
+		RenderWindowComponent();
 
-			added.connect([=](Object& parent) { createWindow(); });
+		void createWindow();
 
-			removed.connect([=](Object& parent) { destroyWindow(); });
-
-			visible.valueChanged.connect([=](AttributeBase& attrib) {
-				if (rtti_cast<Attribute<bool>*>(&attrib)->getValue())
-					showWindow();
-				else
-					hideWindow();
-			});
-
-			visible.setFlag(Editable, true);
-			int flags = visible.getFlags();
-			Logger::debug("%d", flags);
-		}
-
-		void createWindow()
-		{
-			auto service = getRoot()->getCore().getOrCreateService<RenderService>();
-			mWindow = service->getWindow();
-		}
-
-		void destroyWindow()
-		{
-			auto service = getRoot()->getCore().getOrCreateService<RenderService>();
-			service->destroyWindow(mWindow);
-		}
+		void destroyWindow();
 
 		void hideWindow() { destroyWindow(); }
 
 		void showWindow() { createWindow(); }
 
 		Attribute<bool> visible = {this, "visible", true};
+
+	protected:
+		// Callable Slots
+		void onAdded(Object& parent)					{ createWindow(); }
+		void onRemoved(Object& parent)					{ destroyWindow(); }
+		void onVisibilityChanged(const bool& value);
+
+		// Slot declarations
+		NSLOT(componentAdded,	Object&, onAdded)
+		NSLOT(componentRemoved,	Object&, onRemoved)
+		NSLOT(visibilityChanged, const bool&, onVisibilityChanged)
+
 
 	private:
 		SDL_Window* mWindow = nullptr;
