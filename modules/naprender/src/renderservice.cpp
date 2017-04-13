20567beec7d2d9dd216d1e60879c9e75e5c1f345
Ritesh Oedayrajsingh Varma <rovarma@gmail.com>
2017-04-13 11:49:04 +0200

    - Split off RenderState to its own file

--- modules/naprender/src/renderservice.cpp
+++ modules/naprender/src/renderservice.cpp
@@ -14,49 +14,6 @@
 
 namespace nap
 {
-	// Forces the setting of all render states as currently set.
-	void RenderState::force()
-	{
-		opengl::enableDepthTest(mEnableDepthTest);
-		opengl::enableBlending(mEnableBlending);
-		opengl::enableMultiSampling(mEnableMultiSampling);
-		opengl::setLineWidth(mLineWidth);
-		opengl::setPointSize(mPointSize);
-		opengl::setPolygonMode(mPolygonMode);
-	}
-
-	// Switches all render states as set in @targetRenderState. Only the renderStates that are different
-	// will actually cause openGL calls.
-	void RenderState::update(const RenderState& targetRenderState)
-	{
-		if (targetRenderState.mEnableDepthTest != mEnableDepthTest)
-		{
-			opengl::enableDepthTest(targetRenderState.mEnableDepthTest);
-		}
-		if (targetRenderState.mEnableBlending != mEnableBlending)
-		{
-			opengl::enableBlending(targetRenderState.mEnableBlending);
-		}
-		if (targetRenderState.mEnableMultiSampling != mEnableMultiSampling)
-		{
-			opengl::enableMultiSampling(targetRenderState.mEnableMultiSampling);
-		}
-		if (targetRenderState.mLineWidth != mLineWidth)
-		{
-			opengl::setLineWidth(targetRenderState.mLineWidth);
-		}
-		if (targetRenderState.mPointSize != mPointSize)
-		{
-			opengl::setPointSize(targetRenderState.mPointSize);
-		}
-		if (targetRenderState.mPolygonMode != mPolygonMode)
-		{
-			opengl::setPolygonMode(targetRenderState.mPolygonMode);
-		}
-		*this = targetRenderState;
-	}
-
-
 	// Register all types
 	void RenderService::registerTypes(nap::Core& core)
 	{
---
+++ modules/naprender/src/renderstate.h
@@ -0,0 +1,43 @@
+#pragma once
+
+// External Includes
+#include "nglutils.h"
+
+namespace nap
+{
+	/**
+	 * Global render state object. Users can freely change values in this object.
+	 *
+	 * Purpose of this object is to:
+	 * 1) Provide an OpenGL independent way of setting render state
+	 * 2) Provide a state object that can be duplicated for multiple GL contexts
+	 * 3) Minimize openGL state changes
+	 *
+	 * RenderService will maintain a global render state, as well as render states per GL context.
+	 * Whenever objects are rendered, the RenderService will diff the global state against the
+	 * context's state and update only the GL states that are necessary.
+	 */
+	struct RenderState
+	{
+		bool				mEnableDepthTest = true;
+		bool				mEnableBlending = true;
+		bool				mEnableMultiSampling = true;
+		float				mLineWidth = 1.0f;
+		float				mPointSize = 1.0;
+		opengl::PolygonMode mPolygonMode = opengl::PolygonMode::FILL;
+
+	private:
+		friend class RenderService;
+
+		/**
+		* Forces the setting of all render states as currently set.
+		*/
+		void force();
+
+		/**
+		* Switches all render states as set in @targetRenderState. Only the renderStates that are different
+		will actually cause openGL calls.
+		*/
+		void update(const RenderState& targetRenderState);
+	};
+}
