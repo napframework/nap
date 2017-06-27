25ea90f80d3503b271b43f3c9b1fa911f08cd8a0
Ritesh Oedayrajsingh Varma <rovarma@gmail.com>
2017-06-27 11:44:42 +0200

    - Commenting & cleanup

--- modules/naprender/src/spheremeshresource.h
+++ modules/naprender/src/spheremeshresource.h
@@ -12,11 +12,14 @@ namespace nap
 		RTTI_ENABLE(MeshResource)
 
 	public:
+		/**
+ 		 * Load the mesh
+ 		 */
 		virtual bool init(utility::ErrorState& errorState) override;
 
 	public:
-		float mRadius = 1.0f;
-		float mRings = 50.0f;
-		float mSectors = 50.0f;
+		float mRadius	= 1.0f;		// The radius of the mesh
+		float mRings	= 50.0f;	// The number of rings in the mesh
+		float mSectors	= 50.0f;	// The number of sectors in the mesh
 	};
 }
