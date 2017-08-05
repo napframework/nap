52c7b6817752de93c32bff30b85b83befd8ab9d6
bmod <bmodone@gmail.com>
2017-08-05 13:44:49 -0700

    dicing directory

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 working_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-"$working_dir" git config credential.helper cache
+git config credential.helper cache
 git pull https://github.com/naivisoftware/nap.git
-python "$working_dir"/build.py
+python build.py
