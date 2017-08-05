03645406be806d94c5f0d044a23053db334691f0
bmod <bmodone@gmail.com>
2017-08-05 12:05:06 -0700

    Adding git fetch command

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 from multiprocessing import cpu_count
+from sys import platform
 
 WORKING_DIR = os.path.dirname(__file__)
 
@@ -9,16 +10,16 @@ THIRDPARTY_DIR = '%s/thirdparty' % WORKING_DIR
 THIRDPARTY_URL = 'https://ae53bb936bc44bbffbac2dbd1f37101838603903@github.com/naivisoftware/thirdparty.git'
 
 
-
 def isLocalGitRepo(d):
     if not os.path.exists(d): return False
     try:
         call(d, ['git', 'rev-parse'])
-    except: return False
+    except:
+        return False
     return True
 
 
-def call(cwd=None, cmd=None):
+def call(cwd=WORKING_DIR, cmd=None):
     print('dir: %s' % cwd)
     print('cmd: %s' % cmd)
     proc = subprocess.Popen(cmd, cwd=cwd)
@@ -27,7 +28,41 @@ def call(cwd=None, cmd=None):
         raise Exception(proc.returncode)
 
 
-if __name__ == '__main__':
+def installDependenciesLinux():
+    d = WORKING_DIR
+    call(d, ['sudo', 'apt-get', 'install',
+             'cmake',
+             'clang-3.8',
+             ])
+
+
+def installDependenciesOSX():
+    d = WORKING_DIR
+    # https://stackoverflow.com/questions/25535407/bypassing-prompt-to-press-return-in-homebrew-install-script
+    # ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" </dev/null
+    call(d, ['ruby', '-e', '"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'])
+
+    call(d, ['brew', 'install',
+             'cmake'
+             ])
+
+
+def installDependencies():
+    print('Installing dependencies')
+    if platform == "linux" or platform == "linux2":
+        installDependenciesLinux()
+    elif platform == "darwin":
+        installDependenciesOSX()
+    elif platform == "win32":
+        # Windows...
+        pass
+
+
+def main():
+    # installDependencies()
+    print('Updating repo')
+    d = WORKING_DIR
+    call(d, ['git', 'fetch', '--all'])
 
     print('Refreshing: %s' % THIRDPARTY)
     d = THIRDPARTY_DIR
@@ -46,3 +81,7 @@ if __name__ == '__main__':
     d = WORKING_DIR
     call(d, ['cmake', '.'])
     call(d, ['make', 'napcore', '-j%s' % cpu_count()])
+
+
+if __name__ == '__main__':
+    main()
