9fad3ace3e8c7311fd34e0d29786d216cdd1c7e9
Coen Klosters <coen@naivi.nl>
2017-08-06 12:02:54 +0200

    Removing build of x86_32 windows build

--- generateVSSolution.bat
+++ generateVSSolution.bat
@@ -1,5 +1,3 @@
 @echo off
 mkdir msvc64
-mkdir msvc32
-cmake -H. -Bmsvc64  -G "Visual Studio 14 2015 Win64" -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE
-cmake -H. -Bmsvc32  -G "Visual Studio 14 2015" -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE
+cmake -H. -Bmsvc64  -G "Visual Studio 14 2015 Win64"
