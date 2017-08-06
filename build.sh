dc0a208babdae1bb85e34486e2b9f3dacf614abb
bmod <bmodone@gmail.com>
2017-08-05 17:59:07 -0700

    removing a git update

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 working_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-git config credential.helper cache
-git pull https://github.com/naivisoftware/nap.git
 python build.py
