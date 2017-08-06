69dfd46f4dc76fa3cde39ed5d07e907fc3620fed
Coen Klosters <coen@naivi.nl>
2017-08-06 12:08:41 +0200

    fixing obj / def file generation for windows libraries

--- generateVSSolution.bat
+++ generateVSSolution.bat
@@ -1,3 +1,4 @@
 @echo off
 mkdir msvc64
-cmake -H. -Bmsvc64  -G "Visual Studio 14 2015 Win64"
+mkdir msvc32
+cmake -H. -Bmsvc64  -G "Visual Studio 14 2015 Win64" -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE
