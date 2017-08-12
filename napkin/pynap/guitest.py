6448485d2dac0154455992f576569b6491889415
Bas Korsmit <bmodone@gmail.com>
2017-08-12 11:16:50 -0700

    Switch between up and downstream hierarchies

--- napkin/pynap/guitest.py
+++ napkin/pynap/guitest.py
@@ -100,26 +100,37 @@ class TypeHierarchyPanel(QWidget):
         self.__downstreamModel = TypeByRootModel()
         self.__upstreamModel = TypeByLeafModel()
 
-        self.__radioGrp = QHBoxLayout()
-        self.__radioGrp.setContentsMargins(0, 0, 0, 0)
-        self.layout().addChildLayout(self.__radioGrp)
+        self.__modelRadioButtons = QWidget()
+        self.__modelRadioButtons.setLayout(QHBoxLayout())
+        self.__modelRadioButtons.layout().setContentsMargins(0, 0, 0, 0)
+        self.__modelRadioButtons.layout().addStretch(1)
+        self.layout().addWidget(self.__modelRadioButtons)
 
+        self.__modelRadioGrp = QButtonGroup()
+        self.__modelRadioGrp.buttonToggled.connect(self.__refresh)
         self.__rbDownstream = QRadioButton('Downstream')
         self.__rbDownstream.setChecked(True)
-        self.__radioGrp.addWidget(self.__rbDownstream)
+        self.__modelRadioGrp.addButton(self.__rbDownstream)
+        self.__modelRadioButtons.layout().addWidget(self.__rbDownstream)
+
         self.__rbUpstream = QRadioButton('Upstream')
-        self.__radioGrp.addWidget(self.__rbUpstream)
+        self.__modelRadioGrp.addButton(self.__rbUpstream)
+        self.__modelRadioButtons.layout().addWidget(self.__rbUpstream)
+
 
         self.__treeView = QTreeView()
         self.__treeView.setHeaderHidden(True)
         self.layout().addWidget(self.__treeView)
+
         self.__refresh()
 
     def __refresh(self):
         if self.__rbDownstream.isChecked():
             self.__treeView.setModel(self.__downstreamModel)
+            self.__treeView.expandAll()
         elif self.__rbUpstream.isChecked():
             self.__treeView.setModel(self.__upstreamModel)
+
 
 
 if __name__ == '__main__':
