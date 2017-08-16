b10bafc691e3950b922f0d14ed77e064bf3d4b8b
Bas Korsmit <bmodone@gmail.com>
2017-08-16 00:46:25 -0700

    Adding fuzzy string matching utility

--- napkin/generic/butils.py
+++ napkin/generic/butils.py
@@ -1,5 +1,7 @@
 import os
 
+import re
+
 
 def walkDir(d):
     for root, dirs, files in os.walk(d):
@@ -42,3 +44,20 @@ def allSubClasses(cls):
         all_subclasses.extend(allSubClasses(subclass))
 
     return reversed(all_subclasses)
+
+
+def fuzzyfinder(user_input, collection):
+    suggestions = []
+    pattern = '.*?'.join(user_input)  # Converts 'djm' to 'd.*?j.*?m'
+    regex = re.compile(pattern)  # Compiles a regex.
+    for item in collection:
+        match = regex.search(item)  # Checks if the current item matches the regex.
+        if match:
+            suggestions.append((len(match.group()), match.start(), item))
+    return [x for _, _, x in sorted(suggestions)]
+
+
+def fuzzymatches(input, other):
+    pattern = '.*?'.join(input)  # Converts 'djm' to 'd.*?j.*?m'
+    regex = re.compile(pattern)  # Compiles a regex.
+    return bool(regex.search(other))
