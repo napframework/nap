dc217c40849dcb84753974d58058cdc3084c900d
Stijn <stijn.v.beek@gmail.com>
2017-08-26 17:16:02 +0200

    change 'libportaudio-dev' dependency for linux into 'libportaudio2'

--- build.py
+++ build.py
@@ -43,7 +43,7 @@ def installDependenciesLinux():
                'libglm-dev',
                'libtclap-dev',
                'libfreeimage-dev',
-               'libportaudio-dev'
+               'libportaudio2'
                ])
 
 
