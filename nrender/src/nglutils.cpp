b6f43ebd41ba68208e33a9ff3137ff386f05713f
Chris Heywood <chris@theheywoods.id.au>
2017-09-26 15:01:04 +0200

    Add error code to printErrorMessage output.  Useful for when glewGetErrorString returns unknown error.

--- nrender/src/nglutils.cpp
+++ nrender/src/nglutils.cpp
@@ -183,7 +183,7 @@ namespace opengl
 		error_code = glGetError();
 		if (error_code != GL_NO_ERROR)
 		{
-			printMessage(MessageType::ERROR, "file: %s, line: %d, %s", file, line, glewGetErrorString(error_code));
+			printMessage(MessageType::ERROR, "file: %s, line: %d, %s (code: %d)", file, line, glewGetErrorString(error_code), error_code);
 #ifdef __APPLE__
             return false;
 #else
