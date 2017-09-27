4dd2f02e0b36dbb810657a1676fa26b0bb775fb9
Chris Heywood <chris@theheywoods.id.au>
2017-09-27 08:28:55 +0200

    Temporary workarounds for two related macOS issues: - Avoid crashes we're getting as a result of receiving events from the hidden window - Re-hide hidden window when it appears after returning from fullscreen mode for the first time

--- modules/naprender/src/renderservice.cpp
+++ modules/naprender/src/renderservice.cpp
@@ -14,6 +14,7 @@
 #include <glm/gtx/matrix_decompose.hpp>
 #include <rtti/factory.h>
 #include <nap/resourcemanager.h>
+#include <nap/logger.h>
 
 namespace nap
 {
@@ -85,7 +86,30 @@ namespace nap
 
 	void RenderService::addEvent(WindowEventPtr windowEvent)
 	{
+#ifdef __APPLE__
+		/** TODO Hacky temporary workaround for two macOS issues:
+		 * 1) We're receiving window events for the hidden window, which we then can't find in our getWindow lookup, resulting
+		 *    in general badness
+		 * 2) When dropping out of fullscreen mode for the first time the hidden window becomes visible.  Here we force it back 
+		 *    to visible but it still shows for a second.
+		 *
+		 * TODO: Fix macOS hidden window event issues cleanly. Removing logging when fixed.
+		 */
+		if (windowEvent->mWindow == getPrimaryWindow().getNumber()) {
+			nap::rtti::TypeInfo e_type = windowEvent->get_type();
+			if (e_type.is_derived_from(RTTI_OF(nap::WindowResizedEvent)) ||
+				e_type.is_derived_from(RTTI_OF(nap::WindowShownEvent))) {
+                Logger::info("Hidden/primary window (id %d) resized , hiding", windowEvent->mWindow);
+				SDL_HideWindow(getPrimaryWindow().getNativeWindow());
+			} else {
+                Logger::warn("Receiving unexpected event for hidden/primary window (id %d), ignoring", windowEvent->mWindow);
+			}
+			return;
+		}
+#endif
+		
 		nap::ObjectPtr<nap::Window> window = getWindow(windowEvent->mWindow);
+		assert (window != nullptr);
 		window->addEvent(std::move(windowEvent));
 	}
 
