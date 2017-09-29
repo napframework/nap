f84323d53bc731ecb2d20bffc28654c9a84df633
Chris Heywood <chris@theheywoods.id.au>
2017-09-29 11:28:36 +0200

    First crack at bundling test/app into its own class, getting rid of ugly globals and as a result clearing up a deconstruction crash that was happening on OSX.  Crash was due to global ObjectPtrs hanging around after ObjectPtrManager had been destroyed.  Includes Stijn's Core deconstructor.
    Will push structure out to other tests/apps after review.

---
+++ test/steef/src/testrunner.h
@@ -0,0 +1,60 @@
+#pragma once
+
+// Mod nap render includes
+#include <renderablemeshcomponent.h>
+#include <renderwindow.h>
+#include <perspcameracomponent.h>
+#include <sdlinput.h>
+#include <sdlwindow.h>
+
+// Nap includes
+#include <nap/core.h>
+#include <nap/resourcemanager.h>
+#include <sceneservice.h>
+#include <inputservice.h>
+#include <inputrouter.h>
+#include <orthocameracomponent.h>
+
+namespace nap
+{
+	class TestRunner
+	{
+		
+	public:
+		TestRunner();
+		~TestRunner() = default;
+		
+		bool init(Core& core);
+		
+		void onUpdate();
+		void onRender();
+
+		void handleWindowEvent(const WindowEvent& windowEvent);
+		void processWindowEvent(WindowEventPtr windowEvent);
+		void processInputEvent(InputEventPtr inEvent);
+		void setWindowFullscreen(std::string windowIdentifier, bool fullscreen);
+		
+		void shutdown();
+		
+	private:
+		
+		void updateBackgroundImage();
+		void updateShader();
+		
+		
+		ObjectPtr<Image> mVinylLabelImg;
+		ObjectPtr<Image> mVinylCoverImg;
+		
+		RenderService* mRenderService;
+		ResourceManagerService*	mResourceManagerService;
+		SceneService* mSceneService;
+		InputService* mInputService;
+		
+		ObjectPtr<RenderWindow>	mRenderWindow;
+		ObjectPtr<EntityInstance> mModelEntity;
+		ObjectPtr<EntityInstance> mCameraEntity;
+		ObjectPtr<EntityInstance> mBackgroundEntity;
+		
+		DefaultInputRouter mInputRouter;
+	};
+}
