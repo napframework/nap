a6b9b6d2710b0fccca3e1b91ffeb7d28995d8463
Stijn van Beek <stijn.v.beek@gmail.com>
2017-11-08 19:20:09 +0100

    Select json in audio test app

--- test/audiotest/src/audiotestapp.h
+++ test/audiotest/src/audiotestapp.h
@@ -4,11 +4,21 @@
 #include <app.h>
 
 
+/*
+ * Select your serialized json audio demo app here:
+ */
+const std::string appJson = "data/audiotest/audiofileplay.json";
+//const std::string appJson = "data/audiotest/pythonsequencer.json";
+//const std::string appJson = "data/audiotest/ambientdrone.json";
+//const std::string appJson = "data/audiotest/midilogging.json"; // Edit the midi input device in this json file!
+
+
 namespace nap
 {
 	class AudioTestApp : public BaseApp
 	{
 		RTTI_ENABLE(BaseApp)
+        
 	public:
 		AudioTestApp(Core& core) : BaseApp(core)	{ }
 
@@ -28,7 +38,7 @@ namespace nap
 		virtual void shutdown() override;
         
     private:
-        std::string mJsonFile = "data/audiotest/pythonsequencer.json";
+        std::string mJsonFile = appJson;
         
 	}; 
 }
