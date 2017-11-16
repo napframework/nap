6d29c322bcbd0088e7c3f8cee1f7831d45b2a9eb
Coen Klosters <coen@naivi.nl>
2017-11-16 21:55:55 +0100

    - disabled enabling / disabling of window resizing, causing issues with SDL 2.0 version < 2.05 - moved a lot of sdl functionality to nsdlgl utils, makes it useful for other people to use in the future - cleaned up render includes - hopefully fixing builds

--- nrender/src/nsdlgl.h
+++ nrender/src/nsdlgl.h
@@ -3,6 +3,7 @@
 // External Includes
 #include <string>
 #include <SDL.h>
+#include <glm/glm.hpp>
 
 namespace opengl
 {
@@ -14,22 +15,17 @@ namespace opengl
 	/**
 	 * Turn window v-sync on / off
 	 */
-	void setVSync(bool value);
+	void enableVSync(bool value);
 
 	/**
-	 * initVideo
-	 *
 	 * Initializes SDL video system
 	 * Call this before creating any windows or render contexts!
-	 * TODO: Move to sdl header
-	 * @return: if the system initialized correctly or not
+	 * @return if the system initialized correctly or not
 	 */
 	bool initVideo();
 
 	/**
-	 * initOpenGL
-	 *
-	 * Initializes the OpenGL glew wrapper
+	 * Initializes the OpenGL GLEW wrapper
 	 * This call will only succeed when OpenGL has a valid current context
 	 */
 	bool init();
@@ -68,30 +64,98 @@ namespace opengl
 	void setWindowTitle(SDL_Window* window, const std::string& name);
 
 	/**
-	 * quit
-	 *
+	 * @return the current opengl context
+	 */
+	SDL_GLContext getCurrentContext();
+
+	/**
+	 * Swap the OpenGL buffers for a window, if double-buffering is supported.
+	 * @param the window to swap the buffers for
+	 */
+	void swap(SDL_Window* window);
+
+	/**
+	 * Show or show a window
+	 * @param show if the window is shown or hidden
+	 */
+	void showWindow(SDL_Window* window, bool show);
+
+	/**
+	 * Sets the window to be full screen in desktop mode
+	 * @param window the window to enable / disable
+	 * @param value if the window is full screen 
+	 */
+	void setFullscreen(SDL_Window* window, bool value);
+
+	/**
+	 * Set up an OpenGL context for rendering into an OpenGL window.
+	 * The context must have been created with a compatible window.
+	 * @param window the window associated with the context
+	 * @param context the context to set up for rendering
+	 */
+	void makeCurrent(SDL_Window* window, SDL_GLContext context);
+
+	/**
+	 * Set an OpenGL window attribute before window creation.
+	 * @param window attribute to set
+	 * @param value the value to set the window attribute to
+	 */
+	void setAttribute(SDL_GLattr attribute, int value);
+
+	/**
+	 * @return The value of a window attribute associated with the currently active context.
+	 */
+	int getAttribute(SDL_GLattr attribute);
+
+	/**
+	 * Creates an opengl context associated with @window
+	 * @param window the window associated with the opengl context
+	 * @return the new OpenGL context
+	 */
+	SDL_GLContext createContext(SDL_Window* window);
+
+	/**
+	 * Returns the size of an OpenGL window
+	 * @param window the window to retrieve the size for
+	 * @return the window size
+	 */
+	glm::ivec2 getWindowSize(SDL_Window* window);
+
+	/**
+	 * resizes an OpenGL window
+	 * @param window the window to resize
+	 * @param size the new window size
+	 */
+	void setWindowSize(SDL_Window* window, const glm::ivec2& size);
+
+	/**
+	 * returns the OpenGL window position as pixel coordinates
+	 * @param window the window to get the position for
+	 * @return the window position in pixels
+	 */
+	glm::ivec2 getWindowPosition(SDL_Window* window);
+
+	/**
+	 * Set the window position
+	 * @param window the window to set the position for
+	 * @param location the window location in pixels
+	 */
+	void setWindowPosition(SDL_Window* window, const glm::ivec2& position);
+
+	/**
 	 * Shutdown SDL
-	 * TODO: Should go in to a separate sdl header in a different project
 	 */
 	void shutdown();
 
 	/**
-	 * getSDLError
-	 *
-	 * Returns the last SDL error as a string
-	 * TODO: Should go in to a separate sdl header in a different project 
+	 * @ return the last SDL error as a string
 	 */
 	std::string getSDLError();
 
-
 	/**
-	 * printSDLError
-	 *
 	 * Prints the last SDL error to screen
-	 * TODO: Should go in to a separate sdl header in a different project 
 	 */
 	void printSDLError();
-
 
 	/**
 	 * Polls SDL for an event
@@ -99,7 +163,6 @@ namespace opengl
 	 * @return if an event has been generated
 	 */
 	bool pollEvent(opengl::Event& inputEvent);
-
 
 	/**
 	 * Returns an SDL window based on the given ID
