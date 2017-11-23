6a0292f0f6c4ca4a5c82c96a186d852c69fab06f
Bas Korsmit <bmodone@gmail.com>
2017-11-23 10:06:43 -0800

    - Cleanup after merge - Initializing engine again

--- test/unittest/src/main.cpp
+++ test/unittest/src/main.cpp
@@ -1,24 +1,82 @@
-#include <iostream>
+#include <rtti/jsonreader.h>
+#include <rtti/jsonwriter.h>
+#include <fstream>
+#include <utility/fileutils.h>
 #include <nap/logger.h>
+#include <nap/core.h>
+#include <entity.h>
+#include <rtti/rttireader.h>
 
-#include "testmanager.h"
+using namespace nap;
+using namespace nap::rtti;
+using namespace nap::utility;
 
-// Tests
-#include "tests/testcore.h"
+bool ResolveLinks(const OwnedObjectList& objects, const UnresolvedPointerList& unresolvedPointers)
+{
+    std::map<std::string, RTTIObject*> objects_by_id;
+    for (auto& object : objects)
+        objects_by_id.insert({object->mID, object.get()});
+
+    for (const UnresolvedPointer& unresolvedPointer : unresolvedPointers) {
+        ResolvedRTTIPath resolved_path;
+        if (!unresolvedPointer.mRTTIPath.resolve(unresolvedPointer.mObject, resolved_path))
+            return false;
+
+        auto pos = objects_by_id.find(unresolvedPointer.mTargetID);
+        if (pos == objects_by_id.end())
+            return false;
+
+        if (!resolved_path.setValue(pos->second))
+            return false;
+    }
+
+    return true;
+}
+
+
+void loadAndSaveFile(const std::string& filename)
+{
+    // Load File
+
+    ErrorState err;
+
+    Core core;
+    if (!core.initializeEngine(err)) {
+        nap::Logger::fatal(err.toString());
+    }
+    auto& factory = core.getResourceManager()->getFactory();
+    nap::rtti::RTTIDeserializeResult result;
+    if (!readJSONFile(filename, factory, result, err)) {
+        nap::Logger::fatal(err.toString());
+        return;
+    }
+
+    if (!ResolveLinks(result.mReadObjects, result.mUnresolvedPointers)) {
+        nap::Logger::fatal("Failed to resolve links");
+        return;
+    }
+
+    // Save File
+    ObjectList objects;
+    for (auto& ob : result.mReadObjects) {
+        objects.emplace_back(ob.get());
+    }
+
+    JSONWriter writer;
+    if (!serializeObjects(objects, writer, err)) {
+        nap::Logger::fatal(err.toString());
+        return;
+    }
+
+    std::ofstream out(filename);
+    out << writer.GetJSON();
+    out.close();
+}
 
 int main(int argc, char** argv)
 {
-	nap::Logger::setLevel(nap::Logger::infoLevel());
-    nap::Logger::info("KOEK");
-	TEST_SETUP()
+    std::string filename ="./data/tommy/tommy.json";
 
-////	TEST_ADD(testXMLSerializer)
-	TEST_ADD(testCore)
-	TEST_ADD(testObjectPath)
-    TEST_ADD(testJSONSerializer)
-    TEST_ADD(testResourceManager)
-    TEST_ADD(testArrayAttribute)
-    TEST_ADD(testThreading);
+    loadAndSaveFile(filename);
 
-	TEST_RUN()
 }
