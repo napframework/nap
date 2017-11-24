b886cf598f26b106b2c582506309d0b6c9d7b92e
Coen Klosters <coen@naivi.nl>
2017-11-24 11:30:30 +0100

    - adding autmagic color convert functions, from color type a to color type b

--- modules/naprender/src/color.cpp
+++ modules/naprender/src/color.cpp
@@ -1,4 +1,5 @@
 #include "color.h"
+#include <mathutils.h>
 
 RTTI_DEFINE_BASE(nap::BaseColor)
 
@@ -38,3 +39,36 @@ RTTI_BEGIN_CLASS(nap::RGBAColorFloat)
 	RTTI_CONSTRUCTOR(float, float, float, float)
 	RTTI_PROPERTY("Values", &nap::RGBAColorFloat::mValues, nap::rtti::EPropertyMetaData::Default)
 RTTI_END_CLASS
+
+namespace nap
+{
+	static void floatToByte(const BaseColor& inColor, BaseColor& outColor, int channel)
+	{
+		float in  = inColor.getValue<float>(channel);
+		assert(outColor.getValueType() == RTTI_OF(nap::uint8));
+		uint8* out = static_cast<uint8*>(outColor.getData(channel));
+		*out = static_cast<uint8>(math::clamp<float>(in, 0.0f, 1.0f) * static_cast<float>(math::max<uint8>()));
+	}
+
+	static void byteToFLoat(const BaseColor& inColor, BaseColor& outColor, int channel)
+	{
+		uint8 in = inColor.getValue<uint8>(channel);
+		assert(outColor.getValueType() == RTTI_OF(float));
+		float* out = static_cast<float*>(outColor.getData(channel));
+		*out = static_cast<float>(in) / static_cast<float>(math::max<uint8>());
+	}
+}
+
+void nap::BaseColor::convertColor(const BaseColor& from, BaseColor& to)
+{
+	assert(from.getNumberOfChannels() >= to.getNumberOfChannels());
+	for (int i = 0; i < from.getNumberOfChannels(); i++)
+	{
+		byteToFLoat(from, to, i);
+	}
+}
+
+void nap::BaseColor::convert(BaseColor& color) const
+{
+	BaseColor::convertColor(*this, color);
+}
