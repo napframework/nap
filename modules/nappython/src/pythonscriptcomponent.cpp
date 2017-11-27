b7d63aedb2d69b702b4d6ce3de8c2ac050322fa5
Stijn van Beek <stijn.v.beek@gmail.com>
2017-11-27 11:51:45 +0100

    Python script component sibling variables with raw pointers

--- modules/nappython/src/pythonscriptcomponent.cpp
+++ modules/nappython/src/pythonscriptcomponent.cpp
@@ -42,9 +42,7 @@ namespace nap
         // Add all sibling components as identifiers in the script
         for (auto sibling : getEntityInstance()->getComponents())
         {
-            mComponentVariables.emplace_back(ObjectPtr<ComponentInstance>(sibling));
-            mScript.attr(sibling->getComponent()->mID.c_str())  = mComponentVariables.back();
-//            mScript.attr(sibling->getComponent()->mID.c_str())  = sibling;
+            mScript.attr(sibling->getComponent()->mID.c_str())  = sibling;
         }
         
         // Call ths script's init callback
