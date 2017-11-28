1545c6d7466b83730c3788b971397d7a7cc24660
Chris Heywood <chris@theheywoods.id.au>
2017-11-28 16:21:52 +0100

    - Break packaging out from build.py into package.py - A bunch of cleanup to packaging Python wrapper - Add Linux work to package.py

--- build.py
+++ build.py
@@ -4,8 +4,6 @@ from multiprocessing import cpu_count
 from sys import platform
 import sys
 import shutil
-import datetime
-import json
 
 WORKING_DIR = '.'
 
@@ -15,12 +13,7 @@ THIRDPARTY_URL = 'https://ae53bb936bc44bbffbac2dbd1f37101838603903@github.com/na
 NAP_URL = 'https://ae53bb936bc44bbffbac2dbd1f37101838603903@github.com/naivisoftware/nap.git'
 NAP_BRANCH = 'build'
 BUILD_DIR = 'build'
-PACKAGING_DIR = 'packaging'
-ARCHIVING_DIR = 'archiving'
-BUILDINFO_FILE = 'cmake/buildinfo.json'
-PACKAGED_BUILDINFO_FILE = '%s/cmake/buildinfo.json' % PACKAGING_DIR
 CLEAN_BUILD = False
-PACKAGE = False
 
 
 def isLocalGitRepo(d):
@@ -32,13 +25,10 @@ def isLocalGitRepo(d):
     return True
 
 
-def call(cwd, cmd, capture_output=False):
+def call(cwd, cmd):
     print('dir: %s' % cwd)
     print('cmd: %s' % cmd)
-    if capture_output:
-        proc = subprocess.Popen(cmd, cwd=cwd, stdout=subprocess.PIPE)
-    else:
-        proc = subprocess.Popen(cmd, cwd=cwd)
+    proc = subprocess.Popen(cmd, cwd=cwd)
     out, err = proc.communicate()
     if proc.returncode != 0:
         raise Exception(proc.returncode)
@@ -113,12 +103,6 @@ def main(targets):
         # generate prject
         call(WORKING_DIR, ['cmake', '-H.','-B%s' % BUILD_DIR,'-G', 'Visual Studio 14 2015 Win64', '-DPYBIND11_PYTHON_VERSION=3.5'])
 
-    if PACKAGE:
-        # Package our build
-        packaging_success = packageBuild()
-        # TODO improve error propogation behaviour
-        sys.exit(0 if packaging_success else 1)
-
     #copy targets
     build_targets = targets
 
@@ -140,125 +124,6 @@ def main(targets):
             call(d, ['cmake', '--build', BUILD_DIR, '--target', t])
 
 
-def packageBuild():
-    print("Packaging..")
-
-    # Note: Packaging directly from Python for now.  CPack was investigated but it was looking difficult to make it work when
-    # wanting to build multiple configurations at the same time.  If there was a reasonable CPack solution it feels like that 
-    # would be cleaner than this.
-
-    # Remove old packaging path if it exists
-    if os.path.exists(PACKAGING_DIR):
-        shutil.rmtree(PACKAGING_DIR, True)
-    os.makedirs(PACKAGING_DIR)
-
-    # Build package name
-
-    if platform in ["linux", "linux2"]:
-        # TODO
-        package_filename = package_filename % ('Linux') + '.tbz2'
-
-        # d = '%s/%s' % (WORKING_DIR, BUILD_DIR)
-        # call(d, ['make', t, '-j%s' % cpu_count()])
-    # osx
-    elif platform == 'darwin':
-        # Do the build, per configuration, installing into our packaging path
-        d = '%s/%s' % (WORKING_DIR, BUILD_DIR)
-        call(d, ['xcodebuild', '-configuration', 'Debug', '-target', 'install'])
-        call(d, ['xcodebuild', '-configuration', 'Release', '-target', 'install'])
-
-        # Generate Xcode projects for our examples
-        # TODO temp list of examples to iterate over.  Later we should be able to just process the whole dir.
-        # packaged_examples = ['tommy', 'rendertest']
-        # TODO work out if we want to do this
-        # for example in packaged_examples:
-        #     d = '%s/%s/examples/%s' % (WORKING_DIR, PACKAGING_DIR, example)
-        #     call(d, ['cmake', '-H.', '-Bxcode', '-G', 'Xcode'])
-
-        # Fix our dylib paths so fbxconverter will run from released package
-        # TODO push back into cmake
-        d = '%s/%s' % (WORKING_DIR, PACKAGING_DIR)
-        call(d, ['python', '../dist/osx/dylibpathfix.py', 'fbxconverter_pathfix'])
-
-        # TODO remove unwanted files (eg. .DS_Store)
-        
-        package_filename = buildPackageFilenameAndBuildInfo('macOS')
-        shutil.move(PACKAGING_DIR, package_filename)
-
-        # Archive
-        package_filename_with_ext = '%s.%s' % (package_filename, 'zip')
-        print("Archiving to %s" % package_filename_with_ext)
-        call(WORKING_DIR, ['zip', '-yr', package_filename_with_ext, package_filename])
-
-        # Cleanup
-        shutil.move(package_filename, PACKAGING_DIR)
-        print("Done.")
-    # windows
-    else:
-        call(WORKING_DIR, ['cmake', '--build', BUILD_DIR, '--target', 'install', '--config', 'Debug'])
-        call(WORKING_DIR, ['cmake', '--build', BUILD_DIR, '--target', 'install', '--config', 'Release'])
-
-        package_filename = buildPackageFilenameAndBuildInfo('Win64')
-
-        # Rename our packaging dir to match the release
-        shutil.move(PACKAGING_DIR, package_filename)
-
-        # Create our archive dir, used to create a copy level folder within the archive
-        if os.path.exists(ARCHIVING_DIR):
-            shutil.rmtree(ARCHIVING_DIR, True)
-        os.makedirs(ARCHIVING_DIR)
-        archive_path = os.path.join(ARCHIVING_DIR, package_filename)
-        shutil.move(package_filename, archive_path)
-
-        # Create archive
-        # package_filename_with_ext = '%s.%s' % (package_filename, 'zip')
-        print("Archiving to %s.zip" % package_filename)
-        shutil.make_archive(package_filename, 'zip', ARCHIVING_DIR)
-
-        # Cleanup
-        shutil.move(archive_path, PACKAGING_DIR)
-        shutil.rmtree(ARCHIVING_DIR)
-        print("Done.")
-
-    return True
-
-def buildPackageFilenameAndBuildInfo(platform):
-    timestamp = datetime.datetime.now().strftime('%Y.%m.%dT%H.%M')
-    # Update our JSON build info file and get the current version
-    version = processBuildInfo(timestamp)
-    package_filename = "NAP-%s-%s-%s" % (version, platform, timestamp)
-    return package_filename
-
-def processBuildInfo(timestamp):
-    # Write build info back out with bumped build number
-    with open(BUILDINFO_FILE) as json_file:
-        build_info = json.load(json_file)
-
-    # TODO add validation
-
-    # Read our version
-    version = build_info['version']
-
-    # Bump build number
-    if not 'buildNumber' in build_info:
-        build_info['buildNumber'] = 0
-    build_info['buildNumber'] += 1
-
-    # Write build info back out with bumped build number
-    with open(BUILDINFO_FILE, 'w') as outfile:
-        json.dump(build_info, outfile, sort_keys=True, indent=2)
-
-    # Add git revision and timestamp to build info and bundle into package
-    # TODO add ability to not have git revision in release build
-    git_revision = call(WORKING_DIR, ['git', 'rev-parse', 'HEAD'], True)
-    build_info['gitRevision'] = git_revision.decode('ascii', 'ignore').strip()
-    build_info['timestamp'] = timestamp
-    with open(PACKAGED_BUILDINFO_FILE, 'w') as outfile:
-        json.dump(build_info, outfile, sort_keys=True, indent=2)
-
-    # Return version for population into package name
-    return version
-    
 # Extracts all targets from the command line input arguments, syntax is: target:project, ie: target:napcore
 def extractTargets():
     targets = []
@@ -268,12 +133,6 @@ def extractTargets():
             print("performing clean build")
             global CLEAN_BUILD
             CLEAN_BUILD = True
-            continue
-
-        if arg == "package":
-            print("Packaging NAP")
-            global PACKAGE
-            PACKAGE = True
             continue
 
         # not a target
---
+++ package.py
@@ -0,0 +1,263 @@
+import os
+import subprocess
+from multiprocessing import cpu_count
+from sys import platform
+import sys
+import shutil
+import datetime
+import json
+
+WORKING_DIR = '.'
+BUILD_DIR = 'build'
+PACKAGING_DIR = 'packaging'
+ARCHIVING_DIR = 'archiving'
+BUILDINFO_FILE = 'cmake/buildInfo.json'
+PACKAGED_BUILDINFO_FILE = '%s/cmake/buildinfo.json' % PACKAGING_DIR
+
+def call(cwd, cmd, capture_output=False):
+    # print('dir: %s' % cwd)
+    # print('cmd: %s' % cmd)
+    if capture_output:
+        proc = subprocess.Popen(cmd, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    else:
+        proc = subprocess.Popen(cmd, cwd=cwd)
+    (out, err) = proc.communicate()
+    if proc.returncode != 0:
+        raise Exception(proc.returncode)
+    return (out, err)
+
+# Install dependencies for Linux, checking if we have them first (avoiding an sudo if they're already installed)
+def install_dependencies_linux():
+    dependencies = [
+        'cmake',
+        'build-essential',
+        'python3-dev',
+        'libsdl2-dev',
+        'libglew-dev',
+        'libassimp-dev',
+        'libglm-dev',
+        'libtclap-dev',
+        'libfreeimage-dev',
+        'ffmpeg',
+        'libavcodec-dev',
+        'libavformat-dev',
+        'libavutil-dev'
+    ]
+
+    # Create a list of packages we need to install
+    packages_to_install = []
+    for d in dependencies:
+        if not is_linux_apt_package_installed(d):
+            packages_to_install.append(d)
+
+    # Return if all already installed
+    if len(packages_to_install) == 0:
+        print("All dependencies already installed")
+        return
+
+    # Build cmd and install with apt
+    print("Installing packages via apt: %s" % ' '.join(packages_to_install))
+    apt_cmd = ['sudo', 'apt-get', '--assume-yes', 'install']
+    apt_cmd.extend(packages_to_install)
+    call(WORKING_DIR, apt_cmd)
+
+# Check if we have a package installed with apt
+def is_linux_apt_package_installed(package_name):
+    (out, err) = call(WORKING_DIR, ['dpkg', '--get-selections', package_name], True)
+    installed = not 'no packages' in err
+    print("Package '%s' installed? %s" % (package_name, installed))
+    return installed  
+
+def is_osx_brew_installed():
+    try:
+        brew_path = call(WORKING_DIR, ['which', 'brew'], True)[0].strip()
+        return os.path.exists(brew_path)
+    except:
+        return False
+
+def install_dependencies_osx():
+    if not is_osx_brew_installed():
+        print("Not installing macOS dependencies as homebrew was not found")
+        # TODO potentially fail here in future if we have a hard homebrew dependency
+        return
+
+    for pack in ['cmake', 'sdl2', 'glew', 'glm', 'assimp', 'tclap', 'ffmpeg', 'mpg123']:
+        try:
+            call(WORKING_DIR, ['brew', 'install', pack])
+        except:
+            print("Failed installing %s via homebrew" % pack)
+
+def install_dependencies():
+    print("Installing dependencies")
+    if platform in ['linux', 'linux2']:
+        install_dependencies_linux()
+    elif platform == 'darwin':
+        install_dependencies_osx()
+    elif platform == 'win32':
+        # Windows...
+        pass
+
+def package():
+    print("Packaging..")
+
+    install_dependencies()
+
+    # Note: Packaging directly from Python for now.  CPack was investigated but it was looking difficult to make it work when
+    # wanting to build multiple configurations at the same time.  If there was a reasonable CPack solution it feels like that 
+    # would be cleaner than this.
+
+    # Remove old packaging path if it exists
+    if os.path.exists(PACKAGING_DIR):
+        shutil.rmtree(PACKAGING_DIR, True)
+    os.makedirs(PACKAGING_DIR)
+
+    if platform in ["linux", "linux2"]:
+        # TODO Bundling ReleaseWithDebInfo for now as Release is crashing 28-11-17
+        for build_type in ['Debug', 'ReleaseWithDebInfo']:
+            build_dir_for_type = BUILD_DIR + build_type
+            call(WORKING_DIR, ['cmake', '-H.', '-B%s' % build_dir_for_type, '-DCMAKE_BUILD_TYPE=%s' % build_type])
+
+            d = '%s/%s' % (WORKING_DIR, build_dir_for_type)
+                call(d, ['make', 'all', 'install', '-j%s' % cpu_count()])
+
+        # Create archive
+        archive_to_linux_tar_xz()
+    elif platform == 'darwin':
+        # Generate project
+        call(WORKING_DIR, ['cmake', '-H.', '-B%s' % BUILD_DIR, '-G', 'Xcode'])
+
+        # Build & install to packaging dir
+        d = '%s/%s' % (WORKING_DIR, BUILD_DIR)
+        for configuration in ['Debug', 'Release']:
+            call(d, ['xcodebuild', '-configuration', configuration, '-target', 'install'])
+
+        # Fix our dylib paths so fbxconverter will run from released package
+        # TODO push back into cmake
+        d = '%s/%s' % (WORKING_DIR, PACKAGING_DIR)
+        call(d, ['python', '../dist/osx/dylibpathfix.py', 'fbxconverter_pathfix'])
+
+        # Create archive
+        archive_to_macos_zip()
+    else:
+        # Create build dir if it doesn't exist
+        if not os.path.exists(BUILD_DIR):
+            os.makedirs(BUILD_DIR)
+
+        # Generate project
+        call(WORKING_DIR, ['cmake', '-H.','-B%s' % BUILD_DIR,'-G', 'Visual Studio 14 2015 Win64', '-DPYBIND11_PYTHON_VERSION=3.5'])
+
+        # Build & install to packaging dir
+        for configuration in ['Debug', 'Release']:
+            call(WORKING_DIR, ['cmake', '--build', BUILD_DIR, '--target', 'install', '--config', configuration])
+
+        # Create archive
+        archive_to_win64_zip()
+
+    return True
+
+# Create build archive to xz tarball on Linux
+def archive_to_linux_tar_xz():
+    package_filename = build_package_filename_and_build_info('Linux')
+    shutil.move(PACKAGING_DIR, package_filename)
+
+    package_filename_with_ext = '%s.%s' % (package_filename, 'tar.xz')
+    print("Archiving to %s" % package_filename_with_ext)
+    call(WORKING_DIR, ['tar', '-cJvf', package_filename_with_ext, package_filename])
+
+    # Cleanup
+    shutil.move(package_filename, PACKAGING_DIR)
+    print("Packaged to: %s" % package_filename_with_ext)  
+
+# Create build archive to zip on macOS
+def archive_to_macos_zip():
+    # TODO remove unwanted files (eg. .DS_Store)
+    
+    package_filename = build_package_filename_and_build_info('macOS')
+    shutil.move(PACKAGING_DIR, package_filename)
+
+    # Archive
+    package_filename_with_ext = '%s.%s' % (package_filename, 'zip')
+    print("Archiving to %s" % package_filename_with_ext)
+    call(WORKING_DIR, ['zip', '-yr', package_filename_with_ext, package_filename])
+
+    # Cleanup
+    shutil.move(package_filename, PACKAGING_DIR)
+    print("Packaged to: %s" % package_filename_with_ext)  
+
+# Create build archive to zip on Win64
+def archive_to_win64_zip():
+    package_filename = build_package_filename_and_build_info('Win64')
+
+    # Rename our packaging dir to match the release
+    shutil.move(PACKAGING_DIR, package_filename)
+
+    # Create our archive dir, used to create a copy level folder within the archive
+    if os.path.exists(ARCHIVING_DIR):
+        shutil.rmtree(ARCHIVING_DIR, True)
+    os.makedirs(ARCHIVING_DIR)
+    archive_path = os.path.join(ARCHIVING_DIR, package_filename)
+    shutil.move(package_filename, archive_path)
+
+    # Create archive
+    # package_filename_with_ext = '%s.%s' % (package_filename, 'zip')
+    print("Archiving to %s.zip" % package_filename)
+    shutil.make_archive(package_filename, 'zip', ARCHIVING_DIR)
+
+    # Cleanup
+    shutil.move(archive_path, PACKAGING_DIR)
+    shutil.rmtree(ARCHIVING_DIR)
+
+    print("Packaged to: %s" % package_filename_with_ext)  
+
+# Build the name of our package and populate our JSON build info file
+def build_package_filename_and_build_info(platform):
+    timestamp = datetime.datetime.now().strftime('%Y.%m.%dT%H.%M')
+    # Update our JSON build info file and get the current version
+    version = process_build_info(timestamp)
+    package_filename = "NAP-%s-%s-%s" % (version, platform, timestamp)
+    return package_filename
+
+# Process our JSON build info: Fetch our revision, bump our build number and create a build info
+# in the package including source git revision
+def process_build_info(timestamp):
+    # Write build info back out with bumped build number
+    with open(BUILDINFO_FILE) as json_file:
+        build_info = json.load(json_file)
+
+    # TODO validate loaded JSON
+
+    # Read our version
+    version = build_info['version']
+
+    # Bump build number
+    if not 'buildNumber' in build_info:
+        build_info['buildNumber'] = 0
+    build_info['buildNumber'] += 1
+
+    # Write build info back out with bumped build number
+    with open(BUILDINFO_FILE, 'w') as outfile:
+        json.dump(build_info, outfile, sort_keys=True, indent=2)
+
+    # Add git revision and timestamp to build info and bundle into package
+    # TODO add ability to not have git revision in release build
+    (git_revision, _) = call(WORKING_DIR, ['git', 'rev-parse', 'HEAD'], True)
+    build_info['gitRevision'] = git_revision.decode('ascii', 'ignore').strip()
+    build_info['timestamp'] = timestamp
+    with open(PACKAGED_BUILDINFO_FILE, 'w') as outfile:
+        json.dump(build_info, outfile, sort_keys=True, indent=2)
+
+    # Return version for population into package name
+    return version
+    
+# Main
+if __name__ == '__main__':
+    # TODO use argparse and add options for
+    # - managing clean build behaviour
+    # - not populating git revision into buildInfo
+    # - external build number management?
+
+    # Package our build
+    packaging_success = package()
+
+    # TODO improve error propogation behaviour
+    sys.exit(0 if packaging_success else 1)
