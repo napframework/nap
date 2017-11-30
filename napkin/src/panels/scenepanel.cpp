216dc5c8ed91ae9b62a24eed1facf6bb7f113af7
Bas Korsmit <bmodone@gmail.com>
2017-11-29 22:43:32 -0800

    Adding scene panel stub

---
+++ napkin/src/panels/scenepanel.cpp
@@ -0,0 +1,70 @@
+#include "scenepanel.h"
+#include <appcontext.h>
+#include <sceneservice.h>
+
+
+/**
+ * @return All currently loaded scenes
+ */
+nap::SceneService::SceneSet getScenes()
+{
+	return napkin::AppContext::get().getCore().getService<nap::SceneService>()->getScenes();
+}
+
+napkin::SceneItem::SceneItem(nap::Scene& scene) : mScene(scene)
+{
+	setText(QString::fromStdString(scene.mID));
+    mScene.getRootEntity();
+}
+
+
+napkin::SceneModel::SceneModel() : QStandardItemModel()
+{
+    refresh();
+    setHorizontalHeaderLabels({"Name"});
+
+    connect(&AppContext::get(), &AppContext::fileOpened, this, &SceneModel::onFileOpened);
+    connect(&AppContext::get(), &AppContext::newFileCreated, this, &SceneModel::onNewFile);
+}
+
+void napkin::SceneModel::refresh()
+{
+    while (rowCount() > 0)
+        removeRow(0);
+
+	for (auto scene : getScenes())
+		appendRow(new SceneItem(*scene));
+}
+
+void napkin::SceneModel::onEntityAdded(nap::Entity* newEntity, nap::Entity* parent)
+{
+}
+
+void napkin::SceneModel::onComponentAdded(nap::Component& comp, nap::Entity& owner)
+{
+}
+
+void napkin::SceneModel::onObjectAdded(nap::rtti::RTTIObject& obj)
+{
+}
+
+void napkin::SceneModel::onObjectRemoved(nap::rtti::RTTIObject& obj)
+{
+}
+
+void napkin::SceneModel::onNewFile()
+{
+    refresh();
+}
+
+void napkin::SceneModel::onFileOpened(const QString& filename)
+{
+    refresh();
+}
+
+napkin::ScenePanel::ScenePanel() : QWidget()
+{
+	setLayout(&mLayout);
+	layout()->addWidget(&mFilterView);
+    mFilterView.setModel(&mModel);
+}
