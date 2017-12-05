ed891570f95f74b459ec713a2bcad24107c3c65b
jellevdbeek <jelle@presence-vr.net>
2017-12-05 16:22:30 +0100

    - Added 'm' prefix to members of opengl::Texture2DSettings - Added isValid & comparison operators to opengl::Texture2DSettings - ImageSequenceLayer now creates a single GPU texture and updates it whenever a frame changes, instead of creating a texture for each frame in the sequence. The init code checks that all images use the same dimenstions/formats.

--- modules/naprender/src/texture2d.cpp
+++ modules/naprender/src/texture2d.cpp
@@ -20,30 +20,30 @@ namespace nap
 	bool Texture2D::init(utility::ErrorState& errorState)
 	{
 		opengl::Texture2DSettings settings;
-		settings.width = mWidth;
-		settings.height = mHeight;
+		settings.mWidth = mWidth;
+		settings.mHeight = mHeight;
 
 		switch (mFormat)
 	{
 		case EFormat::RGBA8:
-			settings.format			= GL_RGBA;
-			settings.internalFormat = GL_RGBA8;
-			settings.type			= GL_UNSIGNED_BYTE;
+			settings.mFormat			= GL_RGBA;
+			settings.mInternalFormat 	= GL_RGBA8;
+			settings.mType				= GL_UNSIGNED_BYTE;
 			break;
 		case EFormat::RGB8:
-			settings.format			= GL_RGB;
-			settings.internalFormat = GL_RGB8;
-			settings.type			= GL_UNSIGNED_BYTE;
+			settings.mFormat			= GL_RGB;
+			settings.mInternalFormat 	= GL_RGB8;
+			settings.mType				= GL_UNSIGNED_BYTE;
 			break;
 		case EFormat::R8:
-			settings.format			= GL_RED;
-			settings.internalFormat = GL_R8;
-			settings.type			= GL_UNSIGNED_BYTE;
+			settings.mFormat			= GL_RED;
+			settings.mInternalFormat 	= GL_R8;
+			settings.mType				= GL_UNSIGNED_BYTE;
 			break;
 		case EFormat::Depth:
-			settings.format			= GL_DEPTH_COMPONENT;
-			settings.internalFormat = GL_DEPTH_COMPONENT;
-			settings.type			= GL_FLOAT;
+			settings.mFormat			= GL_DEPTH_COMPONENT;
+			settings.mInternalFormat 	= GL_DEPTH_COMPONENT;
+			settings.mType				= GL_FLOAT;
 			break;
 	}
 
