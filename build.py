4ca76648b0b890a93da1dd1e85c416227d659840
Coen Klosters <coen@naivi.nl>
2017-12-23 16:43:00 +0100

    - splitting build.py in to build.py and installdependencies.py - removed old installdependencies.sh

--- build.py
+++ build.py
@@ -35,56 +35,7 @@ def call(cwd, cmd):
     return out
 
 
-def installDependenciesLinux():
-    d = WORKING_DIR
-    call('.', ['sudo', 'apt-get', '--assume-yes', 'install',
-               'cmake',
-               'build-essential',
-	       'python3-dev',
-               'libsdl2-dev',
-               'libglew-dev',
-               'libassimp-dev',
-               'libglm-dev',
-               'libtclap-dev',
-               'libfreeimage-dev',
-               'ffmpeg',
-               'libavcodec-dev',
-               'libavformat-dev',
-               'libavutil-dev'
-               ])
-
-
-def isBrewInstalled():
-    try:
-        return os.path.exists(call(WORKING_DIR, ['which', 'brew']))
-    except:
-        return False
-
-
-def installDependenciesOSX():
-    d = WORKING_DIR
-    for pack in ['cmake', 'sdl2', 'glew', 'glm', 'assimp', 'tclap', 'ffmpeg', 'mpg123']:
-        try:
-            call(d, ['brew', 'install', pack])
-        except:
-            print('Failed installing %s' % pack)
-
-
-def installDependencies():
-    print('Installing dependencies')
-    if platform == "linux" or platform == "linux2":
-        installDependenciesLinux()
-    elif platform == "darwin":
-        installDependenciesOSX()
-    elif platform == "win32":
-        # Windows...
-        pass
-
-
 def main(targets):
-    # install osx / linux specific dependendies
-    installDependencies()
-
     # clear build directory when a clean build is required
     print(CLEAN_BUILD)
     if CLEAN_BUILD and os.path.exists(BUILD_DIR):
