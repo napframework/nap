352fa8adbd42bba52f4d4eeaef8b2d984e48ff2e
Chris Heywood <chris@theheywoods.id.au>
2018-01-21 11:57:01 +0100

    Win64 fixes for changes resulting from showing headers properly in IDE

--- dist/cmake/napcore.cmake
+++ dist/cmake/napcore.cmake
@@ -4,26 +4,24 @@ if (WIN32)
         NAMES Release/napcore.lib
         HINTS ${CMAKE_CURRENT_LIST_DIR}/../lib/
     )
-    set(NAPCORE_LIBS_RELEASE_DLL ${NAPCORE_LIBS_DIR}/Release/napcore.dll)
-    set(NAPCORE_LIBS_DEBUG_DLL ${NAPCORE_LIBS_DIR}/Debug/napcore.dll)
-    set(NAPCORE_LIBS_IMPLIB_DEBUG ${NAPCORE_LIBS_DIR}/Debug/napcore.lib)
-    set(NAPCORE_LIBS_IMPLIB_RELEASE ${NAPCORE_LIBS_DIR}/Release/napcore.lib)
+    set(NAPCORE_LIBS_DEBUG ${NAPCORE_LIBS_DIR}/Debug/napcore.lib)
+    set(NAPCORE_LIBS_RELEASE ${NAPCORE_LIBS_DIR}/Release/napcore.lib)
 elseif (APPLE)
     find_path(
         NAPCORE_LIBS_DIR
         NAMES Release/libnapcore.dylib
         HINTS ${CMAKE_CURRENT_LIST_DIR}/../lib/
     )
-    set(NAPCORE_LIBS_RELEASE_DLL ${NAPCORE_LIBS_DIR}/Release/libnapcore.dylib)
-    set(NAPCORE_LIBS_DEBUG_DLL ${NAPCORE_LIBS_DIR}/Debug/libnapcore.dylib)
+    set(NAPCORE_LIBS_RELEASE ${NAPCORE_LIBS_DIR}/Release/libnapcore.dylib)
+    set(NAPCORE_LIBS_DEBUG ${NAPCORE_LIBS_DIR}/Debug/libnapcore.dylib)
 elseif (UNIX)
     find_path(
         NAPCORE_LIBS_DIR
 	NAMES Debug/libnapcore.so
         HINTS ${CMAKE_CURRENT_LIST_DIR}/../lib/
     )
-    set(NAPCORE_LIBS_RELEASE_DLL ${NAPCORE_LIBS_DIR}/Release/libnapcore.so)
-    set(NAPCORE_LIBS_DEBUG_DLL ${NAPCORE_LIBS_DIR}/Debug/libnapcore.so)
+    set(NAPCORE_LIBS_RELEASE ${NAPCORE_LIBS_DIR}/Release/libnapcore.so)
+    set(NAPCORE_LIBS_DEBUG ${NAPCORE_LIBS_DIR}/Debug/libnapcore.so)
 endif()
 
 if (NOT NAPCORE_LIBS_DIR)
@@ -31,8 +29,8 @@ if (NOT NAPCORE_LIBS_DIR)
 endif()
 
 add_library(napcore INTERFACE)
-target_link_libraries(napcore INTERFACE debug ${NAPCORE_LIBS_DEBUG_DLL})
-target_link_libraries(napcore INTERFACE optimized ${NAPCORE_LIBS_RELEASE_DLL})
+target_link_libraries(napcore INTERFACE debug ${NAPCORE_LIBS_DEBUG})
+target_link_libraries(napcore INTERFACE optimized ${NAPCORE_LIBS_RELEASE})
 file(GLOB core_headers ${CMAKE_CURRENT_LIST_DIR}/../include/nap/*.h)
 target_sources(napcore INTERFACE ${core_headers})
 source_group(NAP\\Core FILES ${core_headers})
@@ -43,13 +41,6 @@ source_group(NAP\\Utility FILES ${utility_headers})
 
 
 if (WIN32)
-    set_target_properties(napcore PROPERTIES
-        IMPORTED_IMPLIB_RELEASE ${NAPCORE_LIBS_IMPLIB_RELEASE}
-        IMPORTED_IMPLIB_DEBUG ${NAPCORE_LIBS_IMPLIB_DEBUG}
-        IMPORTED_IMPLIB_MINSIZEREL ${NAPCORE_LIBS_IMPLIB_RELEASE}
-        IMPORTED_IMPLIB_RELWITHDEBINFO ${NAPCORE_LIBS_IMPLIB_RELEASE}
-    )
-
     # Find our naputility import lib
     find_package(naputility REQUIRED)
     target_link_libraries(${PROJECT_NAME} naputility)
@@ -58,6 +49,6 @@ if (WIN32)
     add_custom_command(
         TARGET ${PROJECT_NAME}
         POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:napcore> $<TARGET_FILE_DIR:${PROJECT_NAME}>/
+        COMMAND ${CMAKE_COMMAND} -E copy ${NAPCORE_LIBS_DIR}/$<CONFIG>/napcore.dll $<TARGET_FILE_DIR:${PROJECT_NAME}>/
     )
 endif()
