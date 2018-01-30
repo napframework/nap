ce805a8e97af32f0ba3b1071c818083d0492d0b4
Coen Klosters <coen@naivi.nl>
2018-01-30 17:19:36 +0100

    - renamed clickandtrace to blobtrace

---
+++ demos/blobtrace/src/blobtraceapp.h
@@ -0,0 +1,109 @@
+#pragma once
+
+// Mod nap render includes
+#include <renderablemeshcomponent.h>
+#include <renderwindow.h>
+
+// Nap includes
+#include <nap/resourcemanager.h>
+#include <sceneservice.h>
+#include <inputservice.h>
+#include <imguiservice.h>
+#include <app.h>
+#include <spheremesh.h>
+#include <smoothdamp.h>
+
+namespace nap
+{
+	/**
+	 * Demo application that is called from within the main loop
+	 *
+	 * Shows a plane with a pulsating blob in the middle of the canvas
+	 * The blob is procedurally generated in a shader, including lighting
+	 * Move the mouse to change the location of the blob on the canvas
+	 * Use the left mouse button to rotate the camera
+	 * Use the right mouse button to zoom
+	 * 
+	 * This demo uses ray-casting to resolve the position of the blob on the plane
+	 * A ray is cast from the screen in to the world when the mouse is moved
+	 * When the ray hits a triangle the returned (barycentric) coordinates are used to compute the uv coordinates at the hit location
+	 * All movement and positioning of the blob happens in uv space!
+	 * The plane only has 2 triangles. All shading and lighting is handled by the blob.vert and fragment shader
+	 * 
+	 * On update a set of shader variables is set, most importantly the accumulated time. 
+	 * The blob slowly moves towards it's target. The SmoothOperator is responsible for this effect
+	 * Every frame that operator is fed with the current mouse location and moves the blob target towards it using a cheap dampening model.
+	 * The velocity of that movement is used by the blob shader to change the blob size and frequency.
+	 *
+	 * Except for the ray-casting this demo is rather simple. The only components are a camera and a plane
+	 * The only object that is rendered is the plane. The shader computes the normals based on the algorithm that creates the blob
+	 * These normals are used to perform shading
+	 * 
+	 * Mouse and key events are forwarded to the input service, the input service collects input events
+	 * and processes all of them on update. Because NAP does not have a default space (objects can
+	 * be rendered in multiple ways), you need to specify what input actually means to the application. 
+	 * The input router does that for you. This demo uses the default one that forwards the events to every input component
+	 * Refer to the cpp-update() call for more information on handling input
+	 */
+	class BlobTraceApp : public App
+	{
+		RTTI_ENABLE(App)
+	public:
+		BlobTraceApp(nap::Core& core) : App(core)	{ }
+
+		/**
+		 *	Initialize app specific data structures
+		 */
+		bool init(utility::ErrorState& error) override;
+		
+		/**
+		 *	Update is called before render, performs all the app logic
+		 */
+		void update(double deltaTime) override;
+
+		/**
+		 *	Render is called after update, pushes all renderable objects to the GPU
+		 */
+		void render() override;
+
+		/**
+		 *	Forwards the received window event to the render service
+		 */
+		void windowMessageReceived(WindowEventPtr windowEvent) override;
+		
+		/**
+		 *  Forwards the received input event to the input service
+		 */
+		void inputMessageReceived(InputEventPtr inputEvent) override;
+		
+		/**
+		 *	Called when loop finishes
+		 */
+		int shutdown() override;
+
+	private:
+		// Nap Services
+		RenderService* mRenderService = nullptr;									//< Render Service that handles render calls
+		ResourceManager* mResourceManager = nullptr;								//< Manages all the loaded resources
+		SceneService* mSceneService = nullptr;										//< Manages all the objects in the scene
+		InputService* mInputService = nullptr;										//< Input service for processing input
+		IMGuiService* mGuiService = nullptr;										//< Manages gui related update / draw calls
+		ObjectPtr<RenderWindow> mRenderWindow;										//< Pointer to the render window		
+		ObjectPtr<EntityInstance> mCameraEntity = nullptr;							//< Pointer to the entity that holds the camera
+		ObjectPtr<EntityInstance> mPlaneEntity = nullptr;							//< Pointer to the entity that holds the sphere
+		ObjectPtr<SphereMesh> mPlaneMesh = nullptr;									//< Plane that is rendered to screen
+		RGBAColor8 mTextHighlightColor = { 0xC8, 0x69, 0x69, 0xFF };				//< GUI text highlight color
+		bool mMouseDown = false;													//< If the mouse button is currently pressed
+		float mTime = 0.0f;															//< Current application running time
+		glm::vec3 mMouseUvPosition = { 0.5,0.5,0.0 };								//< Current mouse position in uv space
+		math::Vec3SmoothOperator mUVSmoother = { glm::vec3(0.5,0.5,0.0), 1.0f };	//< Blends the target to the current mouse position
+			
+		/**
+		 * Shoots a ray from the camera in screen space in to the world
+		 * When intersected with the plane it calculates the plane's uv intersection location
+		 * This location is stored and used as a target for the blob when rendering
+		 * @param event the mouse event that contains the current mouse location
+		 */
+		void doTrace(const PointerEvent& event);
+	};
+}
