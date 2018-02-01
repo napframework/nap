f151adfec540fde042b1e3c87ee813ee9433b03e
Coen Klosters <coen@naivi.nl>
2018-02-01 14:46:03 +0100

    - removing unnecessary code

--- demos/multiwindow/src/multiwindowapp.h
+++ demos/multiwindow/src/multiwindowapp.h
@@ -88,7 +88,5 @@ namespace nap
 		ObjectPtr<EntityInstance>	mWorldEntity = nullptr;				//< Pointer to the entity that holds the world
 		ObjectPtr<EntityInstance>	mPlaneOneEntity = nullptr;			//< Pointer to the plane one
 		ObjectPtr<EntityInstance>	mPlaneTwoEntity = nullptr;			//< Pointer to plane two
-
-		void toWorld(PointerEvent& event);
 	};
 }
