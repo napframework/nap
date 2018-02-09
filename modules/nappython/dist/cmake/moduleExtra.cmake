6d67fecaff6750e0920312c1be8e2d8bff07027b
Chris Heywood <chris@theheywoods.id.au>
2018-02-09 14:01:07 +0100

    Fix for packaging python lib with win32 projects which don't want napkin packaged

--- modules/nappython/dist/cmake/moduleExtra.cmake
+++ modules/nappython/dist/cmake/moduleExtra.cmake
@@ -1,9 +1,11 @@
 # Copy Python framework libs, during post-build or install, depending on our platform
 if(WIN32)
-    add_custom_command(TARGET ${PROJECT_NAME}
-                       POST_BUILD
-                       COMMAND ${CMAKE_COMMAND} -E copy_directory ${THIRDPARTY_DIR}/python/lib $<TARGET_FILE_DIR:${PROJECT_NAME}>
-                       )
+    if(DEFINED PACKAGE_NAPKIN AND NOT PACKAGE_NAPKIN)
+        add_custom_command(TARGET ${PROJECT_NAME}
+                           POST_BUILD
+                           COMMAND ${CMAKE_COMMAND} -E copy_directory ${THIRDPARTY_DIR}/python/ $<TARGET_FILE_DIR:${PROJECT_NAME}>
+                           )
+    endif()
 endif()
 if(APPLE)
     # Install our Python dylib from thirdparty
