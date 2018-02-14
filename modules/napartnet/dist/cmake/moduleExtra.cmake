6a082a0d662829e30b68eb1c8b6a12300ed0c514
Chris Heywood <chris@theheywoods.id.au>
2018-02-14 17:46:10 +0100

    Device a model for keeping thirdparty requirements for modules alongside the module for use with released NAP.  With the intention that it is also somethign that could be used with for packaging individual modules in the long run..  Testing on etherdream.

--- modules/napartnet/dist/cmake/moduleExtra.cmake
+++ modules/napartnet/dist/cmake/moduleExtra.cmake
@@ -11,15 +11,6 @@ if(WIN32)
                                $<TARGET_FILE_DIR:${PROJECT_NAME}> 
                        )
 elseif(UNIX)
-    # Add post-build step to set artnet RPATH
-    # add_custom_command(TARGET ${PROJECT_NAME}
-    #                    POST_BUILD
-    #                    COMMAND ${CMAKE_INSTALL_NAME_TOOL} 
-    #                            -add_rpath
-    #                            $<TARGET_FILE_DIR:artnet>
-    #                            $<TARGET_FILE:${PROJECT_NAME}> 
-    #                    )
-
     # Install artnet lib into packaged app
     install(FILES $<TARGET_FILE:artnet> DESTINATION lib)
 endif()
