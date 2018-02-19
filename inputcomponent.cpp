1b936663464b76d1b324c6bc9715ed9765bec595
Stijn van Beek <stijn.v.beek@gmail.com>
2018-02-19 14:48:26 +0100

    Add InputComponent

---
+++ inputcomponent.cpp
@@ -0,0 +1,47 @@
+#include "inputcomponent.h"
+
+// Nap includes
+#include <entity.h>
+#include <nap/core.h>
+
+// Audio includes
+#include <audio/core/audionodemanager.h>
+#include <audio/service/audioservice.h>
+
+// RTTI
+RTTI_BEGIN_CLASS(nap::audio::InputComponent)
+    RTTI_PROPERTY("Channels", &nap::audio::InputComponent::mChannels, nap::rtti::EPropertyMetaData::Required)
+RTTI_END_CLASS
+
+RTTI_BEGIN_CLASS_NO_DEFAULT_CONSTRUCTOR(nap::audio::InputComponentInstance)
+    RTTI_CONSTRUCTOR(nap::EntityInstance&, nap::Component&)
+RTTI_END_CLASS
+
+namespace nap
+{
+    
+    namespace audio
+    {
+    
+        bool InputComponentInstance::init(utility::ErrorState& errorState)
+        {
+            auto resource = getComponent<InputComponent>();
+            NodeManager* nodeManager = &getEntityInstance()->getCore()->getService<AudioService>(rtti::ETypeCheck::EXACT_MATCH)->getNodeManager();
+
+            for (auto channel = 0; channel < resource->mChannels.size(); ++channel)
+            {
+                if (resource->mChannels[channel] >= nodeManager->getInputChannelCount())
+                {
+                    errorState.fail("Input channel out of bounds");
+                    return false;
+                }
+                auto node = std::make_unique<InputNode>(*nodeManager);
+                node->setInputChannel(resource->mChannels[channel]);
+                mInputNodes.emplace_back(std::move(node));
+            }
+            return true;
+        }
+        
+    }
+    
+}
