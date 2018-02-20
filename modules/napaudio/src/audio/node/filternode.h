f1ad7db13423725e9b2367161a9b3870fa942de3
Stijn van Beek <stijn.v.beek@gmail.com>
2018-02-20 13:39:57 +0100

    Comments in FilterNode

--- modules/napaudio/src/audio/node/filternode.h
+++ modules/napaudio/src/audio/node/filternode.h
@@ -8,6 +8,10 @@ namespace nap {
     
     namespace audio {
         
+        /**
+         * Multi-purpose DSP filter using the Butterworth filter algorithm to calculate biquad coefficients.
+         * Is able to apply lowpass and highpass with or without variable resonance peak and bandpass filtering.
+         */
         class NAPAPI FilterNode : public Node {
         public:
             enum class Mode { LOWPASS, HIGHPASS, BANDPASS, LOWRES, HIGHRES };
@@ -28,16 +32,56 @@ namespace nap {
              */
             OutputPin audioOutput = { this };
             
+            /**
+             * Sets the mode of the filter: lowpass, highpass, bandpass, lowpass with resonance peak or highpass with resonance peak.
+             */
             void setMode(Mode mode);
+            
+            /**
+             * Sets the frequency parameter of the filter in Hz. For the different modes this means:
+             * - Cutoff frequency for highpass and lowpass filters
+             * - Center frequency for bandpass filtering
+             */
             void setFrequency(ControllerValue cutoffFrequency);
+            
+            /**
+             * Sets the resonance peak of the filter. 0 means no resonance, 30 means self-oscillation.
+             */
             void setResonance(ControllerValue resonance);
+            
+            /**
+             * Sets the bandwith for bandpass filtering in Hz.
+             */
             void setBand(ControllerValue band);
+            
+            /**
+             * Sets the gaining factor of the filter's output.
+             */
             void setGain(ControllerValue gain);
             
+            /**
+             * Returns the mode of the filter.
+             */
             Mode getMode() const { return mMode; }
+            
+            /**
+             * @return: the cutoff frequency (for highpass and lowpass filters) or the center frequency (for bandpass filters) in Hz.
+             */
             ControllerValue getFrequency() const { return mFrequency; }
+            
+            /**
+             * Returns the resonance peak for resonating low- and highpass filtering.
+             */
             ControllerValue getResonance() const { return mResonance; }
+            
+            /**
+             * Returns the bandwidth in Hz for bandpass filtering.
+             */
             ControllerValue getBand() const { return mBand; }
+            
+            /**
+             * @return: the output gain factor of the filter.
+             */
             ControllerValue getGain() const { return mGain; }
 
         private:
@@ -49,6 +93,7 @@ namespace nap {
             ControllerValue mBand = 100.f;
             ControllerValue mGain = 1.f;
             
+            // Filter coefficients
             ControllerValue a0, a1, a2, b1, b2;
             
             Delay mOutput;
