94ee929e7a73370f66ea0f936247731658f67124
Chris Heywood <chris@theheywoods.id.au>
2018-02-28 15:01:21 +0100

    Allow for Qt directory specifying on Linux

--- cmake/qtconfig.cmake
+++ cmake/qtconfig.cmake
@@ -2,43 +2,45 @@
 macro(nap_qt_pre)
 
     ## First, let cmake know where the Qt library path is, we go from there.
-    if(MSVC OR APPLE)
-        # Pick up QT_DIR environment variable
-        if(DEFINED ENV{QT_DIR})
-            set(QTDIR $ENV{QT_DIR})
-            message(STATUS "Using QT_DIR environment variable: ${QTDIR}")
-        endif()
-
-        # Add possible Qt installation paths to the HINTS section
-        # The version probably doesn't have to match exactly (5.8.? is probably fine)
-        find_path(QT_DIR lib/cmake/Qt5/Qt5Config.cmake
-                  HINTS
-                  ${QTDIR}
-                  ${NAP_ROOT}/../Qt/5.9.1/msvc2015_64
-                  ${NAP_ROOT}/../Qt/5.9.2/msvc2015_64
-                  ${NAP_ROOT}/../Qt/5.10.0/msvc2015_64
-                  ~/Qt/5.8/clang_64
-                  )
-        # Find_package for Qt5 will pick up the Qt installation from CMAKE_PREFIX_PATH
-        set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ${QT_DIR})
-
-        if(DEFINED QT_DIR)
-            if(APPLE)
-                  # Ensure we're not using Qt from homebrew as we don't know the legal situation with packaging homebrew's packages.
-                  # Plus Qt's own opensource packages should have wider macOS version support.
-                  if(EXISTS ${QT_DIR}/INSTALL_RECEIPT.json)
-                      message(FATAL_ERROR "Homebrew's Qt packages aren't allowed due largely to a legal unknown.  Install Qt's own opensource release and point environment variable QT_DIR there.")
-                  endif()
-
-                  # TODO Ideally add stronger verification that ensures we're finding Qt's own package, or at least don't allow macports' Qt
-            endif()
-        else()
-            message(WARNING
-                    "The QT5 Directory could not be found, "
-                    "consider setting the QT_DIR environment variable "
-                    "to something like: \"C:/dev/Qt/5.9.1/msvc2015_64\"")
-        endif()
+    
+    # Pick up QT_DIR environment variable
+    if(DEFINED ENV{QT_DIR})
+        set(QTDIR $ENV{QT_DIR})
+        message(STATUS "Using QT_DIR environment variable: ${QTDIR}")
+    else()
+        message("No QT_DIR env var found")
     endif()
+
+    # Add possible Qt installation paths to the HINTS section
+    # The version probably doesn't have to match exactly (5.8.? is probably fine)
+    find_path(QT_DIR lib/cmake/Qt5/Qt5Config.cmake
+              HINTS
+              ${QTDIR}
+              ${NAP_ROOT}/../Qt/5.9.1/msvc2015_64
+              ${NAP_ROOT}/../Qt/5.9.2/msvc2015_64
+              ${NAP_ROOT}/../Qt/5.10.0/msvc2015_64
+              ~/Qt/5.8/clang_64
+              )
+    # Find_package for Qt5 will pick up the Qt installation from CMAKE_PREFIX_PATH
+    set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ${QT_DIR})
+
+    if(DEFINED QT_DIR)
+        if(APPLE)
+              # Ensure we're not using Qt from homebrew as we don't know the legal situation with packaging homebrew's packages.
+              # Plus Qt's own opensource packages should have wider macOS version support.
+              if(EXISTS ${QT_DIR}/INSTALL_RECEIPT.json)
+                  message(FATAL_ERROR "Homebrew's Qt packages aren't allowed due largely to a legal unknown.  Install Qt's own opensource release and point environment variable QT_DIR there.")
+              endif()
+
+              # TODO Ideally add stronger verification that ensures we're finding Qt's own package, or at least don't allow macports' Qt
+        endif()
+    elseif(APPLE OR MSVC)
+        message(WARNING
+                "The QT5 Directory could not be found, "
+                "consider setting the QT_DIR environment variable "
+                "to something like: \"C:/dev/Qt/5.9.1/msvc2015_64\"")
+    endif()
+
 
 
     find_package(Qt5Core REQUIRED)
