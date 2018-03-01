6141bf7d9ab4b40f5407aeea305bf4d81be6572b
Chris Heywood <chris@theheywoods.id.au>
2018-03-01 17:48:10 +0100

    - Use rebuilt libsndfile, libmpg123, portaudio and rtmidi for Linux - Get those libs through to packaged projects on Linux

--- cmake/Findrtmidi.cmake
+++ cmake/Findrtmidi.cmake
@@ -22,14 +22,11 @@ elseif(APPLE)
     set(RTMIDI_LIBRARIES_DEBUG ${RTMIDI_DIR}/bin/osx/librtmidi.a
         "-framework CoreMidi" "-framework CoreAudio" "-framework CoreFoundation")
 else()
-    set(RTMIDI_LIBRARIES_RELEASE ${RTMIDI_DIR}/bin/linux/librtmidi.so)
-    set(RTMIDI_LIBRARIES_DEBUG ${RTMIDI_DIR}/bin/linux/librtmidi.so)
+    set(RTMIDI_LIBRARIES_RELEASE ${RTMIDI_DIR}/linux/install/lib/librtmidi.so)
+    set(RTMIDI_LIBRARIES_DEBUG ${RTMIDI_DIR}/linux/install/lib/librtmidi.so)
 endif()
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set RTMIDI_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(rtmidi REQUIRED_VARS RTMIDI_INCLUDE_DIR RTMIDI_LIBRARIES_RELEASE RTMIDI_LIBRARIES_DEBUG)
-
-
-
