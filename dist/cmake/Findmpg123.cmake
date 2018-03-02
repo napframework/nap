6e78f47bdbeb8d1963a210199328ed83d0d8e783
Chris Heywood <chris@theheywoods.id.au>
2018-03-02 10:23:36 +0100

    - Package audiotest into release as a temporary rtmidi test for now - Fix compilation of app against NAP release if it uses only SDL input and not window - Assimp and mpg123 cmake fixes against NAP release after all the Linux packaging changes

--- dist/cmake/Findmpg123.cmake
+++ dist/cmake/Findmpg123.cmake
@@ -5,25 +5,16 @@ if (WIN32)
         HINTS ${THIRDPARTY_DIR}/mpg123
         )
     set(MPG123_LIBS_DIR ${LIBMPG123_DIR}/bin)
+    set(MPG123_LIBS_RELEASE_DLL ${MPG123_LIBS_DIR}/libmpg123.dll)
   	set(MPG123_LIBS ${MPG123_LIBS_DIR}/libmpg123.lib)
-  	set(MPG123_LIBS_RELEASE_DLL ${MPG123_LIBS_DIR}/libmpg123.dll)
-elseif(APPLE)
+elseif(UNIX)
     find_path(
         LIBMPG123_DIR
-        NAMES bin/libmpg123.0.dylib
-        HINTS ${THIRDPARTY_DIR}/mpg123
-        )   
-    set(MPG123_LIBS_DIR ${LIBMPG123_DIR}/bin)
-    set(MPG123_LIBS_RELEASE_DLL ${MPG123_LIBS_DIR}/libmpg123.0.dylib)
-  	set(MPG123_LIBS ${MPG123_LIBS_RELEASE_DLL})
-else()
-    find_path(
-        LIBMPG123_DIR
-        NAMES lib/libmpg123.so
+        NAMES lib/libmpg123${CMAKE_SHARED_LIBRARY_SUFFIX}
         HINTS ${THIRDPARTY_DIR}/mpg123
         )   
     set(MPG123_LIBS_DIR ${LIBMPG123_DIR}/lib)
-    set(MPG123_LIBS_RELEASE_DLL ${MPG123_LIBS_DIR}/libmpg123.so)
+    set(MPG123_LIBS_RELEASE_DLL ${MPG123_LIBS_DIR}/libmpg123${CMAKE_SHARED_LIBRARY_SUFFIX})
   	set(MPG123_LIBS ${MPG123_LIBS_RELEASE_DLL})
 endif()
 
@@ -42,7 +33,6 @@ set_target_properties(mpg123 PROPERTIES
 
 if(WIN32)
     set_target_properties(mpg123 PROPERTIES
-                          IMPORTED_IMPLIB_RELEASE ${MPG123_LIBS}
-                          IMPORTED_IMPLIB_DEBUG ${MPG123_LIBS}
+                          IMPORTED_IMPLIB ${MPG123_LIBS}
                           )
 endif()
