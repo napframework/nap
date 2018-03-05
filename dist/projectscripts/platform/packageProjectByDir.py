f2182080f0079b23585b67ac330541accdf0b49f
Chris Heywood <chris@theheywoods.id.au>
2018-03-05 13:49:59 +0100

    - Allow parameter passing through from project dir scripts to user scripts - Add option to user scripts to not wait on completion

--- dist/projectscripts/platform/packageProjectByDir.py
+++ dist/projectscripts/platform/packageProjectByDir.py
@@ -21,6 +21,15 @@ else:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("PROJECT_PATH", type=str, help="Path to the project")
+    parser.add_argument("-ns", "--no-show", action="store_true",
+                        help="Don't show the generated package")
+    parser.add_argument("-nn", "--no-napkin", action="store_true",
+                        help="Don't include napkin")
+    parser.add_argument("-nz", "--no-zip", action="store_true",
+                        help="Don't zip package")  
+    if not sys.platform in ["linux", "linux2"]:
+        parser.add_argument("-np", "--no-pause", action="store_true",
+                            help="Don't pause afterwards")
     args = parser.parse_args()
 
     project_name = os.path.basename(args.PROJECT_PATH.strip('\\'))
@@ -33,13 +42,21 @@ if __name__ == '__main__':
     else:
         python = 'python'
 
-    call([python, script_path, project_name])
+    # Build our command
+    cmd = [python, script_path, project_name]
+    if args.no_show:
+        cmd.append('--no-show')
+    if args.no_napkin:
+        cmd.append('--no-napkin')
+    if args.no_zip:
+        cmd.append('--no-zip')
+    call(cmd)
 
     # Pause to display output in case we're running from a file manager on Explorer / Finder
     # TODO Ideally work out if we're running from a terminal and don't ever pause if we are
     # TODO Discuss the possibility to only pause for input if we've hit an issue
     # TODO If we think it's a common use case that people are running this from a file manager in Linux do it for all
-    if not sys.platform.startswith('linux'):
+    if not sys.platform.startswith('linux') and not args.no_pause:
         print("Press key to close...")
 
         # Read a char from console
