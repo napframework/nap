d75d7cbed69d7cd880b1c54963c36246bfa0b9c8
Stijn van Beek <stijn.v.beek@gmail.com>
2018-03-07 18:52:53 +0100

    Comments for audio::VideoNode and audio::VideoAudioComponent

--- modules/napvideo/src/videoaudio.h
+++ modules/napvideo/src/videoaudio.h
@@ -10,22 +10,33 @@ namespace nap {
     
     namespace audio {
         
+        /**
+         * An audio node that retrieves the audiochannels from a playing video object
+         */
         class NAPAPI VideoNode : public Node {
         public:
+            /**
+             * Constructor takes the video object and the number of requested output channels.
+             */
             VideoNode(NodeManager& nodeManager, Video& video, int channelCount);
             
+            // Inherited from Node
             int getChannelCount() const { return mOutputs.size(); }
             OutputPin& getOutput(int channel) { return *mOutputs[channel]; }
             
+            /**
+             * Change the source video object
+             */
             void setVideo(Video& video) { mVideo = &video; }
             
         private:
+            // Inherited form Node
             void process() override final;
             
-            std::vector<std::unique_ptr<OutputPin>> mOutputs;
-            Video* mVideo = nullptr;
-            AudioFormat mAudioFormat;
-            std::vector<float> mDataBuffer;
+            std::vector<std::unique_ptr<OutputPin>> mOutputs; ///< The output pins for each individual channel of video audio
+            Video* mVideo = nullptr; ///< Pointer to the source video object
+            AudioFormat mAudioFormat; ///< This object tells the Video object what is our desired audio format
+            std::vector<float> mDataBuffer; ///< The buffer that is passed to the Video object to be filled with audio data
         };
                 
     }
