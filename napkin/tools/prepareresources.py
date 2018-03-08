3fdf293c621d34a85fece78873335021ffcf2513
bmod <bmodone@gmail.com>
2018-03-07 22:18:28 -0800

    - updating icon - setting icon for windows executable

--- napkin/tools/prepareresources.py
+++ napkin/tools/prepareresources.py
@@ -1,9 +1,27 @@
+import logging
 import os
+import platform
 import subprocess
 from xml.dom import minidom
 from xml.etree import ElementTree
 
 _RESOURCE_DIR = '../resources'
+_INKSCAPE = None
+_FFMPEG = None
+
+
+def findAppInWindows(appexe):
+    try:
+        import winreg
+    except ImportError:
+        import _winreg as winreg
+
+    handle = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
+                            r"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\%s" % appexe)
+
+    num_values = winreg.QueryInfoKey(handle)[1]
+    for i in range(num_values):
+        print(winreg.EnumValue(handle, i))
 
 
 def resourcePath(suffix=''):
@@ -19,7 +37,49 @@ def walkDir(d):
             yield '%s/%s' % (dirName, fname)
 
 
+def searchInFolders(folders, filename):
+    for d in folders:
+        for f in walkDir(d):
+            if os.path.basename(f).lower() == filename:
+                return f
+
+
+def getInkscapeExe():
+    global _INKSCAPE
+    if _INKSCAPE is not None:
+        return _INKSCAPE
+    if platform.system() != 'Windows':
+        _INKSCAPE = 'inkscape'
+
+    _INKSCAPE = searchInFolders([
+        'c:/program files/inkscape',
+        'c:/program files',
+        'c:/',
+    ], 'inkscape.exe')
+    return _INKSCAPE
+
+
+def getFFMpegExe():
+    global _FFMPEG
+    if _FFMPEG is not None:
+        return _FFMPEG
+    if platform.system() != 'Windows':
+        return _FFMPEG
+
+    # print(os.path.realpath())
+
+    _FFMPEG = searchInFolders({
+        '%s/../../../thirdparty/ffmpeg/bin' % os.path.dirname(__file__)
+    }, 'ffmpeg.exe')
+    return _FFMPEG
+
+
 def convertSVGtoPNG(size=128):
+    inkscape = getInkscapeExe()
+    if not inkscape:
+        logging.root.warning('Inkscape not found :(')
+        return
+
     dir = resourcePath('icons')
 
     for f in os.listdir(dir):
@@ -27,9 +87,14 @@ def convertSVGtoPNG(size=128):
             continue
         infilename = '%s/%s' % (dir, f)
         outfilename = '%s/%s.png' % (dir, os.path.splitext(f)[0])
-        print(outfilename)
+
+        # only update when necessary
+        if os.path.exists(outfilename) and os.path.getmtime(infilename) < os.path.getmtime(outfilename):
+            print('File was up to date: %s' % outfilename)
+            continue
+        print('Writing: %s' % outfilename)
         cmd = [
-            'inkscape',
+            inkscape,
             '-z',
             '-e', outfilename,
             '-w', size,
@@ -62,7 +127,7 @@ def generateQRC():
     resdir = resourcePath()
     for f in __resourceFiles():
         x = et.Element('file')
-        p = os.path.relpath(f, resdir)
+        p = os.path.relpath(f, resdir).replace(os.sep, '/')
         x.text = p
         xresource.append(x)
 
@@ -85,7 +150,8 @@ def generateHeader():
         resdir = resourcePath()
         for f in __resourceFiles():
             p = os.path.relpath(f, resdir)
-            name = p.replace(os.sep, '_').upper()
+            p = p.replace('\\', '/')
+            name = p.replace('/', '_').upper()
             name = name.replace('-', '_')
             name = os.path.splitext(name)[0]
             name = name.ljust(30, ' ')
@@ -95,7 +161,33 @@ def generateHeader():
             '} // namespace napkin\n\n'
         )
 
+
+def generateICO():
+    ffmpeg = getFFMpegExe()
+    if not ffmpeg:
+        logging.root.warning('FFMPEG not found :(')
+        return
+
+    f = 'nap-logo.png'
+    folder = resourcePath('icons')
+    infilename = '%s/%s' % (folder, f)
+    outfilename = '%s/%s.ico' % (folder, os.path.splitext(f)[0])
+    print('Writing: %s' % outfilename)
+
+    cmd = [
+        ffmpeg,
+        '-loglevel', 'panic',
+        '-y',
+        '-i', infilename,
+        outfilename,
+    ]
+    cmd = [str(c) for c in cmd]
+    subprocess.Popen(cmd).communicate()
+
+
+
 if __name__ == '__main__':
     convertSVGtoPNG()
     generateQRC()
     generateHeader()
+    generateICO()
