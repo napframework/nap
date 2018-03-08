a138374a9dbcef4481ce9eaee518a2c5cab51d34
bmod <bmodone@gmail.com>
2018-03-07 22:19:31 -0800

    - reverting accidentally overwritten file

--- generateVSSolution.bat
+++ generateVSSolution.bat
@@ -1,6 +1,6 @@
 @echo off
 mkdir msvc64
-REM cmake -H. -Bmsvc64  -G "Visual Studio 14 2015 Win64" -DPYBIND11_PYTHON_VERSION=3.5
+cmake -H. -Bmsvc64  -G "Visual Studio 14 2015 Win64" -DPYBIND11_PYTHON_VERSION=3.5
 REM If you have multiple versions of python and pybind cannot find the correct version,
 REM try this:
-cmake -H. -Bmsvc64  -G "Visual Studio 14 2015 Win64" -DPYTHON_EXECUTABLE=c:/python35/python.exe
+REM cmake -H. -Bmsvc64  -G "Visual Studio 14 2015 Win64" -DPYTHON_EXECUTABLE=c:/python35/python.exe
