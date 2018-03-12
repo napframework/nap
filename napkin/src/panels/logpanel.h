951149a3d95f5420f809cae087600882d0569a14
Bas Korsmit <bmodone@gmail.com>
2018-03-11 23:19:33 -0700

    - Added log level filter to the LogPanel - Added cornerwidget to filtertreeview to support adding extra widgets later (used by level combo in logwidget) - Saving user's current log severity level for LogPanel

--- napkin/src/panels/logpanel.h
+++ napkin/src/panels/logpanel.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <QWidget>
-#include <QtGui/QStandardItemModel>
-#include <QtWidgets/QTreeView>
-#include <QtWidgets/QVBoxLayout>
-#include <generic/filtertreeview.h>
+#include <QStandardItemModel>
+#include <QTreeView>
+#include <QVBoxLayout>
+#include <QComboBox>
+
 #include <nap/logger.h>
+
+#include "generic/filtertreeview.h"
 
 namespace napkin
 {
@@ -37,14 +40,33 @@ namespace napkin
 		Q_OBJECT
 	public:
 		explicit LogPanel();
+		const nap::LogLevel& getCurrentLevel() const;
+		void setCurrentLevel(const nap::LogLevel& level);
+		int getLevelIndex(const nap::LogLevel& level) const;
+
+	protected:
+		void closeEvent(QCloseEvent* event) override;
+		void showEvent(QShowEvent* event) override;
 
 	private:
+		void populateFilterCombo();
+		/**
+		 * Provide FilterTreeView with a way of filtering log messages based on our level
+		 */
+		bool levelFilter(const LeafFilterProxyModel& model, int sourceRow, const QModelIndex& sourceParent);
+
+		void onLevelChanged(int index);
 		void onDoubleClicked(const QModelIndex& index);
 		void onRowsAboutToBeInserted(const QModelIndex &parent, int first, int last);
 		void onRowInserted(const QModelIndex &parent, int first, int last);
 
-		FilterTreeView mTreeView; // Treeview with log entries
-		LogModel mLogModel;		  // The model containing the log entries
-		bool wasMaxScroll = true; // Whether the scroll view was at max
+		FilterTreeView mTreeView; 	// Treeview with log entries
+		QVBoxLayout mLayout;		// The main layout
+		QHBoxLayout mCornerLayout; 	// Layout at the top-right corner
+		QComboBox mFilterCombo;		// Combo containing the log levels to filter on
+		LogModel mLogModel;		  	// The model containing the log entries
+		bool wasMaxScroll = true; 	// Whether the scroll view was at max
 	};
+
+
 };
