74ab74c19ce84241b9789979c1773f9a1d86084f
Chris Heywood <chris@theheywoods.id.au>
2018-03-12 12:54:45 +0100

    Win64 clean builds fix

--- cmake/windowsdllcopier.cmake
+++ cmake/windowsdllcopier.cmake
@@ -15,6 +15,11 @@ if(NOT DEFINED COPIER_OUTPUT_PATH)
     message(FATAL_ERROR "No output path")
 endif()
 
+# Make output path if it doesn't exist yet
+if(NOT EXISTS COPIER_OUTPUT_PATH)
+    file(MAKE_DIRECTORY ${COPIER_OUTPUT_PATH})
+endif()
+
 # Iterate the files in the input path and copy
 file(GLOB FILES_TO_COPY ${COPIER_IN_PATH})
 foreach(copy_file ${FILES_TO_COPY})
