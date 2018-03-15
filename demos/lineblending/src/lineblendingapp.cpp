8857ef093a3af2114c8f219c4905b899d6d8d9e7
Coen Klosters <coen@naivi.nl>
2018-03-15 18:08:39 +0100

    initial line blending app

---
+++ demos/lineblending/src/lineblendingapp.cpp
@@ -0,0 +1,144 @@
+#include "lineblendingapp.h"
+
+// Nap includes
+#include <nap/core.h>
+#include <nap/logger.h>
+#include <renderablemeshcomponent.h>
+#include <orthocameracomponent.h>
+#include <mathutils.h>
+#include <scene.h>
+#include <perspcameracomponent.h>
+#include <inputrouter.h>
+#include <imgui/imgui.h>
+#include <triangleiterator.h>
+#include <meshutils.h>
+#include <mathutils.h>
+
+// Register this application with RTTI, this is required by the AppRunner to 
+// validate that this object is indeed an application
+RTTI_BEGIN_CLASS_NO_DEFAULT_CONSTRUCTOR(nap::LineBlendingApp)
+	RTTI_CONSTRUCTOR(nap::Core&)
+RTTI_END_CLASS
+
+namespace nap 
+{
+	/**
+	 * Initialize all the resources and store the objects we need later on
+	 */
+	bool LineBlendingApp::init(utility::ErrorState& error)
+	{
+		// Retrieve services
+		mRenderService	= getCore().getService<nap::RenderService>();
+		mSceneService	= getCore().getService<nap::SceneService>();
+		mInputService	= getCore().getService<nap::InputService>();
+		mGuiService		= getCore().getService<nap::IMGuiService>();
+
+		// Get resource manager and load
+		mResourceManager = getCore().getResourceManager();
+		if (!mResourceManager->loadFile("lineblending.json", error))
+			return false;
+
+		// Extract loaded resources
+		mRenderWindow = mResourceManager->findObject<nap::RenderWindow>("Window0");
+
+		// Find the world and camera entities
+		ObjectPtr<Scene> scene = mResourceManager->findObject<Scene>("Scene");
+
+		mLineEntity = scene->findEntity("Line");
+		mCameraEntity = scene->findEntity("Camera");
+
+		return true;
+	}
+	
+	
+	/**
+	 * Forward all the received input messages to the camera input components.
+	 * The input router is used to filter the input events and to forward them
+	 * to the input components of a set of entities, in this case our camera.
+	 * 
+	 * The camera has two input components: KeyInputComponent and PointerInputComponent
+	 * The key input component receives key events, the pointer input component receives pointer events
+	 * The orbit controller listens to both of them
+	 * When an input component receives a message it sends a signal to the orbit controller.
+	 * The orbit controller validates if it's something useful and acts accordingly,
+	 * in this case by rotating around or zooming in on the sphere.
+	 */
+	void LineBlendingApp::update(double deltaTime)
+	{
+		// The default input router forwards messages to key and mouse input components
+		// attached to a set of entities.
+		nap::DefaultInputRouter input_router;
+	}
+
+	
+	/**
+	 * Render loop is rather straight forward:
+	 * Set the camera position in the world shader for the halo effect
+	 * make the main window active, this makes sure that all subsequent render calls are 
+	 * associated with that window. When you have multiple windows and don't activate the right window subsequent
+	 * render calls could end up being associated with the wrong context, resulting in undefined behavior.
+	 * Next we clear the render target, render the object and swap the main window back-buffer.
+	 */
+	void LineBlendingApp::render()
+	{
+		// Clear opengl context related resources that are not necessary any more
+		mRenderService->destroyGLContextResources({ mRenderWindow });
+
+		// Activate current window for drawing
+		mRenderWindow->makeActive();
+
+		// Clear back-buffer
+		mRenderService->clearRenderTarget(mRenderWindow->getBackbuffer());
+
+		// Swap screen buffers
+		mRenderWindow->swap();
+	}
+	
+	
+	/**
+	 * Occurs when the event handler receives a window message.
+	 * You generally give it to the render service which in turn forwards it to the right internal window. 
+	 * On the next update the render service automatically processes all window events. 
+	 * If you want to listen to specific events associated with a window it's best to listen to a window's mWindowEvent signal
+	 */
+	void LineBlendingApp::windowMessageReceived(WindowEventPtr windowEvent)
+	{
+		mRenderService->addEvent(std::move(windowEvent));
+	}
+
+
+	/**
+	 * Called by the app loop. It's best to forward messages to the input service for further processing later on
+	 * In this case we also check if we need to toggle full-screen or exit the running app
+	 */
+	void LineBlendingApp::inputMessageReceived(InputEventPtr inputEvent)
+	{
+		// Escape the loop when esc is pressed
+		if (inputEvent->get_type().is_derived_from(RTTI_OF(nap::KeyPressEvent)))
+		{
+			nap::KeyPressEvent* press_event = static_cast<nap::KeyPressEvent*>(inputEvent.get());
+			if (press_event->mKey == nap::EKeyCode::KEY_ESCAPE)
+				quit();
+
+			// If 'f' is pressed toggle fullscreen
+			if (press_event->mKey == nap::EKeyCode::KEY_f)
+			{
+				mRenderWindow->toggleFullscreen();
+			}
+		}
+
+		mInputService->addEvent(std::move(inputEvent));
+	}
+
+	
+	void LineBlendingApp::setWindowFullscreen(std::string windowIdentifier, bool fullscreen)
+	{
+		mResourceManager->findObject<RenderWindow>(windowIdentifier)->getWindow()->setFullScreen(fullscreen);
+	}
+
+
+	int LineBlendingApp::shutdown()
+	{
+		return 0;
+	}
+}
