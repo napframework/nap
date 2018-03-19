a503df967bccda892c0dd8fb1f0dfc0d0e3c1132
Coen Klosters <coen@naivi.nl>
2018-03-19 15:18:24 +0100

    - adding yoctopuce as a module that kalvertoren depends upon - also ensures it can be packaged

---
+++ modules/napyoctopuce/dist/cmake/moduleExtra.cmake
@@ -0,0 +1,16 @@
+find_package(yoctopuce REQUIRED)
+target_link_libraries(${PROJECT_NAME} yoctopuce)
+
+if(WIN32)
+    # Add post-build step to set copy yoctopuce to bin
+    add_custom_command(TARGET ${PROJECT_NAME}
+                       POST_BUILD
+                       COMMAND ${CMAKE_COMMAND} 
+                               -E copy
+                               $<TARGET_FILE:yoctopuce>
+                               $<TARGET_FILE_DIR:${PROJECT_NAME}> 
+                       )
+elseif(UNIX)
+    # Install yoctopuce lib into packaged app
+    install(FILES $<TARGET_FILE:yoctopuce> DESTINATION lib)
+endif()
