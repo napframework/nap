06d56e8fbe0b3d73b5564fe892541488c93207e0
Chris Heywood <chris@theheywoods.id.au>
2018-03-20 17:56:51 +0100

    Further tidy up for Napkin Windows bin directory change

--- modules/napartnet/modulePostBuildPerProject.cmake
+++ modules/napartnet/modulePostBuildPerProject.cmake
@@ -1,5 +1,7 @@
 if(WIN32)
     # Copy artnet DLL to project build directory
-    find_package(artnet REQUIRED)
+    if(NOT TARGET artnet)
+        find_package(artnet REQUIRED)
+    endif()
     copy_artnet_dll()
 endif()
