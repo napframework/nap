23c34ecb1d7abe9aa382becbe1a6bf1bb376952a
Chris Heywood <chris@theheywoods.id.au>
2018-03-21 14:48:42 +0100

    Continued reverting of NAP source bin output into project subfolders

--- modules/napetherdream/modulePostBuildPerProject.cmake
+++
@@ -1,7 +0,0 @@
-if(WIN32)
-    # Copy etherdream DLL to project build directory
-    if(NOT TARGET etherdreamlib)
-        find_package(etherdream REQUIRED)
-    endif()
-    copy_etherdream_dll()
-endif()
--- modules/napvideo/modulePostBuildPerProject.cmake
+++
@@ -1,4 +0,0 @@
-if(WIN32)
-    # Copy FFmpeg DLLs to project build directory
-    copy_windows_ffmpeg_dlls()
-endif()
