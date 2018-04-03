a54393d5c23de28dcba55a566cec63fc432dce74
Ritesh Oedayrajsingh Varma <rovarma@gmail.com>
2018-04-03 14:59:01 +0200

    - Commenting for device class

--- core/src/nap/device.h
+++ core/src/nap/device.h
@@ -5,7 +5,14 @@
 namespace nap
 {
 	/**
-	 * 
+	 * A device is a special class of resource, intended to deal with cases where there are only a limited number of them allowed to be in existence at the same time.
+	 * For example, if you have a device that can only be opened by a single client, it's possible for the 'open' of the device to fail in case of real-time editing.
+	 * This is because during real-time edit, it's possible for the object being edited to (temporarily) have multiple 'instances'.
+	 *
+	 * The Device class deals with this case by providing an explicit start/stop virtual, which are called at appropriate times by the ResourceManager.
+	 * It's important that both start & stop can be called multiple times, but note that they will always be called in pairs.
+	 *
+	 * The Device is automatically stopped (by calling stop()) during destruction of the Device.
 	 */
 	class NAPAPI Device : public Resource
 	{
