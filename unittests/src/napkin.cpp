999e2310da40e7fbac5ddc2b269670fd76e71d28
Bas Korsmit <bmodone@gmail.com>
2018-04-07 19:24:35 -0700

    - re-enabling commands test

--- unittests/src/napkin.cpp
+++ unittests/src/napkin.cpp
@@ -435,110 +435,110 @@ TEST_CASE("Document Functions", TAG_NAPKIN)
 //	REQUIRE(doc->getObjects().size() == 1);
 //
 //}
-//
-//TEST_CASE("Commands", TAG_NAPKIN)
-//{
-//	auto& ctx = AppContext::get();
-//	SigCapture sigDocChanged(&ctx, &AppContext::documentChanged);
-//	int sigDocCount = 0;
-//	REQUIRE(sigDocChanged.count() == sigDocCount);
-//
-//	auto loadeddoc = ctx.loadDocument("unit_tests_data/objects.json");
-//	{
-//		REQUIRE(loadeddoc);
-//		REQUIRE(sigDocChanged.count() == ++sigDocCount);
-//		auto object = loadeddoc->getObject("material");
-//
-//	}
-//
-//	auto doc = ctx.newDocument();
-//	REQUIRE(sigDocChanged.count() == ++sigDocCount);
-//
-//	// Add an object and verify
-//	ctx.executeCommand(new AddObjectCommand(RTTI_OF(nap::Entity)));
-//	REQUIRE(sigDocChanged.count() == ++sigDocCount);
+
+TEST_CASE("Commands", TAG_NAPKIN)
+{
+	auto& ctx = AppContext::get();
+	SigCapture sigDocChanged(&ctx, &AppContext::documentChanged);
+	int sigDocCount = 0;
+	REQUIRE(sigDocChanged.count() == sigDocCount);
+
+	auto loadeddoc = ctx.loadDocument("unit_tests_data/objects.json");
+	{
+		REQUIRE(loadeddoc);
+		REQUIRE(sigDocChanged.count() == ++sigDocCount);
+		auto object = loadeddoc->getObject("material");
+
+	}
+
+	auto doc = ctx.newDocument();
+	REQUIRE(sigDocChanged.count() == ++sigDocCount);
+
+	// Add an object and verify
+	ctx.executeCommand(new AddObjectCommand(RTTI_OF(nap::Entity)));
+	REQUIRE(sigDocChanged.count() == ++sigDocCount);
+	REQUIRE(doc->getObjects().size() == 1);
+	{
+		doc->undo();
+		REQUIRE(sigDocChanged.count() == ++sigDocCount);
+		REQUIRE(doc->getObjects().size() == 0);
+		doc->redo();
+		REQUIRE(sigDocChanged.count() == ++sigDocCount);
+		REQUIRE(doc->getObjects().size() == 1);
+	}
+
+	nap::rtti::Object* entity1 = doc->getObjects()[0].get();
+	nap::Entity* e1 = rtti_cast<nap::Entity>(entity1);
+	REQUIRE(e1 != nullptr);
+
+	// Add another one and verify
+	ctx.executeCommand(new AddObjectCommand(RTTI_OF(nap::Entity), entity1));
+	REQUIRE(sigDocChanged.count() == ++sigDocCount);
+	REQUIRE(doc->getObjects().size() == 2);
+	{
+		doc->undo();
+		REQUIRE(sigDocChanged.count() == ++sigDocCount);
+		REQUIRE(doc->getObjects().size() == 1);
+		doc->redo();
+		REQUIRE(sigDocChanged.count() == ++sigDocCount);
+		REQUIRE(doc->getObjects().size() == 2);
+	}
+
+	nap::rtti::Object* entity2 = doc->getObjects()[1].get();
+	nap::Entity* e2 = rtti_cast<nap::Entity>(entity2);
+	REQUIRE(e2 != nullptr);
+	REQUIRE(doc->getParent(*e2) == e1);
+
+	PropertyPath nameProp1(*entity1, nap::rtti::sIDPropertyName);
+	REQUIRE(nameProp1.isValid());
+	PropertyPath nameProp2(*entity2, nap::rtti::sIDPropertyName);
+	REQUIRE(nameProp2.isValid());
+
+	ctx.executeCommand(new SetValueCommand(nameProp1, "Loco"));
+	REQUIRE(sigDocChanged.count() == ++sigDocCount);
+	REQUIRE(entity1->mID == "Loco");
+
+	// Name may not be empty
+	ctx.executeCommand(new SetValueCommand(nameProp1, ""));
+	REQUIRE(sigDocChanged.count() == ++sigDocCount);
+	REQUIRE(entity1->mID == "Loco");
+
+	// No duplicate names
+	ctx.executeCommand(new SetValueCommand(nameProp2, "Loco"));
+	REQUIRE(sigDocChanged.count() == ++sigDocCount);
+	REQUIRE(entity2->mID != "Loco");
+
+	// Setting a unique name should succeed
+	ctx.executeCommand(new SetValueCommand(nameProp2, "Motion"));
+	REQUIRE(sigDocChanged.count() == ++sigDocCount);
+	REQUIRE(entity2->mID == "Motion");
+
+	// Remove object and verify
+	ctx.executeCommand(new DeleteObjectCommand(*entity1));
+	REQUIRE(sigDocChanged.count() == ++sigDocCount);
+	REQUIRE(doc->getObjects().size() == 1);
+
+	// Remove next and verify
+	ctx.executeCommand(new DeleteObjectCommand(*entity2));
+	REQUIRE(sigDocChanged.count() == ++sigDocCount);
+	REQUIRE(doc->getObjects().size() == 0);
+
+	// Add a component
+	auto& entity = doc->addEntity();
+	ctx.executeCommand(new AddComponentCommand(entity, RTTI_OF(nap::PerspCameraComponent)));
+	REQUIRE(entity.hasComponent<nap::PerspCameraComponent>());
+
+	// TODO: Support undo for deletion
+//	doc->undo();
 //	REQUIRE(doc->getObjects().size() == 1);
-//	{
-//		doc->undo();
-//		REQUIRE(sigDocChanged.count() == ++sigDocCount);
-//		REQUIRE(doc->getObjects().size() == 0);
-//		doc->redo();
-//		REQUIRE(sigDocChanged.count() == ++sigDocCount);
-//		REQUIRE(doc->getObjects().size() == 1);
-//	}
 //
-//	nap::rtti::Object* entity1 = doc->getObjects()[0].get();
-//	nap::Entity* e1 = rtti_cast<nap::Entity>(entity1);
-//	REQUIRE(e1 != nullptr);
-//
-//	// Add another one and verify
-//	ctx.executeCommand(new AddObjectCommand(RTTI_OF(nap::Entity), entity1));
-//	REQUIRE(sigDocChanged.count() == ++sigDocCount);
+//	doc->undo();
 //	REQUIRE(doc->getObjects().size() == 2);
-//	{
-//		doc->undo();
-//		REQUIRE(sigDocChanged.count() == ++sigDocCount);
-//		REQUIRE(doc->getObjects().size() == 1);
-//		doc->redo();
-//		REQUIRE(sigDocChanged.count() == ++sigDocCount);
-//		REQUIRE(doc->getObjects().size() == 2);
-//	}
-//
-//	nap::rtti::Object* entity2 = doc->getObjects()[1].get();
-//	nap::Entity* e2 = rtti_cast<nap::Entity>(entity2);
-//	REQUIRE(e2 != nullptr);
-//	REQUIRE(doc->getParent(*e2) == e1);
-//
-//	PropertyPath nameProp1(*entity1, nap::rtti::sIDPropertyName);
-//	REQUIRE(nameProp1.isValid());
-//	PropertyPath nameProp2(*entity2, nap::rtti::sIDPropertyName);
-//	REQUIRE(nameProp2.isValid());
-//
-//	ctx.executeCommand(new SetValueCommand(nameProp1, "Loco"));
-//	REQUIRE(sigDocChanged.count() == ++sigDocCount);
-//	REQUIRE(entity1->mID == "Loco");
-//
-//	// Name may not be empty
-//	ctx.executeCommand(new SetValueCommand(nameProp1, ""));
-//	REQUIRE(sigDocChanged.count() == ++sigDocCount);
-//	REQUIRE(entity1->mID == "Loco");
-//
-//	// No duplicate names
-//	ctx.executeCommand(new SetValueCommand(nameProp2, "Loco"));
-//	REQUIRE(sigDocChanged.count() == ++sigDocCount);
-//	REQUIRE(entity2->mID != "Loco");
-//
-//	// Setting a unique name should succeed
-//	ctx.executeCommand(new SetValueCommand(nameProp2, "Motion"));
-//	REQUIRE(sigDocChanged.count() == ++sigDocCount);
-//	REQUIRE(entity2->mID == "Motion");
-//
-//	// Remove object and verify
-//	ctx.executeCommand(new DeleteObjectCommand(*entity1));
-//	REQUIRE(sigDocChanged.count() == ++sigDocCount);
-//	REQUIRE(doc->getObjects().size() == 1);
-//
-//	// Remove next and verify
-//	ctx.executeCommand(new DeleteObjectCommand(*entity2));
-//	REQUIRE(sigDocChanged.count() == ++sigDocCount);
-//	REQUIRE(doc->getObjects().size() == 0);
-//
-//	// Add a component
-//	auto& entity = doc->addEntity();
-//	ctx.executeCommand(new AddComponentCommand(entity, RTTI_OF(nap::PerspCameraComponent)));
-//	REQUIRE(entity.hasComponent<nap::PerspCameraComponent>());
-//
-//	// TODO: Support undo for deletion
-////	doc->undo();
-////	REQUIRE(doc->getObjects().size() == 1);
-////
-////	doc->undo();
-////	REQUIRE(doc->getObjects().size() == 2);
-//
-////	ctx.executeCommand(new SetPointerValueCommand())
-////	ctx.executeCommand(new AddEntityToSceneCommand())
-////	ctx.executeCommand(new ArrayAddValueCommand());
-//}
+
+//	ctx.executeCommand(new SetPointerValueCommand())
+//	ctx.executeCommand(new AddEntityToSceneCommand())
+//	ctx.executeCommand(new ArrayAddValueCommand());
+}
 
 TEST_CASE("File Extensions", TAG_NAPKIN)
 {
