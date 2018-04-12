9937e2b29f75ad0a693a20bcb3859783349a0aad
Stijn van Beek <stijn.v.beek@gmail.com>
2018-04-12 18:49:22 +0200

    OscMidi demo app setup

---
+++ demos/oscmidi/src/oscmidi.h
@@ -0,0 +1,67 @@
+#pragma once
+
+// Mod nap render includes
+#include <renderwindow.h>
+
+// Nap includes
+#include <nap/resourcemanager.h>
+#include <sceneservice.h>
+#include <inputservice.h>
+#include <imguiservice.h>
+#include <app.h>
+
+namespace nap
+{
+	using namespace rtti;
+
+	/**
+	 * Demo application that is called from within the main loop
+	 *
+	 */
+	class OscMidiApp : public App
+	{
+		RTTI_ENABLE(App)
+	public:
+		OscMidiApp(nap::Core& core) : App(core)	{ }
+
+		/**
+		 *	Initialize app specific data structures
+		 */
+		bool init(utility::ErrorState& error) override;
+		
+		/**
+		 *	Update is called before render, performs all the app logic
+		 */
+		void update(double deltaTime) override;
+
+		/**
+		 *	Render is called after update, pushes all renderable objects to the GPU
+		 */
+		void render() override;
+
+		/**
+		 *	Forwards the received window event to the render service
+		 */
+		void windowMessageReceived(WindowEventPtr windowEvent) override;
+		
+		/**
+		 *  Forwards the received input event to the input service
+		 */
+		void inputMessageReceived(InputEventPtr inputEvent) override;
+		
+		/**
+		 *	Called when loop finishes
+		 */
+		int shutdown() override;
+
+	private:
+		// Nap Services
+		RenderService* mRenderService = nullptr;						//< Render Service that handles render calls
+		ResourceManager* mResourceManager = nullptr;					//< Manages all the loaded resources
+		SceneService* mSceneService = nullptr;							//< Manages all the objects in the scene
+		InputService* mInputService = nullptr;							//< Input service for processing input
+		IMGuiService* mGuiService = nullptr;							//< Manages gui related update / draw calls
+		ObjectPtr<RenderWindow> mRenderWindow;							//< Pointer to the render window		
+		RGBAColor8 mTextHighlightColor = { 0xC8, 0x69, 0x69, 0xFF };	//< GUI text highlight color
+	};
+}
