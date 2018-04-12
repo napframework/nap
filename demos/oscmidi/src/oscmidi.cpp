65b8873928fa78b2a0dc9195a6054e854175d4b8
Stijn van Beek <stijn.v.beek@gmail.com>
2018-04-12 18:49:22 +0200

    OscMidi demo app setup

---
+++ demos/oscmidi/src/oscmidi.cpp
@@ -0,0 +1,124 @@
+#include "oscmidi.h"
+
+// Nap includes
+#include <nap/core.h>
+#include <nap/logger.h>
+#include <mathutils.h>
+#include <scene.h>
+#include <inputrouter.h>
+#include <imgui/imgui.h>
+
+// Register this application with RTTI, this is required by the AppRunner to 
+// validate that this object is indeed an application
+RTTI_BEGIN_CLASS_NO_DEFAULT_CONSTRUCTOR(nap::OscMidiApp)
+	RTTI_CONSTRUCTOR(nap::Core&)
+RTTI_END_CLASS
+
+namespace nap 
+{
+	/**
+	 * Initialize all the resources and store the objects we need later on
+	 */
+	bool OscMidiApp::init(utility::ErrorState& error)
+	{
+		// Retrieve services
+		mRenderService	= getCore().getService<nap::RenderService>();
+		mSceneService	= getCore().getService<nap::SceneService>();
+		mInputService	= getCore().getService<nap::InputService>();
+		mGuiService		= getCore().getService<nap::IMGuiService>();
+
+		// Get resource manager and load
+		mResourceManager = getCore().getResourceManager();
+		if (!mResourceManager->loadFile("oscmidi.json", error))
+			return false;
+
+		// Extract loaded resources
+		mRenderWindow = mResourceManager->findObject<nap::RenderWindow>("Window0");
+
+		// Find the world and camera entities
+		ObjectPtr<Scene> scene = mResourceManager->findObject<Scene>("Scene");
+
+		return true;
+	}
+	
+	
+	/**
+	 */
+	void OscMidiApp::update(double deltaTime)
+	{
+		// The default input router forwards messages to key and mouse input components
+		// attached to a set of entities.
+		nap::DefaultInputRouter input_router;
+		
+		// Forward all input events associated with the first window to the listening components
+//        std::vector<nap::EntityInstance*> entities = { mCameraEntity.get() };
+//        mInputService->processEvents(*mRenderWindow, input_router, entities);
+
+		// Draw some gui elements
+		ImGui::Begin("Controls");
+		ImGui::End();
+	}
+
+	
+	/**
+	 */
+	void OscMidiApp::render()
+	{
+		// Clear opengl context related resources that are not necessary any more
+		mRenderService->destroyGLContextResources({ mRenderWindow });
+
+		// Activate current window for drawing
+		mRenderWindow->makeActive();
+
+		// Clear back-buffer
+		mRenderService->clearRenderTarget(mRenderWindow->getBackbuffer());
+
+		// Draw our gui
+		mGuiService->draw();
+
+		// Swap screen buffers
+		mRenderWindow->swap();
+	}
+	
+	
+	/**
+	 * Occurs when the event handler receives a window message.
+	 * You generally give it to the render service which in turn forwards it to the right internal window. 
+	 * On the next update the render service automatically processes all window events. 
+	 * If you want to listen to specific events associated with a window it's best to listen to a window's mWindowEvent signal
+	 */
+	void OscMidiApp::windowMessageReceived(WindowEventPtr windowEvent)
+	{
+		mRenderService->addEvent(std::move(windowEvent));
+	}
+
+
+	/**
+	 * Called by the app loop. It's best to forward messages to the input service for further processing later on
+	 * In this case we also check if we need to toggle full-screen or exit the running app
+	 */
+	void OscMidiApp::inputMessageReceived(InputEventPtr inputEvent)
+	{
+		// Escape the loop when esc is pressed
+		if (inputEvent->get_type().is_derived_from(RTTI_OF(nap::KeyPressEvent)))
+		{
+			nap::KeyPressEvent* press_event = static_cast<nap::KeyPressEvent*>(inputEvent.get());
+			if (press_event->mKey == nap::EKeyCode::KEY_ESCAPE)
+				quit();
+
+			// If 'f' is pressed toggle fullscreen
+			if (press_event->mKey == nap::EKeyCode::KEY_f)
+			{
+				mRenderWindow->toggleFullscreen();
+			}
+		}
+
+		mInputService->addEvent(std::move(inputEvent));
+	}
+
+
+	int OscMidiApp::shutdown()
+	{
+		return 0;
+	}
+}
