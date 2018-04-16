04fa84dcbde84fe36d2bc079551f3a612cfb886b
Chris Heywood <chris@theheywoods.id.au>
2018-04-16 16:49:00 +0200

    rtmidi packaging fixes (for issues detected in Kalvertoren)

--- modules/napmidi/dist/cmake/moduleExtra.cmake
+++ modules/napmidi/dist/cmake/moduleExtra.cmake
@@ -1,3 +1,7 @@
+find_package(rtmidi REQUIRED)
+target_link_libraries(${PROJECT_NAME} rtmidi)
+target_include_directories(${PROJECT_NAME} PUBLIC ${RTMIDI_INCLUDE_DIRS})
+
 if(UNIX AND NOT APPLE)
     # Install rtmidi lib into packaged app
     file(GLOB RTMIDI_DYLIBS ${THIRDPARTY_DIR}/rtmidi/lib/librt*${CMAKE_SHARED_LIBRARY_SUFFIX}*)
