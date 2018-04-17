d8a714c921ec1fd9f44a5569fa6dac4e54872cd4
jellevdbeek <jelle@presence-vr.net>
2018-04-17 17:32:14 +0200

    Moved platform specific module load functions into platform files

--- core/src/nap/modulemanager.cpp
+++ core/src/nap/modulemanager.cpp
@@ -2,117 +2,17 @@
 #include "modulemanager.h"
 #include "logger.h"
 #include "service.h"
-
-#ifdef _WIN32
-	#define WIN32_LEAN_AND_MEAN
-	#include <windows.h> // Windows dll loading
-#else
-	#include <dlfcn.h> // Posix shared object loading
-#endif
+#include "module.h"
 
 // External Includes
 #include <utility/fileutils.h>
 #include <packaginginfo.h>
 
-namespace
-{
-	// Extension for shared libraries (dependent on platform)
-#ifdef _WIN32
-	static std::string sharedLibExtension = "dll";
-#elif __APPLE__
-	static std::string sharedLibExtension = "dylib";
-#else
-	static std::string sharedLibExtension = "so";
-#endif
-
-
-	/**
-	 * Platform-wrapper function to get the error string for loading a module (if an error was thrown)
-	 * @return The error string
-	 */
-	std::string GetModuleLoadErrrorString()
-	{
-#ifdef _WIN32
-		// Get the error code
-		DWORD error_code = ::GetLastError();
-		if (error_code == 0)
-			return std::string();
-
-		LPSTR messageBuffer = nullptr;
-		size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-			NULL, error_code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
-
-		std::string message(messageBuffer, size);
-		LocalFree(messageBuffer);
-
-		return message;
-#else
-		return dlerror();
-#endif
-	}
-
-
-	/**
-	 * Platform-wrapper function to load a shared library
-	 * @param modulePath The path to the shared library to load
-	 * @param errorString If the module failed to load, the error string
-	 * @return Handle to the loaded module
-	 */
-	void* LoadModule(const std::string& modulePath, std::string& errorString)
-	{
-		void* result;
-#ifdef _WIN32
-		result = LoadLibraryA(modulePath.c_str());
-#else
-		result = dlopen(modulePath.c_str(), RTLD_LAZY);
-#endif
-
-		// If we failed to load the module, get the error string
-		if (!result)
-			errorString = GetModuleLoadErrrorString();
-
-		return result;
-	}
-
-
-	/**
-	 * Platform-wrapper function to unload a shared library
-	 * @param module The module to load
-	 */
-	void UnloadModule(void* module)
-	{
-#ifdef _WIN32
-		FreeLibrary((HMODULE)module);
-#else
-		dlclose(module);
-#endif
-	}
-
-
-	/**
-	 * Platform-wrapper function to find the address of a symbol in the specified shared library
-	 * @param module Handle to the module to find the symbol in
-	 * @param symbolName Name of the symbol to find
-	 * @return The pointer to the symbol if found, nullptr if not
-	 */
-	void* FindSymbol(void* module, const char* symbolName)
-	{
-#ifdef _WIN32
-		return GetProcAddress((HMODULE)module, symbolName);
-#else
-		return dlsym(module, symbolName);
-#endif
-	}
-}
-
 namespace nap
 {
 	ModuleManager::ModuleManager()
 	{
-#ifdef _WIN32
-		// On windows, disable DLL load failure dialog boxes (we handle the errors ourselves))
-		SetErrorMode(SEM_FAILCRITICALERRORS);
-#endif
+		initModules();
 	}
 
 	ModuleManager::~ModuleManager()
@@ -159,17 +59,14 @@ namespace nap
 				if (utility::dirExists(filename))
 					continue;
 
+
 				// Ignore non-shared libraries
-				if (utility::getFileExtension(filename) != sharedLibExtension)
+				if (!isModule(filename))
 					continue;
 				
 				// Skip unrequested modules if we've been specified a list to load
 				// First get our module name from the filename, removing 'lib' prefix on *nix
-				std::string moduleName = utility::getFileNameWithoutExtension(filename);
-#ifndef _WIN32
-				assert(moduleName.substr(0, 3) == "lib");
-				moduleName = moduleName.substr(3, std::string::npos);
-#endif
+				std::string moduleName = getModuleNameFromPath(filename);
 				if (loadSpecificModules && std::find(remainingModulesToLoad.begin(), remainingModulesToLoad.end(), moduleName) == remainingModulesToLoad.end())
 					continue;
 
@@ -177,7 +74,7 @@ namespace nap
 
 				// Try to load the module
 				std::string error_string;
-				void* module_handle = LoadModule(module_path, error_string);
+				void* module_handle = loadModule(module_path, error_string);
 				if (!module_handle)
 				{
 					Logger::warn("Failed to load module %s: %s", module_path.c_str(), error_string.c_str());
@@ -185,10 +82,10 @@ namespace nap
 				}
 
 				// Find descriptor. If the descriptor wasn't found in the dll, assume it's not actually a nap module and unload it again.
-				ModuleDescriptor* descriptor = (ModuleDescriptor*)FindSymbol(module_handle, "descriptor");
+				ModuleDescriptor* descriptor = (ModuleDescriptor*)findSymbolInModule(module_handle, "descriptor");
 				if (descriptor == nullptr)
 				{
-					UnloadModule(module_handle);
+					unloadModule(module_handle);
 					continue;
 				}
 
@@ -196,7 +93,7 @@ namespace nap
 				if (descriptor->mAPIVersion != ModuleDescriptor::ModuleAPIVersion)
 				{
 					Logger::warn("Module %s was built against a different version of NAP (found %d, expected %d); skipping.", module_path.c_str(), descriptor->mAPIVersion, ModuleDescriptor::ModuleAPIVersion);
-					UnloadModule(module_handle);
+					unloadModule(module_handle);
 					continue;
 				}
 
@@ -207,7 +104,7 @@ namespace nap
 					if (!stype.is_derived_from(RTTI_OF(Service)))
 					{
 						Logger::warn("Module %s service descriptor %s is not a service; skipping", module_path.c_str(), descriptor->mService);
-						UnloadModule(module_handle);
+						unloadModule(module_handle);
 						continue;
 					}
 					service = stype;
