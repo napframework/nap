9a679a810c46c99576c5b8eabe702334533e4c21
Chris Heywood <chris@theheywoods.id.au>
2018-04-21 13:02:08 +0200

    First pass at C++ part of module inter-dependencies loading from JSON

--- core/src/nap/modulemanager.cpp
+++ core/src/nap/modulemanager.cpp
@@ -7,6 +7,9 @@
 // External Includes
 #include <utility/fileutils.h>
 #include <packaginginfo.h>
+#include <rapidjson/document.h>
+#include <rapidjson/error/en.h>
+#include <fstream>
 
 namespace nap
 {
@@ -37,9 +40,14 @@ namespace nap
 
 		// Whether we're loading a specific set of requested modules
 		const bool loadSpecificModules = moduleNames.size() != 0;
-		
+
 		// Track which modules remain to be loaded
-		std::vector<std::string> remainingModulesToLoad = moduleNames;
+		std::vector<std::string> remainingModulesToLoad;
+		if (loadSpecificModules)
+		{
+			if (!fetchProjectModuleDependencies(moduleNames, remainingModulesToLoad, error))
+				return false;
+		}
 		
 		// Iterate each directory
 		for (const auto& directory : directories) {
@@ -59,7 +67,6 @@ namespace nap
 				if (utility::dirExists(filename))
 					continue;
 
-
 				// Ignore non-shared libraries
 				if (!isModule(filename))
 					continue;
@@ -71,7 +78,7 @@ namespace nap
 					continue;
 
 				std::string module_path = utility::getAbsolutePath(filename);
-
+				
 				// Try to load the module
 				std::string error_string;
 				void* module_handle = loadModule(module_path, error_string);
@@ -289,5 +296,167 @@ namespace nap
 		
 		// Project module
 		outSearchDirectories.push_back(exeDir + "/../../module/lib/" + buildType);
+	}
+	
+	bool ModuleManager::loadModuleDependenciesFromJSON(std::string& jsonFile, std::vector<std::string>& dependencies, utility::ErrorState& errorState)
+	{
+		// Check for our project.json in its normal location, beside the binary
+		std::string projectInfoToRead;
+		if (!utility::fileExists(jsonFile)) {
+			errorState.fail("Couldn't load from " + jsonFile);
+			return false;
+		}
+		
+		// Open the file
+		std::ifstream in(jsonFile, std::ios::in | std::ios::binary);
+		if (!errorState.check(in.good(), "Unable to open file %s", jsonFile.c_str()))
+			return false;
+		
+		// Create buffer of appropriate size
+		in.seekg(0, std::ios::end);
+		size_t len = in.tellg();
+		std::string buffer;
+		buffer.resize(len);
+		
+		// Read all data
+		in.seekg(0, std::ios::beg);
+		in.read(&buffer[0], len);
+		in.close();
+		
+		//if (!deserializeProjectInfoJSON(buffer, result, errorState))
+
+		// Try to parse the json file
+		rapidjson::Document document;
+		rapidjson::ParseResult parse_result = document.Parse(buffer.c_str());
+		if (!parse_result)
+		{
+			errorState.fail("Error parsing json: %s", rapidjson::GetParseError_En(parse_result.Code()));
+			return false;
+		}
+		
+		// Basic validation
+		rapidjson::Value::ConstMemberIterator dependenciesElement = document.FindMember("dependencies");
+		if (!errorState.check(dependenciesElement != document.MemberEnd(), "Unable to find required 'dependencies' module info field"))
+			return false;
+		
+		if (!errorState.check(dependenciesElement->value.IsArray(), "'dependencies' module info field must be an array"))
+			return false;
+		
+		for (std::size_t index = 0; index < dependenciesElement->value.Size(); ++index)
+		{
+			const rapidjson::Value& json_element = dependenciesElement->value[index];
+			if (!errorState.check(json_element.IsString(), "Entries in 'dependencies' array in module info field must be a strings"))
+				return false;
+			
+			dependencies.push_back(json_element.GetString());
+		}
+		
+		return true;
+	}
+
+	
+	bool ModuleManager::fetchProjectModuleDependencies(std::vector<std::string>& topLevelProjectModules, std::vector<std::string>& outDependentModules, utility::ErrorState& errorState)
+	{
+		// Take the top level modules from project.json as the first modules to work on
+		std::vector<std::string> newModules = topLevelProjectModules;
+		
+		// Work until we find no new dependencies in a loop
+		std::vector<std::string> searchModules;
+		while (!newModules.empty())
+		{
+			// Add our new dependencies to our master list
+			outDependentModules.insert(outDependentModules.end(), newModules.begin(), newModules.end());
+			
+			// Fetch any new dependencies for the dependencies found in the last loop
+			searchModules = newModules;
+			newModules.clear();
+			if (!fetchModuleDependenciesForModules(searchModules, outDependentModules, newModules, errorState))
+				return false;
+		}
+		
+		// Add our new dependencies to our master list
+		outDependentModules.insert(outDependentModules.end(), newModules.begin(), newModules.end());
+		
+		return true;
+	}
+
+	
+	bool ModuleManager::fetchModuleDependenciesForModules(std::vector<std::string>& searchModules, std::vector<std::string>& totalModules, std::vector<std::string>& outFoundDependencies, utility::ErrorState& errorState)
+	{
+		std::vector<std::string> directories;
+		if (!buildModuleSearchDirectories(searchModules, directories, errorState))
+			return false;
+		
+		std::vector<std::string> remainingModulesToFind = searchModules;
+		
+		for (const auto& directory : directories) {
+			// Skip directory if it doesn't exist
+			if (!utility::dirExists(directory))
+				continue;
+			
+			// Find all files in the specified directory
+			std::vector<std::string> files_in_directory;
+			utility::listDir(directory.c_str(), files_in_directory);
+			
+			for (const auto& filename : files_in_directory)
+			{
+				// Ignore directories
+				if (utility::dirExists(filename))
+					continue;
+				
+				// Ignore non-shared libraries
+				if (!isModule(filename))
+					continue;
+				
+				// Skip any modules we come across that aren't the ones we're after
+				std::string moduleName = getModuleNameFromPath(filename);
+				if (std::find(remainingModulesToFind.begin(), remainingModulesToFind.end(), moduleName) == remainingModulesToFind.end())
+					continue;
+				
+				std::string modulePath = utility::getAbsolutePath(filename);
+				
+				std::string jsonFile = utility::getFileDir(modulePath) + "/" + utility::getFileNameWithoutExtension(modulePath);
+				jsonFile = jsonFile + ".json";
+
+				std::vector<std::string> dependencies;
+				if (!loadModuleDependenciesFromJSON(jsonFile, dependencies, errorState))
+					return false;
+
+				// Iterate each dependency for this module
+				for (const auto& dependencyModule : dependencies)
+				{
+					// Check if we already had this dependency before listing dependencies for this module
+					bool hadModulePreviously = std::find(totalModules.begin(), totalModules.end(), dependencyModule) != totalModules.end();
+
+					// Check if we already had added this dependency as a new dependency
+					bool foundModuleRecently = std::find(outFoundDependencies.begin(), outFoundDependencies.end(), dependencyModule) != outFoundDependencies.end();
+					
+					// If we've found a new dependency add it to the list
+					if (!hadModulePreviously && !foundModuleRecently)
+						outFoundDependencies.push_back(dependencyModule);
+				}
+				
+				// Remove our handled module from the remaining modules to load dependencies for
+				std::vector<std::string>::iterator position = std::find(remainingModulesToFind.begin(), remainingModulesToFind.end(), moduleName);
+				if (position != remainingModulesToFind.end())
+					remainingModulesToFind.erase(position);
+			}
+			
+			// At least break our outer loop if we've already loaded dependencies for the requested modules
+			if (remainingModulesToFind.empty())
+				break;
+		}
+		
+		// If we couldn't load dependencies for all modules error and fail
+		if (!remainingModulesToFind.empty())
+		{
+			std::string errorModulesToLog;
+			for (const auto &errorModule : remainingModulesToFind)
+				errorModulesToLog += errorModule + " ";
+			errorState.fail("Couldn't load modules dependencies for: " + errorModulesToLog);
+			return false;
+		}
+	
+		return true;
 	}
 }
