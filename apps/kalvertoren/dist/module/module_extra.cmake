d7187e3ea9679f828e5e4b567fc2e342c15c2ad2
Chris Heywood <chris@theheywoods.id.au>
2018-04-22 13:59:08 +0200

    Remove now unused module dependency lists for project modules

--- apps/kalvertoren/dist/module/module_extra.cmake
+++
@@ -1,1 +0,0 @@
-set(DEPENDENT_MODULES mod_naprender mod_napmath mod_napsdlinput mod_napsdlwindow mod_napcameracontrol mod_napapp mod_napartnet mod_napimgui mod_napscene mod_napyoctopuce mod_napvideo mod_napaudio mod_napmidi)
--- demos/videomodulation/dist/module/module_extra.cmake
+++
@@ -1,2 +0,0 @@
-# Temporary, until we have module configuration
-set(DEPENDENT_MODULES mod_napvideo mod_naprender mod_napscene mod_napmath mod_napaudio)
