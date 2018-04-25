1f5acdc0df03dca882e68dc9882007af37d88663
Chris Heywood <chris@theheywoods.id.au>
2018-04-25 13:32:17 +0200

    Fix some oscpack issues affecting packaged Win64

--- modules/naposc/dist/cmake/module_extra.cmake
+++ modules/naposc/dist/cmake/module_extra.cmake
@@ -1,4 +1,5 @@
 find_package(oscpack REQUIRED)
+target_link_libraries(${PROJECT_NAME} debug ${OSCPACK_LIBS_DEBUG} optimized ${OSCPACK_LIBS_RELEASE})
 target_include_directories(${PROJECT_NAME} PUBLIC ${OSCPACK_INCLUDE_DIRS})
 
 # Install oscpack licenses into packaged project
