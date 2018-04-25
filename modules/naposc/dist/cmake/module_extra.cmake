32eca6be0fb7e08911aade9b163d5508c082d268
Chris Heywood <chris@theheywoods.id.au>
2018-04-25 22:51:29 +0200

    Final fixes for issues introduced through NAP framework-level CMake changes this afternoon/evening

--- modules/naposc/dist/cmake/module_extra.cmake
+++ modules/naposc/dist/cmake/module_extra.cmake
@@ -1,5 +1,9 @@
 find_package(oscpack REQUIRED)
-target_link_libraries(${PROJECT_NAME} oscpack)
+if(WIN32)
+    target_link_libraries(${PROJECT_NAME} debug ${OSCPACK_LIBS_DEBUG} optimized ${OSCPACK_LIBS_RELEASE})
+else()
+    target_link_libraries(${PROJECT_NAME} oscpack)
+endif()
 target_include_directories(${PROJECT_NAME} PUBLIC ${OSCPACK_INCLUDE_DIRS})
 
 # Install oscpack licenses into packaged project
@@ -9,6 +13,6 @@ install(FILES ${THIRDPARTY_DIR}/oscpack/LICENSE DESTINATION licenses/oscpack)
 if(APPLE)
     install(FILES $<TARGET_FILE:oscpack> DESTINATION lib)    
 elseif(UNIX)
-    file(GLOB OSCPACK_DYLIBS ${THIRDPARTY_DIR}/oscpack/lib/liboscpack*${CMAKE_SHARED_LIBRARY_SUFFIX})
+    file(GLOB OSCPACK_DYLIBS ${THIRDPARTY_DIR}/oscpack/lib/liboscpack*${CMAKE_SHARED_LIBRARY_SUFFIX}*)
     install(FILES ${OSCPACK_DYLIBS} DESTINATION lib)
 endif()
