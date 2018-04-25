0e47c8c7c63d6ab93aab616f26133a99cc28cf21
Chris Heywood <chris@theheywoods.id.au>
2018-04-25 19:25:40 +0200

    Use oscpack shared lib from thirdparty on Unix

--- modules/naposc/dist/cmake/module_extra.cmake
+++ modules/naposc/dist/cmake/module_extra.cmake
@@ -4,3 +4,8 @@ target_include_directories(${PROJECT_NAME} PUBLIC ${OSCPACK_INCLUDE_DIRS})
 
 # Install oscpack licenses into packaged project
 install(FILES ${THIRDPARTY_DIR}/oscpack/LICENSE DESTINATION licenses/oscpack)
+
+# Install oscpack shared lib into packaged project for Unix
+if(UNIX)
+    install(FILES ${OSCPACK_LIBS_RELEASE} DESTINATION lib)
+endif()
