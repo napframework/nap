e4e53c7bc3b97db524270df42333a85315929dc4
Coen Klosters <coen@naivi.nl>
2018-05-16 16:17:24 +0200

    - lights above store turn off when the shops are closed

--- apps/kalvertoren/module/src/facadelightcomponent.cpp
+++ apps/kalvertoren/module/src/facadelightcomponent.cpp
@@ -6,10 +6,11 @@
 
 // nap::facadelightcomponent run time class definition 
 RTTI_BEGIN_CLASS(nap::FacadeLightComponent)
-RTTI_PROPERTY("Controller", &nap::FacadeLightComponent::mArtnetController,	nap::rtti::EPropertyMetaData::Required)
-	RTTI_PROPERTY("MinChannel", &nap::FacadeLightComponent::mMinChannel,	nap::rtti::EPropertyMetaData::Default)
-	RTTI_PROPERTY("MaxChannel", &nap::FacadeLightComponent::mMaxChannel,	nap::rtti::EPropertyMetaData::Default)
-	RTTI_PROPERTY("Send",		&nap::FacadeLightComponent::mSend,		nap::rtti::EPropertyMetaData::Default)
+RTTI_PROPERTY("Controller",				&nap::FacadeLightComponent::mArtnetController,		nap::rtti::EPropertyMetaData::Required)
+	RTTI_PROPERTY("MinChannel",			&nap::FacadeLightComponent::mMinChannel,			nap::rtti::EPropertyMetaData::Default)
+	RTTI_PROPERTY("MaxChannel",			&nap::FacadeLightComponent::mMaxChannel,			nap::rtti::EPropertyMetaData::Default)
+	RTTI_PROPERTY("Send",				&nap::FacadeLightComponent::mSend,					nap::rtti::EPropertyMetaData::Default)
+	RTTI_PROPERTY("IntensityComponent", &nap::FacadeLightComponent::mIntensityComponent,	nap::rtti::EPropertyMetaData::Required)
 RTTI_END_CLASS
 
 // nap::facadelightcomponentInstance run time class definition 
@@ -40,14 +41,25 @@ namespace nap
 
 		// Populate data buffer
 		int count = (mMaxChannel - mMinChannel) + 1;
-		mData = std::vector<uint8_t>(count, math::max<uint8_t>());
+		mOpeningData = std::vector<uint8_t>(count, math::max<uint8_t>());
+		mClosingData = std::vector<uint8_t>(count, 0);
 		return true;
 	}
 
 
 	void FacadeLightComponentInstance::update(double deltaTime)
 	{
+		// Don't update if send is explicitly turned off
 		if (!mSend) { return; }
-		mController->send(mData, mMinChannel);
+		
+		// Turn lights off when we take into account shopping hours and the stores are closed
+		if (mIntensityComponent->mUseOpeningHours && !mIntensityComponent->isOpen())
+		{
+			mController->send(mClosingData, mMinChannel);
+		}
+		else
+		{
+			mController->send(mOpeningData, mMinChannel);
+		}
 	}
 }
