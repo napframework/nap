2209f12b2ba4d7e45d884fac0fbb2efd44e91558
Coen Klosters <coen@naivi.nl>
2018-09-25 11:31:17 +0200

    - forgot to check in some files

---
+++ modules/napinput/src/controller.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <nap/numeric.h>
+#include <utility/dllexport.h>
+
+namespace nap
+{
+	/**
+	 * The most common game-controller buttons
+	 */
+	enum class NAPAPI EControllerButton : int
+	{
+		UNKNOWN = -1,
+		A,
+		B,
+		X,
+		Y,
+		BACK,
+		GUIDE,
+		START,
+		LEFT_STICK,
+		RIGHT_STICK,
+		LEFT_SHOULDER,
+		RIGHT_SHOULDER,
+		DPAD_UP,
+		DPAD_DOWN,
+		DPAD_LEFT,
+		DPAD_RIGHT
+	};
+
+
+	/**
+	 * List of axis available from a controller
+	 */
+	enum class NAPAPI EControllerAxis : int
+	{
+		UNKNOWN	= -1,
+		LEFT_X,
+		LEFT_Y,
+		RIGHT_X,
+		RIGHT_Y,
+		TRIGGER_LEFT,
+		TRIGGER_RIGHT
+	};
+}
