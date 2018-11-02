ceb3ee4cd648ead2f9e0f026d26d04e9c0d61f2f
Bas Korsmit <bmodone@gmail.com>
2018-11-01 21:09:34 -0700

    - add initial curve functionality

---
+++ modules/napmath/src/curvefunctions.h
@@ -0,0 +1,81 @@
+
+namespace nap
+{
+	namespace math
+	{
+		/**
+		 * Evaluate a clamped bezier curve at time t
+		 */
+		template<typename T, typename U>
+		T bezier(const T (& pts)[4], U t)
+		{
+			U u = 1 - t;
+			U a = u * u * u;
+			U b = 3 * u * u * t;
+			U c = 3 * u * t * t;
+			U d = t * t * t;
+			return pts[0] * a + pts[1] * b + pts[2] * c + pts[3] * d;
+		}
+
+		/**
+		 * Do a binary search on a curve for the t value at position x
+		 * ie. Convert a time value in cartesian space into a parametric t value.
+		 * @tparam P 2D point, its components should be of type U
+		 * @tparam F floating point
+		 * @param pts The curve points to evaluate
+		 * @param x Time input value in cartesian space
+		 * @param threshold The desired precision of the result
+		 * @param maxIterations Max number of tries to get to the desired precision
+		 * @return
+		 */
+		template<typename P, typename F>
+		F tForX(const P (& pts)[4], F x, F threshold = 0.0001, int maxIterations = 100)
+		{
+			F depth = 0.5;
+			F t = 0.5;
+			for (int i = 0; i < maxIterations; i++)
+			{
+				auto dx = x - bezier(pts, t).x();
+				auto adx = qAbs(dx);
+				if (adx <= threshold)
+					break;
+
+				depth *= 0.5;
+				t += (dx > 0) ? depth : -depth;
+			}
+			return t;
+		}
+
+		/**
+		 * Evaluate a curve segment
+		 * @tparam P 2D point, its components should be of type U
+		 * @tparam F floating point
+		 * @param pts The curve points to evaluate
+		 * @param x Time input value in cartesian space
+		 * @return Vertical component of the evaluated curve segment at time x
+		 */
+		template<typename P, typename F>
+		F evalCurveSegmentBezier(const P (& pts)[4], F x)
+		{
+			F t = tForX(pts, x);
+			return bezier(pts, t).y();
+		}
+
+		template<typename P, typename F>
+		F evalCurveSegmentLinear(const P (& pts)[4], F x)
+		{
+			const auto& a = pts[0];
+			const auto& b = pts[3];
+			F t = (x - a.x()) / (b.x() - a.x());
+			return lerp(a.y(), b.y(), t);
+		}
+
+		template<typename P, typename F>
+		F evalCurveSegmentStepped(const P (& pts)[4], F x)
+		{
+			return pts[0].y();
+		}
+
+	}
+}
+
---
+++ modules/napmath/src/fcurve.h
@@ -0,0 +1,241 @@
+#pragma once
+
+#include <utility/dllexport.h>
+#include <rtti/rtti.h>
+#include <glm/glm.hpp>
+
+#include "curvefunctions.h"
+
+namespace nap
+{
+	namespace math
+	{
+		/**
+		 * Possible interpolations per curve segment
+		 */
+		enum FCurveInterp {
+			Bezier,
+			Linear,
+			Stepped,
+		};
+
+		/**
+		 * A time/value pair to be used in function curves
+		 * @tparam T type of the time parameter
+		 * @tparam U type of the value parameter
+		 */
+		template<typename T, typename U>
+		class NAPAPI FComplex
+		{
+			T mTime;
+			U mValue;
+		};
+
+		/**
+		 * Represents a single handle on an FCurve
+		 * @tparam T type of the time parameter
+		 * @tparam U type of the value parameter
+		 */
+		template<typename T, typename U>
+		class NAPAPI FCurvePoint
+		{
+		public:
+			FComplex<T, U> mPos;
+			FComplex<T, U> mInTan;
+			FComplex<T, U> mOutTan;
+			FCurveInterp mInterp;
+
+			// Non-essential to functionality, but necessary for editing
+			bool mTangentsAligned = true;
+		};
+
+		/**
+		 * Compare functor to be used to keep curve points sorted for faster evaluation
+		 * @tparam T type of the time parameter
+		 * @tparam U type of the value parameter
+		 */
+		template<typename T, typename U>
+		struct PointCompare
+		{
+			bool operator()(const FCurvePoint<T, U>& lhs, const FCurvePoint<T, U>& rhs)
+			{
+				return lhs.mPos.mTime < rhs.mPos.mTime;
+			}
+		};
+
+		/**
+		 * A 1-D function curve that can be used to map one value to another,
+		 * for example to animate something over time.
+		 *
+		 * @tparam T type of the time parameter
+		 * @tparam U type of the value parameter
+		 */
+		template<typename T, typename U>
+		class NAPAPI FCurve
+		{
+			using Pt = FCurvePoint<T, U>;
+			using Fc = FComplex<T, U>;
+		public:
+
+			FCurve() = default;
+			
+			/**
+			 * Evaluate this curve at time t
+			 * @param t 
+			 * @return the evaluated value of the curve at time t
+			 */
+			T evaluate(const U& t);
+
+
+			std::vector<FCurvePoint<T, U>> mPoints;
+
+		private:
+			/**
+			 * Ensure points are sorted before evaluating the curve
+			 */
+			void sortPoints();
+
+			/**
+			 * Grab the two points on either side of time
+			 * @param time
+			 * @param curr
+			 * @param next
+			 */
+			void pointsAtTime(const U& time, Pt*& curr, Pt*& next) const;
+
+			/**
+			 * In case of Bezier interpolation, keep this curve segment from having multiple solutions
+			 * @param x0
+			 * @param x1
+			 * @param x2
+			 * @param x3
+			 */
+			void limitOverhang(U& x0, U& x1, U& x2, U& x3);
+
+			/**
+			 * In case of Bezier interpolation, keep this curve segment from having multiple solutions
+			 * @param pa
+			 * @param pb
+			 * @param pc
+			 * @param pd
+			 */
+			void limitOverhangPoints(Fc& pa, Fc& pb, Fc& pc, Fc& pd);
+
+			std::set<FCurvePoint<T, U>, PointCompare<T, U>> mSortedPoints;
+		};
+
+
+		//////////////////////////////////////////////////////////////////////////
+		// Template definitions
+		//////////////////////////////////////////////////////////////////////////
+
+		template<typename T, typename U>
+		void FCurve<T, U>::limitOverhangPoints(FCurve::Fc& pa, FCurve::Fc& pb, FCurve::Fc& pc, FCurve::Fc& pd)
+		{
+			auto a = pa.mTime;
+			auto b = pb.mTime;
+			auto c = pc.mTime;
+			auto d = pd.mTime;
+			auto bb = b;
+			auto cc = c;
+
+			limitOverhang(a, b, c, d);
+
+			// Calculate ratios for both tangents
+			auto rb = (b - a) / (bb - a);
+			auto rc = (c - d) / (cc - d);
+
+			// Apply corrected times
+			pb.mTime = b;
+			pc.mTime = c;
+
+			// we limited time, keep the value on the tangent line for c1 continuity
+			pb.mValue = ((pb.mValue - pa.mValue) * rb) + pa.mValue;
+			pc.mValue = ((pc.mValue - pd.mValue) * rc) + pd.mValue;
+		}
+
+		template<typename T, typename U>
+		void FCurve<T, U>::limitOverhang(U& x0, U& x1, U& x2, U& x3)
+		{
+			x1 = min(x1, x3);
+			x2 = qMax(x2, x0);
+		}
+
+		template<typename T, typename U>
+		void FCurve<T, U>::pointsAtTime(const U& time, FCurve::Pt*& curr, FCurve::Pt*& next) const
+		{
+			Pt* lastPt = mSortedPoints.cbegin();
+			for (auto pt : mSortedPoints)
+			{
+				if (pt.mPos.mTime > time)
+				{
+					curr = lastPt;
+					next = pt;
+					return;
+				}
+				lastPt = pt;
+			}
+			assert(false);
+		}
+
+		template<typename T, typename U>
+		void FCurve<T, U>::sortPoints()
+		{
+			mSortedPoints.clear();
+			mSortedPoints.emplace(mPoints);
+		}
+
+		template<typename T, typename U>
+		T FCurve<T, U>::evaluate(const U& time)
+		{
+			// TODO: Optimize or move this call somewhere else
+			sortPoints();
+
+
+			Pt* firstPoint = mSortedPoints[0];
+			if (time < firstPoint->mPos.mTime)
+				return firstPoint->mPos.mValue;
+
+			Pt* lastPoint = mSortedPoints[mSortedPoints.size() - 1];
+			if (time >= lastPoint->mPos.mTime)
+				return lastPoint->mPos.mValue;
+
+			Pt* curr = nullptr;
+			Pt* next = nullptr;
+			pointsAtTime(time, curr, next);
+			assert(curr);
+			assert(next);
+			auto a = curr->mPos;
+			auto b = a + curr->mOutTan;
+			auto d = next->mPos;
+			auto c = d + next->mInTan;
+
+			limitOverhangPoints(a, b, c, d);
+
+			switch (curr->mInterp)
+			{
+				case Bezier:
+					evalCurveSegmentBezier({a, b, c, d}, time);
+					break;
+				case Linear:
+					evalCurveSegmentLinear({a, b, c, d}, time);
+					break;
+				case Stepped:
+					evalCurveSegmentStepped({a, b, c, d}, time);
+					break;
+				default:
+					assert(false);
+			}
+
+			return 0;
+		}
+
+		//////////////////////////////////////////////////////////////////////////
+		// Alias some type specializations
+		//////////////////////////////////////////////////////////////////////////
+		using FloatFCurve = FCurve<float, float>;
+		using Vec2FCurve  = FCurve<float, glm::vec2>;
+		using Vec3FCurve  = FCurve<float, glm::vec3>;
+		using Vec4FCurve  = FCurve<float, glm::vec4>;
+	}
+}
