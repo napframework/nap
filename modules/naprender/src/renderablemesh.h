92ee94a3694ee618369f4e3055cb91c47d61ec38
Coen Klosters <coen@naivi.nl>
2018-11-21 17:56:00 +0100

    - BIGGIE - moved creation of renderable meshes to service. This makes it available to other components that work with material / mesh combinations besides from the default renderable component - added prototype copy stamp implementation of renderable mesh component.

--- modules/naprender/src/renderablemesh.h
+++ modules/naprender/src/renderablemesh.h
@@ -8,16 +8,22 @@
 namespace nap
 {
 	// Forward Declares
-	class RenderableMeshComponentInstance;
-	class RenderableTextComponentInstance;
+	class RenderService;
 
 	/**
-	 * Represents the coupling between a mesh and a material. This object can only be created by a RenderableComponent
-	 * This object manages a vertex array object handle that is issued by the render service. 
+	 * Represents the coupling between a mesh and a material that can be rendered to screen.
+	 * Use the vertex array object handle to bind your mesh before rendering.
+	 * The vertex array object handle is given by the render service on construction.
+	 * Call RenderService.createRenderableMesh() from your own renderable component to create a renderable mesh on initialization.
 	 */
 	class NAPAPI RenderableMesh final
 	{
+		friend class RenderService;
 	public:
+		/**
+		 * Default constructor, by default this represents an invalid mesh / material combination.
+		 * Can be used to declare a member of this type that is invalid on initialization.
+		 */
 		RenderableMesh() = default;
 
 		/**
@@ -27,34 +33,47 @@ namespace nap
 		bool isValid() const													{ return mVAOHandle.isValid(); }
 
 		/**
-		* @return The mesh object used to create this object.
-		*/
+		 * @return The mesh object used to create this object.
+		 */
 		IMesh& getMesh()														{ return *mMesh; }
 
 		/**
-		* @return The mesh object used to create this object.
-		*/
+		 * @return The mesh object used to create this object.
+		 */
 		const IMesh& getMesh() const											{ return *mMesh; }
 
 		/**
-		* @return The material instance object used to create this object.
-		*/
+		 * @return The material instance object used to create this object.
+		 */
 		MaterialInstance& getMaterialInstance()									{ return *mMaterialInstance; }
 
 		/**
-		* @return The material instance object used to create this object.
-		*/
+		 * @return The material instance object used to create this object.
+		 */
 		const MaterialInstance& getMaterialInstance() const						{ return *mMaterialInstance; }
 
-	private:
-		friend class RenderableMeshComponentInstance;
-		friend class RenderableTextComponentInstance;
-
 		/**
-		 * Constructor.
+		 * Binds the managed vertex array object. Call this before rendering your vertex buffers.
+		 * Only call this when the VAO is considered valid!
+		 */
+		void bind();
+		
+		/**
+		 * Unbinds the managed vertex array object. Call this after rendering your vertex buffers.
+		 * Only call this when the VAO is considered valid!
+		 */
+		void unbind();
+
+	protected:
+		/**
+		 * Constructor
+		 * @param mesh the mesh that is rendered
+		 * @param materialInstance the material the mesh is rendered with
+		 * @param vaoHandle, issued by the render service based on mesh / material combination
 		 */
 		RenderableMesh(IMesh& mesh, MaterialInstance& materialInstance, const VAOHandle& vaoHandle);
 
+	private:
 		MaterialInstance*	mMaterialInstance = nullptr;	///< Material instance
 		IMesh*				mMesh = nullptr;				///< Mesh
 		VAOHandle			mVAOHandle;						///< Vertex Array Object handle, acquired from the RenderService
