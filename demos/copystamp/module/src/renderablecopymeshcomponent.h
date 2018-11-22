59c6c7cb158014f45e03dd22abeb2bf4aa623865
Coen Klosters <coen@naivi.nl>
2018-11-22 19:24:34 +0100

    - working implementation of copy-stamp of meshes on target mesh

--- demos/copystamp/module/src/renderablecopymeshcomponent.h
+++ demos/copystamp/module/src/renderablecopymeshcomponent.h
@@ -2,6 +2,7 @@
 
 #include <rendercomponent.h>
 #include <renderablemesh.h>
+#include <rtti/objectptr.h>
 
 namespace nap
 {
@@ -25,6 +26,8 @@ namespace nap
 		// Properties
 		MaterialInstanceResource mMaterialInstanceResource;		///< Property: 'MaterialInstance' the material used to shade the text
 		std::string mColorUniform = "color";					///< Property: 'ColorUniform' name of the color uniform binding (vec3) in the shader
+		std::vector<rtti::ObjectPtr<IMesh>> mCopyMeshes;		///< Property: 'CopyMeshes' list of meshes to copy onto target
+		rtti::ObjectPtr<IMesh> mTargetMesh;						///< Property: 'Target' mesh to copy meshes onto
 	};
 
 
@@ -51,6 +54,11 @@ namespace nap
 		 */
 		virtual void update(double deltaTime) override;
 
+		/**x
+		 * @return material used for rendering the meshes	
+		 */
+		MaterialInstance& getMaterial();
+
 	protected:
 		/**
 		* Draws the text into to active render target using the provided matrices.
@@ -62,9 +70,34 @@ namespace nap
 		virtual void onDraw(const glm::mat4& viewMatrix, const glm::mat4& projectionMatrix) override;
 
 	private:
+		template<typename T>
+		T* extractUniform(const std::string& name, utility::ErrorState& error);
+
 		TransformComponentInstance* mTransform = nullptr;				///< Transform used to position instanced meshes
 		std::vector<RenderableMesh> mMeshes;							///< All the valid mesh / material combinations
 		MaterialInstance mMaterialInstance;								///< The MaterialInstance as created from the resource. 
 		VertexAttribute<glm::vec3>* mPositionAttr = nullptr;			///< Handle to the vertices we want to stamp
+		IMesh* mTargetMesh;												///< Mesh to copy onto
+		std::vector<RenderableMesh> mCopyMeshes;						///< All renderable variants of the mesh to copy
+		Vec3VertexAttribute* mTargetVertices = nullptr;					///< Point positions to copy meshes onto
+		nap::UniformVec3* mColorUniform = nullptr;						///< Color uniform slot
+		nap::UniformMat4* mProjectionUniform = nullptr;					///< Projection matrix uniform slot
+		nap::UniformMat4* mViewUniform = nullptr;						///< View matrix uniform slot
+		nap::UniformMat4* mModelUniform = nullptr;						///< Model matrix uniform slot
 	};
+
+
+	template<typename T>
+	T* nap::RenderableCopyMeshComponentInstance::extractUniform(const std::string& name, utility::ErrorState& error)
+	{
+		// Find the uniform in the material first
+		nap::Material* material = mMaterialInstance.getMaterial();
+		T* found_uni = material->findUniform<T>(name);
+		if (!error.check(found_uni != nullptr, "%s: unable to find uniform: %s", material->mID.c_str(), name.c_str()))
+			return nullptr;
+
+		// If found, create a uniform associated with the instance of that material
+		return &(mMaterialInstance.getOrCreateUniform<T>(name));
+	}
+
 }
