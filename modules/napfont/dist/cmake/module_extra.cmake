f0f61d2232a31c553fc0ff4e648e6bd1d1facd14
Coen Klosters <coen@naivi.nl>
2018-12-05 19:23:39 +0100

    - using copy macro instead of new post build function distribution win32

--- modules/napfont/dist/cmake/module_extra.cmake
+++ modules/napfont/dist/cmake/module_extra.cmake
@@ -3,14 +3,7 @@ target_link_libraries(${PROJECT_NAME} freetype)
 target_include_directories(${PROJECT_NAME} PUBLIC ${FREETYPE_INCLUDE_DIRS})
 
 if(WIN32)
-    # Add post-build step to set copy yoctopuce to bin
-    add_custom_command(TARGET ${PROJECT_NAME}
-                       POST_BUILD
-                       COMMAND ${CMAKE_COMMAND} 
-                               -E copy
-                               $<TARGET_FILE:freetype>
-                               $<TARGET_FILE_DIR:${PROJECT_NAME}> 
-                       )
+  copy_freetype_dll()
 elseif(APPLE)
     file(GLOB RTFREETYPE_LIBS ${THIRDPARTY_DIR}/freetype/lib/libfreetype*${CMAKE_SHARED_LIBRARY_SUFFIX})
       install(FILES ${RTFREETYPE_LIBS} DESTINATION lib)
