e9ebafc4f5da3cc6b82b6386d09867188db859e1
Coen Klosters <coen@naivi.nl>
2019-01-10 18:44:55 +0100

    - adding some mock code -> TODO: remove reading object (blegh) and create ranged containers for various types

--- apps/emography/module/src/emographyreading.cpp
+++ apps/emography/module/src/emographyreading.cpp
@@ -5,6 +5,7 @@ RTTI_BEGIN_CLASS(nap::emography::Reading)
 	RTTI_PROPERTY("Snapshots",	&nap::emography::Reading::mSnapshots,	nap::rtti::EPropertyMetaData::Default)
 	RTTI_PROPERTY("StartTime",	&nap::emography::Reading::mStartTime,	nap::rtti::EPropertyMetaData::Default)
 	RTTI_PROPERTY("EndTime",	&nap::emography::Reading::mEndTime,		nap::rtti::EPropertyMetaData::Default)
+	RTTI_PROPERTY("Samples",	&nap::emography::Reading::mSamples,		nap::rtti::EPropertyMetaData::Default)
 RTTI_END_CLASS
 
 //////////////////////////////////////////////////////////////////////////
--- apps/emography/module/src/emographyreading.h
+++ apps/emography/module/src/emographyreading.h
@@ -11,6 +11,28 @@ namespace nap
 {
 	namespace emography
 	{
+		/* 
+		TODO: Fully implement templated container types based on range
+		class NAPAPI BaseRangedContainer
+		{
+		public:
+			TimeStamp mStartTime;
+			TimeStamp mEndTime;
+			int mSamples;
+		};
+
+		template<typename T> 
+		class RangedContainer : public BaseRangedContainer
+		{
+		public:
+
+		private:
+			std::vector<T> mMembers;
+		};
+
+		using SnapshotContainer = RangedContainer<Snapshot>;
+		*/
+
 		/**
 		 * Represents a set of emography snapshots that spans a particular time range
 		 */
@@ -27,8 +49,9 @@ namespace nap
 			virtual bool init(utility::ErrorState& errorState) override;
 
 			std::vector<Snapshot> mSnapshots;		///< Property: 'Snapshots' all snapshots associated with this reading
-			TimeStamp mStartTime;					///< Property: Start time associated with snapshots
-			TimeStamp mEndTime;						///< Property: End time associated with snapshots
+			TimeStamp mStartTime;					///< Property: 'StartTime' Start date associated with snapshots
+			TimeStamp mEndTime;						///< Property: 'EndTime' End date associated with snapshots
+			int mSamples = 1024;					///< Property: 'Samples' number of snapshots associated with this reading
 		};
 	}
 }
