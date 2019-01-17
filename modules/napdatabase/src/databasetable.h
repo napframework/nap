2d440bacdaa183c8f2d126b02b225d62b3e586d1
jellevdbeek <jelle@presence-vr.net>
2019-01-17 14:11:38 +0100

    DatabaseTable is now caching column data and preparing SQL queries for insertions. This makes insertions faster and is a preparation for deserialization of objects.

--- modules/napdatabase/src/databasetable.h
+++ modules/napdatabase/src/databasetable.h
@@ -2,9 +2,11 @@
 
 #include "utility/dllexport.h"
 #include "utility/errorstate.h"
-#include "sqlite3.h"
 #include "rtti/typeinfo.h"
 #include "rtti/object.h"
+#include "rtti/path.h"
+
+struct sqlite3_stmt;
 
 namespace nap
 {
@@ -14,18 +16,27 @@ namespace nap
 	{
 	public:
 		DatabaseTable(Database& database, const std::string& tableID, const rtti::TypeInfo& objectType);
+		~DatabaseTable();
 		
 		bool init(utility::ErrorState& errorState);
 		bool add(const rtti::Object& object, utility::ErrorState& errorState);
 
-		std::vector<rtti::Object*> getLast(int count)
-		{
-			return std::vector<rtti::Object*>();
-		}
+		bool getLast(int count, std::vector<std::unique_ptr<rtti::Object>>& objects, utility::ErrorState& errorState);
 
 	private:
+		struct Column
+		{
+			rtti::Path	mPath;
+			std::string mSqlType;
+		};
+
+		using ColumnList = std::vector<Column>;
+
 		rtti::TypeInfo	mObjectType;
 		Database*		mDatabase;
 		std::string		mTableID;
+		ColumnList		mColumns;
+
+		sqlite3_stmt*   mInsertStatement = nullptr;
 	};
 }
