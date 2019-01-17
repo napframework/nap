e59db48aa5ee1cfa0d821bdf8888e3639e272810
jellevdbeek <jelle@presence-vr.net>
2019-01-17 12:19:13 +0100

    Initial support for serializing rtti Objects into a database. Initial DataModel for the Emography app that creates tables in a database for different kinds of LODs.

---
+++ modules/napdatabase/src/database.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "utility/dllexport.h"
+#include "utility/errorstate.h"
+#include "sqlite3.h"
+#include "databasetable.h"
+
+namespace nap
+{
+	class NAPAPI Database final
+	{
+	public:
+		Database();
+		~Database();
+
+		Database(const Database& rhs) = delete;
+		Database& operator=(const Database& rhs) = delete;
+// 		Database(Database&& rhs) = delete;
+// 		Database& operator=(Database&& rhs) = delete;
+		
+		bool init(utility::ErrorState& errorState);
+		DatabaseTable* createTable(const std::string& tableID, const rtti::TypeInfo& objectType, utility::ErrorState& errorState);
+
+	private:
+		sqlite3& GetDatabase() { return *mDatabase; }
+
+	private:
+		friend class DatabaseTable;
+		using DatabaseTableMap = std::unordered_map<std::string, std::unique_ptr<DatabaseTable>>;
+
+		sqlite3*			mDatabase = nullptr;		
+		DatabaseTableMap	mTables;
+	};
+}
