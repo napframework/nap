4824365575d4f17de67f1ac1a670066d68d8c0e2
Bas Korsmit <bmodone@gmail.com>
2019-02-07 21:31:20 -0800

    convert spaces to tabs

--- apps/solarsystem/src/solarsystem.h
+++ apps/solarsystem/src/solarsystem.h
@@ -21,30 +21,30 @@ namespace nap
 {
 	using namespace rtti;
 
-    /**
-     * Main application called from within the main loop
-     */
-    class CoreApp : public App 
+	/**
+	 * Main application called from within the main loop
+	 */
+	class CoreApp : public App
 	{
-    public:
+	public:
 
 		/**
 		 * Constructor
 		 */
-        CoreApp(nap::Core& core) : App(core) {}
+		CoreApp(nap::Core& core) : App(core) {}
 
-        /**
-         * Provide the app with the json file that will be loaded, must be called before initialization.
+		/**
+		 * Provide the app with the json file that will be loaded, must be called before initialization.
 		 * @param fileName the json file to load on initialization
-         */
-        void setFilename(const std::string& fileName)								{ mFilename = fileName; }
+		 */
+		void setFilename(const std::string& fileName)								{ mFilename = fileName; }
 
-        /**
-         * Initialize all the services and app specific data structures
+		/**
+		 * Initialize all the services and app specific data structures
 		 * @param error contains the error code when initialization fails
 		 * @return if initialization succeeded
-         */
-        bool init(utility::ErrorState& error) override;
+		 */
+		bool init(utility::ErrorState& error) override;
 
 		/**
 		 * Update is called every frame, before render.
@@ -52,40 +52,40 @@ namespace nap
 		 */
 		void update(double deltaTime) override;
 
-        /**
-         * Render is called after update. Use this call to render objects to a specific target
-         */
-        void render() override;
+		/**
+		 * Render is called after update. Use this call to render objects to a specific target
+		 */
+		void render() override;
 
-        /**
-         * Called when the app receives a window message.
+		/**
+		 * Called when the app receives a window message.
 		 * @param windowEvent the window message that occurred
-         */
-        void windowMessageReceived(WindowEventPtr windowEvent) override;
+		 */
+		void windowMessageReceived(WindowEventPtr windowEvent) override;
 
-        /**
-         * Called when the app receives an input message (from a mouse, keyboard etc.)
+		/**
+		 * Called when the app receives an input message (from a mouse, keyboard etc.)
 		 * @param inputEvent the input event that occurred
-         */
-        void inputMessageReceived(InputEventPtr inputEvent) override;
+		 */
+		void inputMessageReceived(InputEventPtr inputEvent) override;
 
-        /**
+		/**
 		 * Called when the app is shutting down after quit() has been invoked
 		 * @return the application exit code, this is returned when the main loop is exited
-         */
-        int shutdown() override;
+		 */
+		int shutdown() override;
 
-    private:
-        ResourceManager*		mResourceManager = nullptr;		///< Manages all the loaded data
-        std::string				mFilename = "";					///< The JSON file that is loaded on initialization
+	private:
+		ResourceManager*		mResourceManager = nullptr;		///< Manages all the loaded data
+		std::string				mFilename = "";					///< The JSON file that is loaded on initialization
 		RenderService*			mRenderService = nullptr;		///< Render Service that handles render calls
 		SceneService*			mSceneService = nullptr;		///< Manages all the objects in the scene
 		InputService*			mInputService = nullptr;		///< Input service for processing input
 		IMGuiService*			mGuiService = nullptr;			///< Manages GUI related update / draw calls
 		ObjectPtr<RenderWindow> mRenderWindow;					///< Pointer to the render window	
 		ObjectPtr<Scene>		mScene = nullptr;				///< Pointer to the main scene
-        
-        ObjectPtr<EntityInstance> mCamera = nullptr;
-        ObjectPtr<EntityInstance> mSphere = nullptr;
+
+		ObjectPtr<EntityInstance> mCamera = nullptr;
+		ObjectPtr<EntityInstance> mSphere = nullptr;
 	};
 }
