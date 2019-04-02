900d877d062f9995eb57fb5f652ca581f880dac3
Coen Klosters <coen@naivi.nl>
2019-04-02 12:23:24 +0200

    - removed android specific service runner - removed android specific app - android extension is used to resolve all paths etc, no need for special classes anymore - mod_napapp doesn't have any special android related classes anymore

--- core/src/nap/android/androidextension.h
+++ core/src/nap/android/androidextension.h
@@ -2,16 +2,16 @@
 
 // External Includes
 #include <nap/coreextension.h>
-#include <utility/dllexport.h>
-#include <rtti/typeinfo.h>
+#include <nap/logger.h>
 #include <android/asset_manager.h>
+#include <jni.h>
 
 namespace nap
 {
 	/**
-	 * Extension required by Android specific applications.
-	 * Provides access to the android asset manager and shared library directory.
-	 * The AssetManager is used to load assets bundled included with the Android APK.
+	 * Extension required by Android specific applications. 
+	 * Provides access to the android environment and context.
+	 * This extension is used on initialization of Core and the running app to source android specific data.
 	 */
 	class NAPAPI AndroidExtension : public CoreExtension
 	{
@@ -22,10 +22,10 @@ namespace nap
 
 		/**
 		 * Constructor
-		 * @param assetManager the android asset manager
-		 * @param naiveLibDir the android native library directory
+		 * @param jniEnv the java native environment
+		 * @param androidContextObject the android context (activity or service)
 		 */
-		AndroidExtension(AAssetManager* assetManager, std::string nativeLibDir);
+		AndroidExtension(JNIEnv *jniEnv, jobject androidContextObject);
 
 		/**
 		 * @return the android asset manager
@@ -37,15 +37,57 @@ namespace nap
 		 */
 		const std::string& getNativeLibDir() const;
 
-	private:
-		// The AssetManager is used to load assets bundled included with the Android APK. Bundled assets with the app
-		// aren't files on disk and need to be accessed via this mechanism. Currently used by the ResourceManager to 
-		// load the project structure JSON and the ProjectInfoManager for the.. project info but may likely be useful 
-		// elsewhere later.
-		AAssetManager* mAndroidAssetManager;
+		/**
+		 * @return the android internal storage directory	
+		 */
+		const std::string& getInternalFilesDir() const;
 
-		// The location of the shared libraries for the Android app on disk. Used by the ModuleManager to load the 
-		// module shared libs.
-		std::string mAndroidNativeLibDir;
+		/**
+		 * Get the JNI environment from our Java VM
+		 * @return The Java native environment
+		 */
+		JNIEnv* getEnv() const;
+
+		/**
+		 * Get the context back in Android space, typically the activity or service
+		 * @return The context back in Android space
+		 */
+		jobject getContext() const;
+
+	private:
+		JavaVM*	mAndroidJvm = nullptr; 				///< The JNI environment
+		jobject	mAndroidGlobalObject = nullptr;		///< Our context object back in Android space, typically the activity or service
+		JNIEnv* mAndroidEnv = nullptr;				///< The android environment
+		
+		std::string mAppInternalFilesDir;			///< The application file directory.
+		std::string mAndroidNativeLibDir;			///< The location of the shared libraries for the Android app on disk.
+
+		// The AssetManager is used to load assets bundled included with the Android APK. Bundled assets with the app
+        // aren't files on disk and need to be accessed via this mechanism. Currently used by the ResourceManager to 
+        // load the project structure JSON and the ProjectInfoManager for the.. project info but may likely be useful 
+        // elsewhere later.
+        AAssetManager* mAndroidAssetManager;
+
+		/**
+		 * Extract the java native environment.
+		 * @param virtualMachine the java virtual environment.
+		 * @return java environment, nullptr if extraction fails	
+		 */
+		static JNIEnv* extractEnv(JavaVM* virtualMachine);
+
+		/**
+		 * Extracts the asset manager	
+		 */
+		AAssetManager* extractAssetManager(JNIEnv* env);
+
+		/**
+		 * Extracts the android native library	
+		 */
+		std::string extractNativeLibDir(JNIEnv* env);
+
+		/**
+		 * Extracts the internal application directory from the app	
+		 */
+		std::string extractAppInternalFilesDir(JNIEnv* env);
 	};
 }
