29716ebea87e10b9e4c3b8369e248ac3d10d2bbc
cklosters <coen@naivi.nl>
2019-04-11 11:10:44 +0200

    - adding stress state component - adding stress state component to api control component - adding getStressState signature to list of external callbacks

--- apps/emography/module/src/emographyaddstresscomponent.cpp
+++ apps/emography/module/src/emographyaddstresscomponent.cpp
@@ -36,12 +36,30 @@ namespace nap
 
 		bool AddStressSampleComponentInstance::addSample(const TimeStamp& timeStamp, float stressValue, int stressState, utility::ErrorState& error)
 		{
+			// Add intensity value
 			std::unique_ptr<StressIntensityReading> intensityReading = std::make_unique<StressIntensityReading>(stressValue, timeStamp.toSystemTime());
 			if (!mDataModel->add(*intensityReading, error))
 			{
-				error.fail("failed to add additional stress sample");
+				error.fail("failed to add additional sample, database error");
 				return false;
 			}
+
+			// Ensure state is valid
+			if (stressState < (int)EStressState::Under || stressState >(int)EStressState::Over)
+			{
+				error.fail("failed to add additional sample, invalid stress state");
+				return false;
+			}
+
+			// Add new state sample
+			EStressState new_state = static_cast<EStressState>(stressState);
+			std::unique_ptr<StressStateReading> stateReading = std::make_unique<StressStateReading>(new_state, timeStamp.toSystemTime());
+			if (!mDataModel->add(*stateReading, error))
+			{
+				error.fail("failed to add additional sample, database error");
+				return false;
+			}
+
 			return true;
 		}
 	}
