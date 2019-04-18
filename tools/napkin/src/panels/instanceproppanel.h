e24b844ef73394a534626912754274f8c0a191e9
Bas <bmodone@gmail.com>
2019-04-17 21:13:53 -0700

    - documentation - disable editability of items

--- tools/napkin/src/panels/instanceproppanel.h
+++ tools/napkin/src/panels/instanceproppanel.h
@@ -8,7 +8,9 @@
 
 namespace napkin
 {
-
+	/**
+	 * An item holding a nap::TargetAttribute
+	 */
 	class InstPropAttribItem : public QStandardItem
 	{
 	public:
@@ -18,45 +20,66 @@ namespace napkin
 		nap::TargetAttribute& mAttrib;
 	};
 
+	/**
+	 * An item holding nap::ComponentInstanceProperties
+	 */
 	class InstancePropsItem : public QStandardItem
 	{
 	public:
 		explicit InstancePropsItem(nap::ComponentInstanceProperties& props);
 		QVariant data(int role) const override;
+		nap::ComponentInstanceProperties& props() const { return mProps; }
+
 	private:
 		nap::ComponentInstanceProperties& mProps;
 	};
 
+	/**
+	 * An item holding a RootEntity
+	 */
 	class RootEntityPropItem : public QStandardItem
 	{
 	public :
 		explicit RootEntityPropItem(nap::RootEntity& rootEntity);
 		QVariant data(int role) const override;
+
 	private:
 		nap::RootEntity& mRootEntity;
 	};
 
+	/**
+	 * An item holding a Scene
+	 */
 	class InstPropSceneItem : public QStandardItem
 	{
 	public:
 		explicit InstPropSceneItem(nap::Scene& scene);
 		QVariant data(int role) const override;
+
 	private:
 		nap::Scene& mScene;
 	};
 
+	/**
+	 * A model providing a tree of all instance properties in a scene
+	 */
 	class InstancePropModel : public QStandardItemModel
 	{
 	Q_OBJECT
 	public:
 		InstancePropModel();
+
 	Q_SIGNALS:
 		void sceneChanged();
+
 	private:
 		void onDocumentChanged();
 		void onSceneChanged();
 	};
 
+	/**
+	 * This panel shows the currently set instance properties
+	 */
 	class InstancePropPanel : public QWidget
 	{
 	public:
