721d41eb87d37d2f7edcca6415ea0f0fe6f2841a
cklosters <coen@naivi.nl>
2019-04-27 15:12:29 +0200

    - fixing support for child groups in napkin

--- modules/napparameter/src/parameter.cpp
+++ modules/napparameter/src/parameter.cpp
@@ -5,8 +5,8 @@ RTTI_BEGIN_CLASS_NO_DEFAULT_CONSTRUCTOR(nap::Parameter)
 RTTI_END_CLASS
 
 RTTI_BEGIN_CLASS(nap::ParameterGroup)
-	RTTI_PROPERTY("Parameters",	&nap::ParameterGroup::mParameters, nap::rtti::EPropertyMetaData::Embedded)
-	RTTI_PROPERTY("Children",	&nap::ParameterGroup::mChildren, nap::rtti::EPropertyMetaData::Embedded)
+	RTTI_PROPERTY("Parameters",		&nap::ParameterGroup::mParameters,	nap::rtti::EPropertyMetaData::Embedded)
+	RTTI_PROPERTY("Groups",			&nap::ParameterGroup::mChildren,	nap::rtti::EPropertyMetaData::Embedded)
 RTTI_END_CLASS
 
 namespace nap
