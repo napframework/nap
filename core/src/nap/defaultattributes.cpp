0d846a0bfa04a2447f148ef428981da559df8ce5
Bas <bmodone@gmail.com>
2019-05-08 22:39:42 -0700

    - add support for short - improve error logging when types cannot be created

--- core/src/nap/defaultattributes.cpp
+++ core/src/nap/defaultattributes.cpp
@@ -7,6 +7,7 @@
 
 // Numeric Defaults
 RTTI_DEFINE_STRUCT(std::string)
+RTTI_DEFINE_STRUCT(short)
 RTTI_DEFINE_STRUCT(int)
 RTTI_DEFINE_STRUCT(float)
 RTTI_DEFINE_STRUCT(double)
@@ -17,6 +18,7 @@ RTTI_DEFINE_STRUCT(nap::uint8)
 // Arrays of numeric defaults
 RTTI_DEFINE_STRUCT(std::vector<double>)
 RTTI_DEFINE_STRUCT(std::vector<std::string>)
+RTTI_DEFINE_STRUCT(std::vector<short>)
 RTTI_DEFINE_STRUCT(std::vector<int>)
 RTTI_DEFINE_STRUCT(std::vector<float>)
 RTTI_DEFINE_STRUCT(std::vector<nap::uint>)
