c3f8de02b953c03f6bd1988e2db17625673fe647
cklosters <coen@naivi.nl>
2019-05-20 18:10:31 +0200

    - adding serial module and msvc serial support

---
+++ dist/cmake/Findserial.cmake
@@ -0,0 +1,49 @@
+# default oscpack directory
+find_path(SERIAL_DIR
+          NAMES include/serial/serial.h
+          HINTS ${THIRDPARTY_DIR}/serial
+          )
+
+if(SERIAL_DIR)
+    set(SERIAL_FOUND true)
+else()
+    set(SERIAL_FOUND false)
+endif()
+
+if(WIN32)
+    set(SERIAL_LIBS_DEBUG ${SERIAL_DIR}/lib/Debug/serial.lib Ws2_32 winmm)
+    set(SERIAL_LIBS_RELEASE ${SERIAL_DIR}/lib/Release/serial.lib Ws2_32 winmm)
+elseif(APPLE)
+    #set(OSCPACK_LIBS_DEBUG ${OSCPACK_DIR}/lib/Debug/liboscpack.1.1.0.dylib)
+    #set(OSCPACK_LIBS_RELEASE ${OSCPACK_DIR}/lib/Release/liboscpack.1.1.0.dylib)
+elseif(ANDROID)
+    #set(OSCPACK_LIBS_DEBUG ${OSCPACK_DIR}/lib/Debug/${ANDROID_ABI}/liboscpack.so)
+    #set(OSCPACK_LIBS_RELEASE ${OSCPACK_DIR}/lib/Release/${ANDROID_ABI}/liboscpack.so)
+else()
+    #set(OSCPACK_LIBS_DEBUG ${OSCPACK_DIR}/lib/liboscpack.so)
+    #set(OSCPACK_LIBS_RELEASE ${OSCPACK_DIR}/lib/liboscpack.so)
+endif()
+
+# include directory is universal
+set(SERIAL_INCLUDE_DIRS ${SERIAL_DIR}/include)
+
+mark_as_advanced(SERIAL_INCLUDE_DIRS)
+
+if(SERIAL_FOUND)
+    message(STATUS "Found serial in: ${SERIAL_DIR}")
+endif()
+
+# promote package for find
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(serial REQUIRED_VARS SERIAL_DIR SERIAL_INCLUDE_DIRS SERIAL_LIBS_DEBUG SERIAL_LIBS_RELEASE)
+
+if(WIN32)
+    add_library(serial STATIC IMPORTED)
+else()
+    add_library(serial SHARED IMPORTED)
+endif()
+set_target_properties(serial PROPERTIES
+                      IMPORTED_CONFIGURATIONS "Debug;Release"
+                      IMPORTED_LOCATION_RELEASE ${SERIAL_LIBS_RELEASE}
+                      IMPORTED_LOCATION_DEBUG ${SERIAL_LIBS_DEBUG}
+                      )
