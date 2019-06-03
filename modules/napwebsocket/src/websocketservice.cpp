931cf6d46ae7cede88adeb1110dec295a7c43202
cklosters <coen@naivi.nl>
2019-06-03 18:47:58 +0200

    - adding registration of server to websocket end point and service

--- modules/napwebsocket/src/websocketservice.cpp
+++ modules/napwebsocket/src/websocketservice.cpp
@@ -27,4 +27,37 @@ namespace nap
 	{
 		factory.addObjectCreator(std::make_unique<WebSocketServerObjectCreator>(*this));
 	}
+
+
+	void WebSocketService::update(double deltaTime)
+	{
+		std::queue<WebSocketEventPtr> events;
+		for (auto& server : mServers)
+		{
+			server->consumeEvents(events);
+			while (!(events.empty()))
+			{
+				WebSocketEvent& websocket_event = *(events.front());
+				nap::Logger::info("processing: %s", websocket_event.get_type().get_name().to_string().c_str());
+				events.pop();
+			}
+		}
+	}
+
+
+	void WebSocketService::registerServer(WebSocketServer& server)
+	{
+		mServers.emplace_back(&server);
+	}
+
+
+	void WebSocketService::removeServer(WebSocketServer& server)
+	{
+		auto found_it = std::find_if(mServers.begin(), mServers.end(), [&](const auto& it)
+		{
+			return it == &server;
+		});
+		assert(found_it != mServers.end());
+		mServers.erase(found_it);
+	}
 }
