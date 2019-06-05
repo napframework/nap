5c261bfba7f1c0ada3d9098e401bdf2302196e04
cklosters <coen@naivi.nl>
2019-06-05 13:22:51 +0200

    - ensuring move operation and ability to move contents of message for all message types

--- modules/napwebsocket/src/websocketmessage.cpp
+++ modules/napwebsocket/src/websocketmessage.cpp
@@ -1,34 +1,70 @@
 #include "websocketmessage.h"
 
+#include <nap/logger.h>
+
 namespace nap
 {
-	WebSocketMessage::WebSocketMessage(wspp::MessagePtr message) : mMessage(message)
+	WebSocketMessage::WebSocketMessage(wspp::MessagePtr message)
+	{
+		mMessage = message->get_payload();
+		mCode = static_cast<EWebSocketOPCode>(message->get_opcode());
+		mFin = message->get_fin();
+	}
+
+
+	WebSocketMessage::WebSocketMessage(const std::string& message, EWebSocketOPCode code, bool fin) :
+		mMessage(message),
+		mCode(code),
+		mFin(fin)
 	{
 
+	}
+
+	WebSocketMessage::WebSocketMessage(std::string&& message, EWebSocketOPCode code, bool fin) :
+		mMessage(std::move(message)),
+		mCode(code),
+		mFin(fin)
+	{
+	}
+
+	WebSocketMessage::WebSocketMessage(WebSocketMessage&& other) :
+		mMessage(std::move(other.mMessage)),
+		mCode(other.mCode),
+		mFin(other.mFin)
+	{
 	}
 
 
 	const std::string& WebSocketMessage::getPayload() const
 	{
-		return mMessage->get_payload();
+		return mMessage;
 	}
 
 
 	nap::EWebSocketOPCode WebSocketMessage::getCode() const
 	{
-		return static_cast<EWebSocketOPCode>(mMessage->get_opcode());
+		return mCode;
 	}
 
 
 	bool WebSocketMessage::getFin() const
 	{
-		return mMessage->get_fin();
+		return mFin;
 	}
 
 
-	bool WebSocketMessage::getCompressed() const
+	void WebSocketMessage::appendPayload(const std::string& payload)
 	{
-		return mMessage->get_compressed();
+		mMessage.append(payload);
+	}
+
+
+	WebSocketMessage& WebSocketMessage::operator=(WebSocketMessage&& other)
+	{
+		mMessage = std::move(other.mMessage);
+		mCode = other.mCode;
+		mFin = other.mFin;
+		return *this;
 	}
 
 }
