ec97abf96953f434a004b340d361a29542f235a2
cklosters <coen@naivi.nl>
2019-06-07 23:02:40 +0200

    - trying to work around initialization crap again.

---
+++ modules/napwebsocket/src/websocketclient.cpp
@@ -0,0 +1,34 @@
+#include "websocketclient.h"
+#include <nap/logger.h>
+
+// nap::websocketclient run time class definition 
+RTTI_BEGIN_CLASS(nap::IWebSocketClient)
+	RTTI_PROPERTY("EndPoint",	&nap::IWebSocketClient::mEndPoint,	nap::rtti::EPropertyMetaData::Required)
+	RTTI_PROPERTY("URI",		&nap::IWebSocketClient::mURI,		nap::rtti::EPropertyMetaData::Required)
+RTTI_END_CLASS
+
+//////////////////////////////////////////////////////////////////////////
+
+
+namespace nap
+{
+	IWebSocketClient::~IWebSocketClient()			
+	{
+		destroyed(mConnection);
+	}
+
+
+	bool IWebSocketClient::init(utility::ErrorState& errorState)
+	{
+		if (!mEndPoint->connect(*this, errorState))
+			return false;
+		return true;
+	}
+
+
+	bool IWebSocketClient::isConnected() const
+	{
+		return true;
+	}
+
+}
--- modules/napwebsocket/src/websocketconnection.h
+++ modules/napwebsocket/src/websocketconnection.h
@@ -11,17 +11,18 @@ namespace nap
 	class WebSocketServerEndPoint;
 
 	/**
-	 * Utility class that wraps a web-socket connection handle.
-	 * Can only be constructed web socket server end points.
+	 * Utility class that wraps a web-socket connection handle. By default the connection is invalid.
+	 * Only web-socket endpoints can construct a valid web-socket connection.
 	 * The object is light and can be copied and or moved around freely.
 	 */
 	class NAPAPI WebSocketConnection final
 	{
 		friend class WebSocketServerEndPoint;
+		friend class WebSocketClientEndPoint;
 
 	public:
-		// Default constructor
-		WebSocketConnection() = delete;
+		// Creates an invalid (expired) connection
+		WebSocketConnection() = default;
 		// Default move constructor
 		WebSocketConnection(WebSocketConnection&& other) = default;
 		// Default move assignment operator
@@ -30,6 +31,13 @@ namespace nap
 		WebSocketConnection(const WebSocketConnection& other) = default;
 		// Default copy assignment operator
 		WebSocketConnection& operator=(const WebSocketConnection& other) = default;
+
+		/**
+		 * An expired connection isn't managed by an endpoint.
+		 * Not to be confused by the connection status.
+		 * @return if this connection expired.
+		 */
+		bool expired() const;
 
 	private:
 		/**
