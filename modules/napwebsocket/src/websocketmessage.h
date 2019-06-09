191772921618fef05a271d3b596ed1a8b995f3a8
cklosters <coen@naivi.nl>
2019-06-09 23:00:00 +0200

    Revert "- attempting to wrap separate locks around client connections"
    This reverts commit b71b7b27b57dc507d7198586e009feb659705221.

--- modules/napwebsocket/src/websocketmessage.h
+++ modules/napwebsocket/src/websocketmessage.h
@@ -19,7 +19,6 @@ namespace nap
 	{
 		friend class WebSocketServerEndPoint;
 		friend class WebSocketClientEndPoint;
-		friend class WebSocketMetaClient;
 	public:
 		/**
 		 * Construct a new web socket message using the given payload, opcode and finalize bit flag.
