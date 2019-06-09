9ead9e046b746da0ade9cf439c46eef1c8d8492d
cklosters <coen@naivi.nl>
2019-06-09 14:01:06 +0200

    - websocket client end point now manages all client connections - added websocket client interface, represents a single connection to a server - added basic websocket client, able to send and receive messages

--- modules/napwebsocket/src/websocketclient.cpp
+++ modules/napwebsocket/src/websocketclient.cpp
@@ -2,9 +2,12 @@
 #include <nap/logger.h>
 
 // nap::websocketclient run time class definition 
-RTTI_BEGIN_CLASS(nap::IWebSocketClient)
+RTTI_BEGIN_CLASS_NO_DEFAULT_CONSTRUCTOR(nap::IWebSocketClient)
 	RTTI_PROPERTY("EndPoint",	&nap::IWebSocketClient::mEndPoint,	nap::rtti::EPropertyMetaData::Required)
 	RTTI_PROPERTY("URI",		&nap::IWebSocketClient::mURI,		nap::rtti::EPropertyMetaData::Required)
+RTTI_END_CLASS
+
+RTTI_BEGIN_CLASS(nap::WebSocketClient)
 RTTI_END_CLASS
 
 //////////////////////////////////////////////////////////////////////////
@@ -14,21 +17,105 @@ namespace nap
 {
 	IWebSocketClient::~IWebSocketClient()			
 	{
-		disconnect(mConnection);
+		destroyed(*this);
 	}
 
 
 	bool IWebSocketClient::init(utility::ErrorState& errorState)
 	{
-		if (!mEndPoint->connect(*this, errorState))
+		if (!mEndPoint->registerClient(*this, errorState))
 			return false;
 		return true;
 	}
 
 
-	bool IWebSocketClient::isConnected() const
+	bool IWebSocketClient::isOpen() const
 	{
+		return mOpen;
+	}
+
+
+	void IWebSocketClient::connectionOpened()
+	{
+		mOpen = true;
+		onConnectionOpened();
+	}
+
+
+	void IWebSocketClient::connectionClosed(int code, const std::string& reason)
+	{
+		mOpen = false;
+		onConnectionClosed(code, reason);
+	}
+
+
+	void IWebSocketClient::connectionFailed(int code, const std::string& reason)
+	{
+		mOpen = false;
+		onConnectionFailed(code, reason);
+	}
+
+
+	bool WebSocketClient::init(utility::ErrorState& errorState)
+	{
+		if (!IWebSocketClient::init(errorState))
+			return false;
 		return true;
+	}
+
+
+	bool WebSocketClient::send(const std::string& message, EWebSocketOPCode code, nap::utility::ErrorState& error)
+	{
+		if (!error.check(isOpen(), "%s: client not connected to: %s", mID.c_str(), mURI.c_str()))
+			return false;
+
+		if (!mEndPoint->send(mConnection, message, code, error))
+			return false;
+
+		return true;
+	}
+
+
+	bool WebSocketClient::send(void const* payload, int length, EWebSocketOPCode code, nap::utility::ErrorState& error)
+	{
+		if (!error.check(isOpen(), "%s: client not connected to: %s", mID.c_str(), mURI.c_str()))
+			return false;
+
+		if (!mEndPoint->send(mConnection, payload, length, code, error))
+			return false;
+
+		return true;
+	}
+
+
+	bool WebSocketClient::send(const WebSocketMessage& message, nap::utility::ErrorState& error)
+	{
+		if (!error.check(isOpen(), "%s: client not connected to: %s", mID.c_str(), mURI.c_str()))
+			return false;
+
+		if (!mEndPoint->send(mConnection, message.getPayload(), message.getCode(), error))
+			return false;
+
+		return true;
+	}
+
+	void WebSocketClient::onConnectionOpened()
+	{
+		nap::utility::ErrorState error;
+		if (!send("hi there!", EWebSocketOPCode::Text, error))
+			assert(false);
+	}
+
+
+	void WebSocketClient::onConnectionClosed(int code, const std::string& reason)
+	{
+
+	}
+
+
+	void WebSocketClient::onConnectionFailed(int code, const std::string& reason)
+	{
+
 	}
 
 }
--- modules/napwebsocket/src/websocketclient.h
+++ modules/napwebsocket/src/websocketclient.h
@@ -7,6 +7,7 @@
 #include <nap/resource.h>
 #include <nap/resourceptr.h>
 #include <nap/signalslot.h>
+#include <atomic>
 
 namespace nap
 {
@@ -18,26 +19,89 @@ namespace nap
 		friend class WebSocketClientEndPoint;
 		RTTI_ENABLE(Resource)
 	public:
+		// Destructor
 		virtual ~IWebSocketClient();
 
 		/**
-		* Initialize this object after de-serialization
+		* Registers the web-socket client interface with the endpoint.
 		* @param errorState contains the error message when initialization fails
+		* @return if initialization succeeded.
 		*/
 		virtual bool init(utility::ErrorState& errorState) override;
 
 		/**
 		 * @return if the client is connected
 		 */
-		bool isConnected() const;
+		bool isOpen() const;
 
 		ResourcePtr<WebSocketClientEndPoint> mEndPoint;					///< Property: 'EndPoint' the client endpoint that manages all connections.
 		std::string mURI;												///< Property: "UIR" Server URI to open connection to.
 
 	protected:
-		WebSocketConnection mConnection;								///< Websocket connection
+		WebSocketConnection mConnection;								///< Web-socket connection
+
+		virtual void onConnectionOpened() = 0;
+		virtual void onConnectionClosed(int code, const std::string& reason) = 0;
+		virtual void onConnectionFailed(int code, const std::string& reason) = 0;
 
 	private:
-		nap::Signal<const WebSocketConnection&> disconnect;
+		// Called by web-socket client endpoint when the connection is opened
+		void connectionOpened();
+		// Called by web-socket client endpoint when the connection is closed
+		void connectionClosed(int code, const std::string& reason);
+		// Called by web-socket client endpoint when the connection failed to establish
+		void connectionFailed(int code, const std::string& reason);
+
+		std::atomic<bool> mOpen = { false };				///< If this client is currently connected
+		nap::Signal<const IWebSocketClient&> destroyed;		///< Called when the client is destroyed
+	};
+
+
+	//////////////////////////////////////////////////////////////////////////
+	// WebSocketClient
+	//////////////////////////////////////////////////////////////////////////
+
+	class NAPAPI WebSocketClient : public IWebSocketClient
+	{
+		RTTI_ENABLE(IWebSocketClient)
+	public:
+		/**
+		 * Initialize this object after de-serialization
+		 * @param errorState contains the error message when initialization fails.
+		 * @return if initialization succeeded.
+		 */
+		virtual bool init(utility::ErrorState& errorState) override;
+
+		/**
+		 * Sends a message with the given opcode to the server.
+		 * @param message the message to send
+		 * @param code message type
+		 * @param error contains the error if sending fails
+		 * @return if message was sent successfully
+		 */
+		bool send(const std::string& message, EWebSocketOPCode code, nap::utility::ErrorState& error);
+
+		/**
+		 * Sends a message using the given payload and opcode to the server.
+		 * @param payload the message buffer
+		 * @param length total number of bytes
+		 * @param code message type
+		 * @param error contains the error if sending fails
+		 * @return if message was sent successfully
+		 */
+		bool send(void const* payload, int length, EWebSocketOPCode code, nap::utility::ErrorState& error);
+
+		/**
+		 * Sends a message to the server.
+		 * @param message the message to send
+		 * @param error contains the error if sending fails
+		 * @return if message was sent successfully.
+		 */
+		bool send(const WebSocketMessage& message, nap::utility::ErrorState& error);
+
+	protected:
+		virtual void onConnectionOpened() override;
+		virtual void onConnectionClosed(int code, const std::string& reason) override;
+		virtual void onConnectionFailed(int code, const std::string& reason) override;
 	};
 }
