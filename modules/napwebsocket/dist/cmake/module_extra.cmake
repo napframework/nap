5088bad62f2752a062ca4ed5c35cf227e9fd80dc
cklosters <coen@naivi.nl>
2019-06-11 15:47:17 +0200

    - exposing asio headers

--- modules/napwebsocket/dist/cmake/module_extra.cmake
+++ modules/napwebsocket/dist/cmake/module_extra.cmake
@@ -5,4 +5,9 @@ if(NOT TARGET websocketpp)
     find_package(websocketpp REQUIRED)
 endif()
 
+if(NOT TARGET asio)
+    find_package(asio REQUIRED)
+endif()
+
 add_include_to_interface_target(mod_napwebsocket ${WEBSOCKETPP_INCLUDE_DIRS})
+add_include_to_interface_target(mod_napwebsocket ${ASIO_INCLUDE_DIRS})
