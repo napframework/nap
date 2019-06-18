4035b25b366102d00c8d0368df0c2c2cee605c5c
cklosters <coen@naivi.nl>
2019-06-18 15:32:41 +0200

    - moving findSerial for distribution to native dir

--- cmake/Findserial.cmake
+++ cmake/Findserial.cmake
@@ -5,12 +5,6 @@ find_path(SERIAL_DIR
           HINTS ${THIRDPARTY_DIR}/serial
           )
 
-if(SERIAL_DIR)
-    set(SERIAL_FOUND true)
-else()
-    set(SERIAL_FOUND false)
-endif()
-
 if(WIN32)
     set(SERIAL_LIBS_DEBUG ${SERIAL_DIR}/msvc/x64/Debug/serial.lib Ws2_32 winmm)
     set(SERIAL_LIBS_RELEASE ${SERIAL_DIR}/msvc/x64/Release/serial.lib Ws2_32 winmm)
@@ -22,14 +16,10 @@ else()
     set(SERIAL_LIBS_RELEASE ${SERIAL_DIR}/linux/Release/libserial.a)
 endif()
 
-if(SERIAL_FOUND)
-    message(STATUS "Found serial: ${SERIAL_DIR}")
-endif()
-
 # include directory is universal
 set(SERIAL_INCLUDE_DIRS ${SERIAL_DIR}/include)
 mark_as_advanced(SERIAL_INCLUDE_DIRS)
 
 # promote package for find
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(serial REQUIRED_VARS SERIAL_DIR SERIAL_INCLUDE_DIRS SERIAL_LIBS_DEBUG SERIAL_LIBS_RELEASE)
+find_package_handle_standard_args(serial REQUIRED_VARS SERIAL_DIR)
