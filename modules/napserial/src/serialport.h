7cba6ade2b8c48ad751d1bc275f4810ab61376bf
Coen Klosters <coen@naivi.nl>
2019-06-18 15:28:44 +0200

    fixing forward declaration Clang serial

--- modules/napserial/src/serialport.h
+++ modules/napserial/src/serialport.h
@@ -339,7 +339,7 @@ namespace nap
 		int mInterByteTimeout = 0;										///< Property: "InterByteTimeout" the max amount of time in ms between receiving bytes that can pass before a timeout occurs. Setting this to 0 will prevent inter byte timeouts.
 		bool mAllowFailure = false;										///< Property: "AllowFailure" when set to true opening the port is allowed to fail on startup.
 	private:
-		std::unique_ptr<serial::Serial> mSerialPort = nullptr;
+		std::unique_ptr<serial::Serial> mSerialPort;
 		
 	};
 }
