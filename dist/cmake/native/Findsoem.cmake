16aa48f9d214c7ad02d0b7b7cbaa61ab0cee62dc
cklosters <coen@naivi.nl>
2019-06-28 19:55:09 +0200

    - soem can  now be included as a module

--- dist/cmake/native/Findsoem.cmake
+++ dist/cmake/native/Findsoem.cmake
@@ -1,13 +1,23 @@
-# default artnet directory
-find_path(SOEM_DIR
+if(WIN32)
+  # default soem directory
+  find_path(SOEM_DIR
           NO_CMAKE_FIND_ROOT_PATH
-          NAMES soem/ethercat.h
+          NAMES include/soem/ethercat.h
           HINTS ${THIRDPARTY_DIR}/soem
           )
 
-if(WIN32)
-    set(SOEM_LIBS_DEBUG ${SOEM_DIR}/msvc/x64/Debug/soem.lib Ws2_32 winmm)
-    set(SOEM_LIBS_RELEASE ${SOEM_DIR}/msvc/x64/Release/soem.lib Ws2_32 winmm)
+    # library
+    set(SOEM_LIBS ${SOEM_DIR}/lib/soem.lib Ws2_32 winmm)
+
+    # wpcap dir
+    set(WPCAP_DIR ${THIRDPARTY_DIR}/soem/wpcap)
+
+    # all includes for soem windows
+    set(SOEM_INCLUDE_DIRS 
+        ${SOEM_DIR}/include 
+        ${SOEM_DIR}/include/soem 
+        ${WPCAP_DIR}/Include)
+
 elseif(APPLE)
     #set(SERIAL_LIBS_DEBUG ${SERIAL_DIR}/osx/Debug/libserial.a)
     #set(SERIAL_LIBS_RELEASE ${SERIAL_DIR}/osx/Release/libserial.a)
@@ -16,8 +26,6 @@ else()
     #set(SERIAL_LIBS_RELEASE ${SERIAL_DIR}/linux/Release/libserial.a)
 endif()
 
-# include directory is universal
-set(SOEM_INCLUDE_DIRS ${SOEM_DIR}/soem)
 mark_as_advanced(SOEM_INCLUDE_DIRS)
 
 # promote package for find
