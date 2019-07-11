06b50cba3d419ff7531b286602c97b6f86694e1b
cklosters <coen@naivi.nl>
2019-07-11 11:26:28 +0200

    - wpcap lib now included in SOEM module, necessary when resolving external symbols win64

--- modules/napsoem/src/soemservice.cpp
+++ modules/napsoem/src/soemservice.cpp
@@ -3,6 +3,7 @@
 
 // External includes
 #include <memory>
+#include <nap/logger.h>
 #include <soem/ethercat.h>
 
 RTTI_BEGIN_CLASS_NO_DEFAULT_CONSTRUCTOR(nap::SOEMService)
@@ -23,6 +24,17 @@ namespace nap
 
 	bool SOEMService::init(utility::ErrorState& error)
 	{
+		// Print adapter info
+		nap::Logger::info("SOEM: available network adapters: ");
+		ec_adaptert* it_adapter = ec_find_adapters();
+
+		int id(0);
+		while (it_adapter != NULL)
+		{
+			nap::Logger::info("%d: Device: %s, Description: %s", id, it_adapter->name, it_adapter->desc);
+			it_adapter = it_adapter->next;
+			id++;
+		}
 		return true;
 	}
 
