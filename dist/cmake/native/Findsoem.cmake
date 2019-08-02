58b387724d7a8cc0b8c18c7326a475625858e95d
Coen Klosters <coen@naivi.nl>
2019-08-02 14:12:45 +0200

    fixing soem include paths + dependencies

--- dist/cmake/native/Findsoem.cmake
+++ dist/cmake/native/Findsoem.cmake
@@ -23,8 +23,22 @@ if(WIN32)
         ${WPCAP_DIR}/Include)
 
 elseif(APPLE)
-    #set(SERIAL_LIBS_DEBUG ${SERIAL_DIR}/osx/Debug/libserial.a)
-    #set(SERIAL_LIBS_RELEASE ${SERIAL_DIR}/osx/Release/libserial.a)
+  # default soem directory
+  find_path(SOEM_DIR
+          NO_CMAKE_FIND_ROOT_PATH
+          NAMES include/soem/ethercat.h
+          HINTS ${THIRDPARTY_DIR}/soem
+          )
+    # library
+    set(SOEM_LIBS 
+      ${SOEM_DIR}/lib/libsoem.a
+      pcap
+      pthread)
+
+    # all includes for soem windows
+    set(SOEM_INCLUDE_DIRS 
+        ${SOEM_DIR}/include 
+        ${SOEM_DIR}/include/soem)
 else()
     #set(SERIAL_LIBS_DEBUG ${SERIAL_DIR}/linux/Debug/libserial.a)
     #set(SERIAL_LIBS_RELEASE ${SERIAL_DIR}/linux/Release/libserial.a)
