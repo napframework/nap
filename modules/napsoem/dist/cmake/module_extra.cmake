521c910a0e7ce9f2c3379b2fb428bbc55fe6c103
cklosters <coen@naivi.nl>
2019-08-02 12:41:18 +0200

    - fixing soem cmake includes for win package release

--- modules/napsoem/dist/cmake/module_extra.cmake
+++ modules/napsoem/dist/cmake/module_extra.cmake
@@ -1,12 +1,16 @@
 # Install readme
 install(FILES ${THIRDPARTY_DIR}/soem/LICENSE DESTINATION licenses/soem)
 
-include(${NAP_ROOT}/cmake/dist_shared_crossplatform.cmake)
-
 if(NOT TARGET soem)
     find_package(soem REQUIRED)
 endif()
 set(MODULE_NAME_EXTRA_LIBS soem)
 
-add_include_to_interface_target(mod_napsoem ${SOEM_INCLUDE_DIRS})
+add_include_to_interface_target(mod_napsoem ${SOEM_DIR}/include)
+add_include_to_interface_target(mod_napsoem ${SOEM_DIR}/include/soem)
+
+if(WIN32)
+	add_include_to_interface_target(mod_napsoem ${WPCAP_DIR}/Include))
+endif()
+
 add_define_to_interface_target(mod_napsoem __STDC_LIMIT_MACROS)
