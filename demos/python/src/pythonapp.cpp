c473b20be0a70f7984f3f65e142244341701fbbe
cklosters <coen@naivi.nl>
2019-08-09 10:47:34 +0200

    fixing compile error merge

--- demos/python/src/pythonapp.cpp
+++ demos/python/src/pythonapp.cpp
@@ -87,7 +87,7 @@ namespace nap
 	void PythonApp::render()
 	{
 		// Clear opengl context related resources that are not necessary any more
-		mRenderService->destroyGLContextResources({ mRenderWindow });
+		mRenderService->destroyGLContextResources({ mRenderWindow.get() });
 
 		// Activate current window for drawing
 		mRenderWindow->makeActive();
