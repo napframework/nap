6fd8b3640fc97252b0c0f29eaaf3ec7e68661ab3
Ritesh Oedayrajsingh Varma <rovarma@gmail.com>
2019-08-12 10:05:17 +0200

    Fixed issue with array uniforms needing to be addressed using the array specifier in json
    - Array uniforms are now added with the array specifiers stripped off; this ensures that you can address a uniform array in json as, for example, 'inputs', rather than 'inputs[0]'
    - Added some documentation around functions that were missing it

--- modules/naprender/src/uniformbinding.cpp
+++ modules/naprender/src/uniformbinding.cpp
@@ -99,6 +99,44 @@ namespace nap
 
 	Uniform& UniformContainer::addUniform(std::unique_ptr<Uniform> uniform, const opengl::UniformDeclaration& declaration)
 	{
+		// The name in the declaration is the name as returned by OpenGL. This means it's the full 'path' to the uniform.
+		// The path also includes the array specifier. For example, if you have the following in your shader:
+		//
+		// uniform float inputs[2]
+		// 
+		// The name of this uniform will be returned as 'inputs[0]'. However, this is not what we want; we want the user to be able to address
+		// this uniform simply by using its name, in this case 'inputs' without the array specifier.
+		//
+		// We can't just simply strip off all array specifiers, as there might be array elements earlier on the path; we can only strip off the trailing array specifier.
+		// For example, if you have the following in your shader:
+		//
+		// struct SomeStruct
+		// {
+		//     float mValues[2];
+		// }
+		//
+		// uniform SomeStruct structs[2];
+		//
+		// And, assuming you use these uniforms, the uniforms returned will be:
+		//
+		// structs[0].mValues[0]
+		// structs[1].mValues[0]
+		//
+		// In this case, we need to ensure we only strip off the trailing array specifier; the array specifier after the 'structs' name is important information about which 
+		// array element is being indexed.
+		std::string name = declaration.mName;
+
+		// Determine where we should start searching for the array specifier: if this is a path with multiple elements, we start at the start of the last element.
+		// If it's only a single element path, we search from the start.
+		size_t bracket_start_search_pos = name.find_last_of('.');
+		if (bracket_start_search_pos == std::string::npos)
+			bracket_start_search_pos = 0;
+
+		// Find the array specifier starting at the position we found above; if found, strip it off.
+		size_t bracket_pos = name.find_first_of('[', bracket_start_search_pos);
+		if (bracket_pos != std::string::npos)
+			name = name.substr(0, bracket_pos);
+
 		// Create association between uniform and declaration. At the same time, split between textures and values
 		// as texture have a slightly different interface.
 		std::unique_ptr<UniformTexture> texture_uniform = rtti_cast<UniformTexture>(uniform);
@@ -106,12 +144,12 @@ namespace nap
 		{
 			std::unique_ptr<UniformValue> value_uniform = rtti_cast<UniformValue>(uniform);
 			assert(value_uniform);
-			auto inserted = mUniformValueBindings.emplace(std::make_pair(declaration.mName, UniformBinding(std::move(value_uniform), declaration)));
+			auto inserted = mUniformValueBindings.emplace(std::make_pair(name, UniformBinding(std::move(value_uniform), declaration)));
 			return *inserted.first->second.mUniform;
 		}
 		else
 		{
-			auto inserted = mUniformTextureBindings.emplace(std::make_pair(declaration.mName, UniformBinding(std::move(texture_uniform), declaration)));
+			auto inserted = mUniformTextureBindings.emplace(std::make_pair(name, UniformBinding(std::move(texture_uniform), declaration)));
 			return *inserted.first->second.mUniform;
 		}
 	}
