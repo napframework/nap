037912edb699a3d2c9a35506f8df96a3672e3c8b
Chris Heywood <31988069+creywood@users.noreply.github.com>
2019-10-28 14:37:24 +0100

    Start on check_build_environment for source users

--- dist/linux/check_build_environment/check_build_environment_worker.py
+++ dist/linux/check_build_environment/check_build_environment_worker.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import os
+from packaging import version
 import subprocess
+import shutil
 import sys
 
 REQUIRED_UBUNTU_VERSION = '18.04'
+REQUIRED_QT_VERSION = '5.11.3'
 
 
 def call(cmd):
@@ -13,6 +16,14 @@ def call(cmd):
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     (out, _) = proc.communicate()
     return out.strip().decode('utf-8')
+
+def call_with_returncode(cmd):
+    """Execute command and return stdout and returncode"""
+
+    # print("Command: %s" % cmd)
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    (out, _) = proc.communicate()
+    return (out.strip().decode('utf-8'), proc.returncode)
 
 def log_test_success(test_item, success):
     """Log a test success"""
@@ -59,6 +70,66 @@ def check_compiler():
 
     log_test_success('C++ is GCC', gcc_ok)
     return gcc_ok 
+    
+def check_for_thirdparty():
+    """Check for the thirdparty repository"""
+    
+    nap_root = get_nap_root()
+    thirdparty_ok = os.path.exists(os.path.join(nap_root, os.pardir, 'thirdparty'))
+    log_test_success('for third party repository', thirdparty_ok)
+    return thirdparty_ok
+    
+def check_qt_env_var():
+    """Check Qt env. var. for source user"""
+
+    qt_env_var_ok = 'QT_DIR' in os.environ
+    log_test_success('Qt environment variable', qt_env_var_ok)        
+    return qt_env_var_ok
+    
+def check_qt_version():
+    """Check Qt version for source user"""
+
+    qt_found_version = None
+    qt_version_ok = False
+    
+    # Remove temporary directory for CMake project files to go into if it exists 
+    nap_root = get_nap_root()
+    qt_checker_path = os.path.join(nap_root, 'dist', 'cmake', 'qt_checker')
+    temp_build_dir = os.path.join(qt_checker_path, 'project_temp')
+    if os.path.exists(temp_build_dir):
+        shutil.rmtree(temp_build_dir)
+    
+    # Run Qt version checking logic, parsing output
+    thirdparty_dir = os.path.join(os.pardir, 'thirdparty')
+    cmake = os.path.join(thirdparty_dir, 'cmake', 'linux', 'install', 'bin', 'cmake')
+    (out, returncode) = call_with_returncode(' '.join((cmake, qt_checker_path, '-B', temp_build_dir)))
+    if returncode == 0:
+        lines = out.split('\n')
+        for line in lines:
+            if 'Found Qt' in line:
+                chunks = line.strip().split('Found Qt')
+                if len(chunks) > 1:
+                    qt_found_version = chunks[-1].strip()
+                break
+    
+    # OK is version matching required version
+    if not qt_found_version is None:
+        qt_version_ok = version.parse(qt_found_version) == version.parse(REQUIRED_QT_VERSION)
+    
+    # Cleanup
+    if os.path.exists(temp_build_dir):
+        shutil.rmtree(temp_build_dir)
+    
+    log_test_success('Qt version v%s' % REQUIRED_QT_VERSION, qt_version_ok)        
+    return (qt_version_ok, qt_found_version)
+    
+def log_qt_help(qt_env_var_ok, qt_found_version):
+    """Log help for source user Qt problems"""
+
+    if not qt_env_var_ok:
+        print("\nThis version of NAP requires Qt v%s as downloaded directly from Qt. Distribution versions are not supported. Once Qt v%s has been downloaded it should be pointed to with the environment variable QT_DIR, eg. QT_DIR=\"/home/username/Qt%s/%s/gcc_64\"." % (REQUIRED_QT_VERSION, REQUIRED_QT_VERSION, REQUIRED_QT_VERSION, REQUIRED_QT_VERSION))
+    else:
+        print("\nThis version of NAP requires Qt v%s, however you appear to have v%s. Other versions are currently unsupported." % (REQUIRED_QT_VERSION, qt_found_version))    
 
 def read_yes_no(question):
     """Read a yes/no answer for a question"""
@@ -79,8 +150,27 @@ def read_yes_no(question):
            return False
         else:
            print("Please respond with 'yes' or 'no'\n")
+         
+def get_nap_root():
+    """Get framework root directory"""
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    return os.path.abspath(os.path.join(script_dir, os.pardir, os.pardir, os.pardir))
+    
+def check_and_warn_for_potential_system_qt():
+    """Attempt to detect if the Qt installation might be a system version and warn if so"""
+    
+    running_apt_distro = call('which apt') != ''
+    if running_apt_distro:
+        (_, returncode) = call_with_returncode("dpkg -l qtdeclarative5-dev")
+        if returncode == 0:
+            print("\nWarning: You appear to have Qt development packages installed via your system packaging system. This may cause issues, espcially with packaging.")
+    
+    qt_dir = os.environ['QT_DIR']
+    qt_dir = os.path.abspath(qt_dir)
+    if qt_dir.startswith('/usr/') and not qt_dir.startswith('/usr/local'):
+        print("\nWarning: Your Qt appears to be located in a system directory. Ensure that you are using the distribution directly from Qt instead of your system packaged version.")
 
-def check_build_environment():
+def check_build_environment(against_source):
     """Check whether Linux build environment appears ready for NAP"""
 
     # Check if 64bit
@@ -92,7 +182,7 @@ def check_build_environment():
     # Check distribution
     distribution_ok = check_distribution()
     if not distribution_ok:
-        print("\nThis version of NAP supports Ubuntu Linux (%s).  Other distributions may work but are unsupported." % REQUIRED_UBUNTU_VERSION)
+        print("\nThis version of NAP supports Ubuntu Linux (%s). Other distributions may work but are unsupported." % REQUIRED_UBUNTU_VERSION)
         print("Hint for the adventurous: On Ubuntu we depend on build-essential, libglu1-mesa-dev and patchelf")
         print("\nNot continuing checks.")
         sys.exit(1)
@@ -115,10 +205,38 @@ def check_build_environment():
     glut_intalled = apt_package_installed('libglu1-mesa-dev')
     log_test_success('for libglu1-mesa-dev package', glut_intalled)
 
+    if against_source:
+        # Check for thirdparty repo
+        thirdparty_ok = check_for_thirdparty()
+
+        if not thirdparty_ok:
+            print("\nThe third party repository ('thirdparty') needs to be cloned alongside the main repository.")
+            print("\nNot continuing checks. Re-run this script after cloning.")
+            sys.exit(1)
+
+        # Check for Qt
+        qt_env_var_ok = check_qt_env_var()
+        
+        # Check Qt version
+        if qt_env_var_ok:
+            (qt_version_ok, qt_found_version) = check_qt_version()
+        else:
+            qt_version_ok = False
+            qt_found_version = None
+
     print("")
+    
+    # If we're running for source users check source requirements are met
+    extra_source_requirements_ok = True
+    if against_source and (not thirdparty_ok or not qt_env_var_ok or not qt_version_ok):
+        extra_source_requirements_ok = False 
 
     # If everything looks good log and exit
-    if distribution_version_ok and build_essential_installed and patchelf_installed and compiler_ok:
+    if distribution_version_ok \
+      and build_essential_installed \
+      and patchelf_installed \
+      and compiler_ok \
+      and extra_source_requirements_ok:
         print("Your build environment appears to be ready for NAP!")
         return False
 
@@ -126,7 +244,7 @@ def check_build_environment():
 
     # Warn about wrong Ubuntu version
     if not distribution_version_ok:
-        print("\nWarning: This version of NAP is supported on Ubuntu %s.  Other Linux configurations may work but are unsupported." % REQUIRED_UBUNTU_VERSION)
+        print("\nWarning: This version of NAP is supported on Ubuntu %s. Other Linux configurations may work but are unsupported." % REQUIRED_UBUNTU_VERSION)
 
     if not compiler_ok:
         print("\nYour C++ compiler is not currently set to GCC. This release of NAP only currently supports GCC.")
@@ -149,9 +267,19 @@ def check_build_environment():
             subprocess.call('sudo apt-get install %s' % package_str, shell=True)
             return True
         else:
-            print("Re-run checkBuildEnvironment once you have installed the requirements.")
+            print("Re-run check_build_environment once you have installed the requirements.")
             return False
+            
+    # Show Qt help
+    if against_source :
+        if (not qt_env_var_ok or not qt_version_ok):
+            log_qt_help(qt_env_var_ok, qt_found_version)
+            
+        if qt_env_var_ok:
+            check_and_warn_for_potential_system_qt()
+        
 
 if __name__ == '__main__':
-     while check_build_environment():
+    against_source = len(sys.argv) > 1 and sys.argv[1] == '--source'
+    while check_build_environment(against_source):
          print("\nRe-running checks...\n")
