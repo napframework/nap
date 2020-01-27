a1bbe11eb3d0729bf33c330749f2f92b5f63b040
cklosters <coen@naivi.nl>
2020-01-27 13:22:00 +0100

    - adding 16 bit and 32 bit format options to rendertarget

--- modules/naprender/src/rendertexture2d.cpp
+++ modules/naprender/src/rendertexture2d.cpp
@@ -3,7 +3,13 @@
 RTTI_BEGIN_ENUM(nap::RenderTexture2D::EFormat)
 	RTTI_ENUM_VALUE(nap::RenderTexture2D::EFormat::RGBA8,	"RGBA8"),
 	RTTI_ENUM_VALUE(nap::RenderTexture2D::EFormat::RGB8,	"RGB8"),
-	RTTI_ENUM_VALUE(nap::RenderTexture2D::EFormat::R8,		"R8"),	
+	RTTI_ENUM_VALUE(nap::RenderTexture2D::EFormat::R8,		"R8"),
+	RTTI_ENUM_VALUE(nap::RenderTexture2D::EFormat::RGBA16,	"RGBA16"),
+	RTTI_ENUM_VALUE(nap::RenderTexture2D::EFormat::RGB16,	"RGB16"),
+	RTTI_ENUM_VALUE(nap::RenderTexture2D::EFormat::R16,		"R16"),
+	RTTI_ENUM_VALUE(nap::RenderTexture2D::EFormat::RGBA32,	"RGBA32"),
+	RTTI_ENUM_VALUE(nap::RenderTexture2D::EFormat::RGB32,	"RGB32"),
+	RTTI_ENUM_VALUE(nap::RenderTexture2D::EFormat::R32,		"R32"),
 	RTTI_ENUM_VALUE(nap::RenderTexture2D::EFormat::Depth,	"Depth")
 RTTI_END_ENUM
 
@@ -39,6 +45,36 @@ namespace nap
 			settings.mFormat			= GL_RED;
 			settings.mInternalFormat 	= GL_R8;
 			settings.mType				= GL_UNSIGNED_BYTE;
+			break;
+		case EFormat::RGBA16:
+			settings.mFormat			= GL_RGBA;
+			settings.mInternalFormat	= GL_RGBA16;
+			settings.mType				= GL_UNSIGNED_SHORT;
+			break;
+		case EFormat::RGB16:
+			settings.mFormat			= GL_RGB;
+			settings.mInternalFormat	= GL_RGB16;
+			settings.mType				= GL_UNSIGNED_SHORT;
+			break;
+		case EFormat::R16:
+			settings.mFormat			= GL_R;
+			settings.mInternalFormat	= GL_R16;
+			settings.mType				= GL_UNSIGNED_SHORT;
+			break;
+		case EFormat::RGBA32:
+			settings.mFormat			= GL_RGBA;
+			settings.mInternalFormat	= GL_RGBA32F;
+			settings.mType				= GL_FLOAT;
+			break;
+		case EFormat::RGB32:
+			settings.mFormat			= GL_RGB;
+			settings.mInternalFormat	= GL_RGB32F;
+			settings.mType				= GL_FLOAT;
+			break;
+		case EFormat::R32:
+			settings.mFormat			= GL_R;
+			settings.mInternalFormat	= GL_R32F;
+			settings.mType				= GL_FLOAT;
 			break;
 		case EFormat::Depth:
 			settings.mFormat			= GL_DEPTH_COMPONENT;
