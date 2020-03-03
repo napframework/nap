646601cd8cd33b52bea0743d80fa6004b76d0132
cklosters <coen@naivi.nl>
2020-03-03 13:54:41 +0100

    -remove glm license copy from mod_napcolor

--- modules/napcolor/dist/cmake/module_extra.cmake
+++ modules/napcolor/dist/cmake/module_extra.cmake
@@ -5,6 +5,3 @@ if(NOT TARGET glm)
 endif()
 
 add_include_to_interface_target(mod_napscene ${GLM_INCLUDE_DIRS})
-
-# Install thirdparty licenses into lib
-install(FILES ${THIRDPARTY_DIR}/glm/copying.txt DESTINATION licenses/glm)
