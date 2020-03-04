76a2f8bc85f80c5aedbde851d65947a0a32f464e
cklosters <coen@naivi.nl>
2020-03-04 13:05:25 +0100

    - adding custom assert with message macro

---
+++ core/src/nap/assert.cpp
@@ -0,0 +1,30 @@
+#include "assert.h"
+
+namespace nap
+{
+	void _assert(const char* exprs, bool expr, const char* file, int line)
+	{
+		if (!expr)
+		{
+			std::cerr << "Assertion failed: " <<
+				exprs << ", source: " <<
+				file  << ", line " <<
+				line  << "\n";
+			abort();
+		}
+	}
+
+
+	void _assertMsg(const char* exprs, bool expr, const char* file, int line, const char* msg)
+	{
+		if (!expr)
+		{
+			std::cerr << "Assertion failed: " <<
+				exprs << ", source: " <<
+				file  << ", line " <<
+				line  << ", msg: " << 
+				msg   << "\n";
+			abort();
+		}
+	}
+}
