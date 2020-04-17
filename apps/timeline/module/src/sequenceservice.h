0513bb2d20b1ff33d23bb5ce9089b74910d4d5da
Tim Groeneboom <tim@timgroeneboom.com>
2020-04-17 14:26:23 +0200

    - added some comments

--- apps/timeline/module/src/sequenceservice.h
+++ apps/timeline/module/src/sequenceservice.h
@@ -9,9 +9,18 @@
 
 namespace nap
 {
+	//////////////////////////////////////////////////////////////////////////
+
+	// forward declares
 	class SequenceEventReceiver;
 	class SequencePlayerParameterSetterBase;
 
+	/**
+	 * Main interface for processing sequence events and setting the parameters on the main thread
+	 * All SequenceEventReceivers and ParameterSetters are registered and de-registered with this service on initialization and destruction
+	 * This service consumes all received sequence events and forwards them to all registered SequenceEventReceivers
+	 * Also, the service sets all parameter values that are tied to a sequenceplayer and its tracks when the user wants parameters to be set on main thread
+	 */
 	class NAPAPI SequenceService : public Service
 	{
 		friend class SequenceEventReceiver;
@@ -27,9 +36,9 @@ namespace nap
 
 	protected:
 		/**
-		* Registers all objects that need a specific way of construction
-		* @param factory the factory to register the object creators with
-		*/
+		 * Registers all objects that need a specific way of construction
+		 * @param factory the factory to register the object creators with
+		 */
 		virtual void registerObjectCreators(rtti::Factory& factory) override;
 
 		virtual bool init(nap::utility::ErrorState& errorState) override;
