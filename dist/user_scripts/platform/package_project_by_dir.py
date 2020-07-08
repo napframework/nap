89b652ff99a3d266919c2bf53183cd595109748d
Chris Heywood <31988069+creywood@users.noreply.github.com>
2020-07-08 14:39:22 +1000

    Quick pre v0.4 build Python cleanup

--- dist/user_scripts/platform/package_project_by_dir.py
+++ dist/user_scripts/platform/package_project_by_dir.py
@@ -7,6 +7,33 @@ import sys
 
 from nap_shared import read_console_char, get_python_path
 
+def package_project_by_dir(project_path, include_napkin, zip_package, show, pause_on_package):
+    project_name = os.path.basename(project_path.strip('\\'))
+    nap_root = os.path.abspath(os.path.join(project_path, os.pardir, os.pardir))
+    script_path = os.path.join(nap_root, 'tools', 'platform', 'package_project_by_name.py')
+
+    # Determine our Python interpreter location
+    python = get_python_path()
+
+    # Build our command
+    cmd = [python, script_path, project_name]
+    if not include_napkin:
+        cmd.append('--no-napkin')
+    if not zip_package:
+        cmd.append('--no-zip')
+    if not show:
+        cmd.append('--no-show')
+    exit_code = call(cmd)
+
+    # Pause to display output in case we're running from Windows Explorer / macOS Finder
+    if pause_on_package:
+        print("Press key to close...")
+
+        # Read a char from console
+        read_console_char()
+
+    return exit_code
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='package')
     parser.add_argument("PROJECT_PATH", type=str, help=argparse.SUPPRESS)
@@ -21,29 +48,10 @@ if __name__ == '__main__':
                             help="Don't pause afterwards")
     args = parser.parse_args()
 
-    project_name = os.path.basename(args.PROJECT_PATH.strip('\\'))
-    nap_root = os.path.abspath(os.path.join(args.PROJECT_PATH, os.pardir, os.pardir))
-    script_path = os.path.join(nap_root, 'tools', 'platform', 'package_project_by_name.py')
-
-    # Determine our Python interpreter location
-    python = get_python_path()
-
-    # Build our command
-    cmd = [python, script_path, project_name]
-    if args.no_napkin:
-        cmd.append('--no-napkin')
-    if args.no_zip:
-        cmd.append('--no-zip')
-    if args.no_show:
-        cmd.append('--no-show')
-    exit_code = call(cmd)
-
-    # Pause to display output in case we're running from Windows Explorer / macOS Finder
+    pause_on_package = False
     if not sys.platform.startswith('linux') and not args.no_pause:
-        print("Press key to close...")
-
-        # Read a char from console
-        read_console_char()
+        pause_on_package = True
+    exit_code = package_project_by_dir(args.PROJECT_PATH, not args.no_napkin, not args.no_zip, not args.no_show, pause_on_package)
 
     # Expose exit code
     sys.exit(exit_code)
