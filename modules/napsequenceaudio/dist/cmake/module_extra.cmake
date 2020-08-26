486739fae125453eaec2dc5e4b605634480f76d1
cklosters <coen@naivi.nl>
2020-08-26 11:49:59 +0200

    - removing mod_napsequenceaudio and sequenceraudio demo - demos and modules that are not part of the main cmakelist should never be part of master - moved to: https://github.com/naivisoftware/nap/tree/sequencer_audio

--- modules/napsequenceaudio/dist/cmake/module_extra.cmake
+++
@@ -1,7 +0,0 @@
-include(${NAP_ROOT}/cmake/dist_shared_crossplatform.cmake)
-
-if(NOT TARGET glm)
-    find_package(glm REQUIRED)
-endif()
-
-add_include_to_interface_target(mod_napsequence ${GLM_INCLUDE_DIRS})
--- modules/napsequenceaudio/src/mod_sequenceaudio.cpp
+++
@@ -1,3 +0,0 @@
-#include <utility/module.h>
-
-NAP_SERVICE_MODULE("mod_napsequenceaudio", "0.2.0")
