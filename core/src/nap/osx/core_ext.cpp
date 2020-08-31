10abf85b485094bc1573e8da46519d3924854638
Chris Heywood <31988069+creywood@users.noreply.github.com>
2020-08-31 13:02:30 +1000

    Use path mapping system for macOS MoltenVK ICD path population

--- core/src/nap/osx/core_ext.cpp
+++ core/src/nap/osx/core_ext.cpp
@@ -12,18 +12,17 @@ namespace nap
     {
         // Build Vulkan ICD path environment variable. We're currently blatting this blindly, which
         // won't do any harm, even if the project isn't using graphics.
-        
-        // TODO Once napkin_relocation has merged use path mapping instead to locate thirdparty
-        
-        // For Source context
-        std::string exe_dir = utility::getExecutableDir();
-        std::string icd_paths = exe_dir + "/../../../thirdparty/vulkansdk/osx/share/vulkan/icd.d/MoltenVK_icd.json";
 
-        // For Framework Release context
-        icd_paths.append(":" + exe_dir + "/../../../../thirdparty/vulkansdk/share/vulkan/icd.d/MoltenVK_icd.json");
-
-        // For Packaged App context
-        icd_paths.append(":" + exe_dir + "/lib/vulkansdk/share/vulkan/icd.d/MoltenVK_icd.json");
+        // These three in order represent the three contexts: Source, Framework Release and 
+        // Packaged App
+		std::vector<std::string> paths({
+            "{ROOT}/../thirdparty/vulkansdk/osx/share/vulkan/icd.d/MoltenVK_icd.json",
+            "{ROOT}/thirdparty/vulkansdk/share/vulkan/icd.d/MoltenVK_icd.json",
+            "{EXE_DIR}/lib/MoltenVK_icd.json"
+		});
+		mProjectInfo->patchPaths(paths);
+        
+        std::string icd_paths = utility::joinString(paths, ":");
         setenv("VK_ICD_FILENAMES", icd_paths.c_str(), 1);
     }
 }
