d62c01d52f620e2b3a81cd0b66bd1929a94714a1
Coen Klosters <coen@naivi.nl>
2020-09-09 00:12:11 +0200

    - optimizing move construction of lerping.

--- modules/napmath/src/mathutils.cpp
+++ modules/napmath/src/mathutils.cpp
@@ -58,33 +58,35 @@ namespace nap
 		template<>
 		glm::vec4 lerp<glm::vec4>(const glm::vec4& start, const glm::vec4& end, float percent)
 		{
-			glm::vec4 return_v;
-			return_v.x = lerp<float>(start.x, end.x, percent);
-			return_v.y = lerp<float>(start.y, end.y, percent);
-			return_v.z = lerp<float>(start.z, end.z, percent);
-			return_v.w = lerp<float>(start.w, end.w, percent);
-			return return_v;
+			return {
+				lerp<float>(start.x, end.x, percent),
+				lerp<float>(start.y, end.y, percent),
+				lerp<float>(start.z, end.z, percent),
+				lerp<float>(start.w, end.w, percent)
+			};
 		}
 
 
 		template<>
 		glm::vec3 lerp<glm::vec3>(const glm::vec3& start, const glm::vec3& end, float percent)
 		{
-			glm::vec3 return_v;
-			return_v.x = lerp<float>(start.x, end.x, percent);
-			return_v.y = lerp<float>(start.y, end.y, percent);
-			return_v.z = lerp<float>(start.z, end.z, percent);
-			return return_v;
+			return
+			{
+				lerp<float>(start.x, end.x, percent),
+				lerp<float>(start.y, end.y, percent),
+				lerp<float>(start.z, end.z, percent)
+			};
 		}
 
 
 		template<>
 		glm::vec2 lerp<glm::vec2>(const glm::vec2& start, const glm::vec2& end, float percent)
 		{
-			glm::vec2 return_v;
-			return_v.x = lerp<float>(start.x, end.x, percent);
-			return_v.y = lerp<float>(start.y, end.y, percent);
-			return return_v;
+			return
+			{
+				lerp<float>(start.x, end.x, percent),
+				lerp<float>(start.y, end.y, percent)
+			};
 		}
 
 
