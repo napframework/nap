63b7d94d236b9ecc58d2762433c89c0278d86b0f
cklosters <coen@naivi.nl>
2021-01-21 18:43:16 +0100

    - adding gnomon mesh, shader and render gnomon component - makes it easy to add an origin gnomon to every project that uses rendering

---
+++ modules/naprender/src/gnomonshader.h
@@ -0,0 +1,28 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#pragma once
+
+// External Includes
+#include <shader.h>
+
+namespace nap
+{
+	/**
+	 * Hard coded shader to display the nap::GnomonMesh
+	 */
+	class NAPAPI GnomonShader : public Shader
+	{
+		RTTI_ENABLE(Shader)
+	public:
+		GnomonShader(Core& core);
+
+		/**
+		 * Cross compiles the GLSL shader code to SPIR-V, creates the shader module and parses all the uniforms and samplers.
+		 * @param errorState contains the error if initialization fails.
+		 * @return if initialization succeeded.
+		 */
+		virtual bool init(utility::ErrorState& errorState) override;
+	};
+}
