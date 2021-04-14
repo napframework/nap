7f95942366d58a6556c60470485091e0c8a46ed6
cklosters <coen@naivi.nl>
2021-04-14 15:59:37 +0200

    - cleaning up cmake

--- modules/naptween/dist/cmake/module_extra.cmake
+++ modules/naptween/dist/cmake/module_extra.cmake
@@ -1,7 +1,8 @@
 include(${NAP_ROOT}/cmake/dist_shared_crossplatform.cmake)
 
 if(NOT TARGET glm)
+	set(GLM_FIND_QUIETLY TRUE)
     find_package(glm REQUIRED)
 endif()
 
-add_include_to_interface_target(mod_napsequence ${GLM_INCLUDE_DIRS})
+add_include_to_interface_target(mod_naptween ${GLM_INCLUDE_DIRS})
