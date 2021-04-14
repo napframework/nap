7f73927bb7b45dc80c6f24c89733264ca32026e6
cklosters <coen@naivi.nl>
2021-04-14 16:34:06 +0200

    - removing GLM package requirement, already included and exposed by mod_napmath

--- modules/naptween/dist/cmake/module_extra.cmake
+++
@@ -1,8 +0,0 @@
-include(${NAP_ROOT}/cmake/dist_shared_crossplatform.cmake)
-
-if(NOT TARGET glm)
-	set(GLM_FIND_QUIETLY TRUE)
-    find_package(glm REQUIRED)
-endif()
-
-add_include_to_interface_target(mod_naptween ${GLM_INCLUDE_DIRS})
