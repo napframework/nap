8fd6369bf2fcb6ad2cc9afda052bc7a90b0e2436
cklosters <coen@naivi.nl>
2021-08-31 12:46:27 +0200

    removing 'HAVE_CONFIG_H' define in websocket module

--- modules/napasio/src/mod_napasio.cpp
+++ modules/napasio/src/mod_napasio.cpp
@@ -2,6 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "utility/module.h"
+#include <utility/module.h>
 
 NAP_MODULE("mod_napasio", "0.1.0")
