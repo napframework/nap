823bc8559b831bc1ff8c0efca67c46e53c21ea19
Thijs van Hoof <thijs.v.h@gmail.com>
2021-09-06 14:51:16 +0200

    Add Art-Net demo with ArtNet receiving example

---
+++ demos/artnetdemo/module/src/artnethandler.cpp
@@ -0,0 +1,46 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "artnethandler.h"
+
+ // External includes
+#include <entity.h>
+#include <artnetinputcomponent.h>
+#include <algorithm>
+
+// Register Art-Net Handler Component
+RTTI_BEGIN_CLASS(nap::ArtNetHandlerComponent)
+	RTTI_PROPERTY("Input", &nap::ArtNetHandlerComponent::mInput, nap::rtti::EPropertyMetaData::Required)
+RTTI_END_CLASS
+
+// Register Art-Net Handler Component Instance
+RTTI_BEGIN_CLASS_NO_DEFAULT_CONSTRUCTOR(nap::ArtNetHandlerComponentInstance)
+	RTTI_CONSTRUCTOR(nap::EntityInstance&, nap::Component&)
+RTTI_END_CLASS
+
+namespace nap
+{
+
+	bool ArtNetHandlerComponentInstance::init(utility::ErrorState& errorState)
+	{
+		mInput->packetReceived.connect(eventReceivedSlot);
+		return true;
+	}
+
+
+	const std::vector<float>& ArtNetHandlerComponentInstance::getData()
+	{
+		return mReceivedData;
+	}
+
+
+	void ArtNetHandlerComponentInstance::onEventReceived(const ArtNetEvent& event)
+	{
+		mReceivedData.clear();
+
+		// Store data as float so we can render it directly in the ImGui histogram
+		for (int16_t i = 0; i < event.getChannelCount(); i++)
+			mReceivedData.push_back(static_cast<float>(event[i]));
+	}
+}
---
+++ demos/artnetdemo/src/artnetdemo.cpp
@@ -0,0 +1,182 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+// Local Includes
+#include "artnetdemo.h"
+
+// External Includes
+#include <utility/fileutils.h>
+#include <nap/logger.h>
+#include <inputrouter.h>
+#include <rendergnomoncomponent.h>
+#include <perspcameracomponent.h>
+#include <artnethandler.h>
+#include <algorithm>
+#include <sstream>
+
+RTTI_BEGIN_CLASS_NO_DEFAULT_CONSTRUCTOR(nap::ArtNetDemo)
+	RTTI_CONSTRUCTOR(nap::Core&)
+RTTI_END_CLASS
+
+namespace nap
+{
+	/**
+	 * Initialize all the resources and instances used for drawing
+	 * slowly migrating all functionality to NAP
+	 */
+	bool ArtNetDemo::init(utility::ErrorState& error)
+	{
+		// Retrieve services
+		mRenderService	= getCore().getService<nap::RenderService>();
+		mSceneService	= getCore().getService<nap::SceneService>();
+		mInputService	= getCore().getService<nap::InputService>();
+		mGuiService		= getCore().getService<nap::IMGuiService>();
+
+		// Fetch the resource manager
+		mResourceManager = getCore().getResourceManager();
+
+		// Convert our path and load resources from file
+		if (!mResourceManager->loadFile("artnetdemo.json", error))
+			return false;
+
+		// Get the render window
+		mRenderWindow = mResourceManager->findObject<nap::RenderWindow>("Window");
+		if (!error.check(mRenderWindow != nullptr, "unable to find render window with name: %s", "Window"))
+			return false;
+
+		// Get the scene that contains our entities and components
+		mScene = mResourceManager->findObject<Scene>("Scene");
+		if (!error.check(mScene != nullptr, "unable to find scene with name: %s", "Scene"))
+			return false;
+
+		// Get the Art-Net entity
+		mArtNetEntity = mScene->findEntity("ArtNetEntity");
+		if (!error.check(mArtNetEntity != nullptr, "unable to find Art-Net entity with name: %s", "ArtNetEntity"))
+			return false;
+
+		// All done!
+		return true;
+	}
+
+
+	// Called when the window is updating
+	void ArtNetDemo::update(double deltaTime)
+	{
+		// Use a default input router to forward input events (recursively) to all input components in the default scene
+		nap::DefaultInputRouter input_router(true);
+		mInputService->processWindowEvents(*mRenderWindow, input_router, { &mScene->getRootEntity() });
+
+		// Display general information
+		showGeneralInfo();
+
+		// Display received Art-Net
+		showReceivedArtnet();
+	}
+
+
+	// Called when the window is going to render
+	void ArtNetDemo::render()
+	{
+		// Signal the beginning of a new frame, allowing it to be recorded.
+		// The system might wait until all commands that were previously associated with the new frame have been processed on the GPU.
+		// Multiple frames are in flight at the same time, but if the graphics load is heavy the system might wait here to ensure resources are available.
+		mRenderService->beginFrame();
+
+		// Begin recording the render commands for the main render window
+		if (mRenderService->beginRecording(*mRenderWindow))
+		{
+			// Begin render pass
+			mRenderWindow->beginRendering();
+
+			// Render GUI elements
+			mGuiService->draw();
+
+			// Stop render pass
+			mRenderWindow->endRendering();
+
+			// End recording
+			mRenderService->endRecording();
+		}
+
+		// Proceed to next frame
+		mRenderService->endFrame();
+	}
+
+
+	void ArtNetDemo::windowMessageReceived(WindowEventPtr windowEvent)
+	{
+		mRenderService->addEvent(std::move(windowEvent));
+	}
+
+
+	void ArtNetDemo::inputMessageReceived(InputEventPtr inputEvent)
+	{
+		if (inputEvent->get_type().is_derived_from(RTTI_OF(nap::KeyPressEvent)))
+		{
+			// If we pressed escape, quit the loop
+			nap::KeyPressEvent* press_event = static_cast<nap::KeyPressEvent*>(inputEvent.get());
+			if (press_event->mKey == nap::EKeyCode::KEY_ESCAPE)
+				quit();
+
+			// f is pressed, toggle full-screen
+			if (press_event->mKey == nap::EKeyCode::KEY_f)
+				mRenderWindow->toggleFullscreen();
+		}
+		// Add event, so it can be forwarded on update
+		mInputService->addEvent(std::move(inputEvent));
+	}
+
+
+	int ArtNetDemo::shutdown()
+	{
+		return 0;
+	}
+
+
+	void nap::ArtNetDemo::showGeneralInfo()
+	{
+		ImGui::SetNextWindowPos(ImVec2(32, 32), ImGuiCond_Once);
+		ImGui::SetNextWindowSize(ImVec2(256, 128), ImGuiCond_Once);
+		ImGui::Begin("Art-Net Demo");
+		ImGui::Text(getCurrentDateTime().toString().c_str());
+		ImGui::Text(utility::stringFormat("Framerate: %.02f", getCore().getFramerate()).c_str());
+		ImGui::End();
+	}
+
+
+	void nap::ArtNetDemo::showReceivedArtnet()
+	{
+		auto artnet_handler = mArtNetEntity->findComponent<ArtNetHandlerComponentInstance>();
+		auto received_data = artnet_handler->getData();
+
+		int32_t offset = 0;
+		int32_t total_count = static_cast<int32_t>(received_data.size());
+
+		ImGui::SetNextWindowPos(ImVec2(320, 32), ImGuiCond_Once);
+		ImGui::SetNextWindowSize(ImVec2(512, 512), ImGuiCond_Once);
+		ImGui::Begin("Received Art-Net");
+		ImGui::SliderInt("Channels per Group", &mArtNetInputGroupSize, 1, 512);
+
+		while (offset < total_count)
+		{
+			// The amount of channels to display in this histogram
+			int32_t count = std::min(mArtNetInputGroupSize, total_count - offset);
+
+			// The label for this histogram
+			std::stringstream label;
+			if (count > 1)
+				label << "Channels " << (offset + 1) << " to " << (offset + count);
+			else
+				label << "Channel " << (offset + 1);
+
+			// Plot the historgram
+			ImGui::PlotHistogram(label.str().c_str(), received_data.data() + offset, count, 0, NULL, 0.0f, 255.0f, ImVec2(0, 64));
+
+			// Increment the offset
+			offset += count;
+		}
+
+		ImGui::End();
+	}
+}
