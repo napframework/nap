8a1646bc703164991efe64fc9fd4a771fb2439dd
Thijs van Hoof <thijs.v.h@gmail.com>
2021-09-13 20:50:39 +0200

    Add Art-Net sending demo

---
+++ demos/artnetsend/src/main.cpp
@@ -0,0 +1,36 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+// Local Includes
+#include "artnetsend.h"
+
+// Nap includes
+#include <nap/core.h>
+#include <nap/logger.h>
+#include <apprunner.h>
+#include <guiappeventhandler.h>
+
+/**
+ * Art-Net Sending Demo
+ * refer to artnetsend.h for a more detailed description of the application
+ */
+int main(int argc, char *argv[])
+{
+	// Create core
+	nap::Core core;
+
+	// Create app runner
+	nap::AppRunner<nap::ArtNetSend, nap::GUIAppEventHandler> app_runner(core);
+
+	// Start
+	nap::utility::ErrorState error;
+	if (!app_runner.start(error))
+	{
+		nap::Logger::fatal("error: %s", error.toString().c_str());
+		return -1;
+	}
+
+	// Return if the app ran successfully
+	return app_runner.exitCode();
+}
