c0daddb925950c0e1d2b2318762812363c846fed
cklosters <coen.klosters@gmail.com>
2021-09-15 14:35:33 +0200

    - remove usage of new to create buffer on stack, prevents memory leak - size is known at compile time, use std::array instead - move globals to artnet namespace

--- modules/napartnet/src/artnetlistener.cpp
+++ modules/napartnet/src/artnetlistener.cpp
@@ -23,31 +23,19 @@ namespace nap
 		, mSocket(ioService, ip.empty()
 			? asio::ip::udp::endpoint(asio::ip::udp::v4(), port)
 			: asio::ip::udp::endpoint(asio::ip::make_address(ip), port))
-		, mBufferSize(HEADER_LENGTH + DATA_LENGTH)
-		, mBuffer(new uint8_t[mBufferSize])
 	{
-		// Ensure the buffer starts empty
-		std::memset(mBuffer, 0, mBufferSize);
-
 		// Start receiving UDP packets
 		startReceive();
 	}
 
-
-	ArtNetListener::~ArtNetListener()
-	{
-		// Deallocate our buffer
-		delete mBuffer;
-	}
-
-
 	void ArtNetListener::startReceive()
 	{
 		// Start receiving UDP asynchronously
-		mSocket.async_receive(
-			asio::buffer(mBuffer, mBufferSize),
-			std::bind(&ArtNetListener::handleReceive, this,
-				std::placeholders::_1, std::placeholders::_2));
+		mSocket.async_receive
+		( 
+			asio::buffer(mBuffer.data(), mBuffer.size()),
+			std::bind(&ArtNetListener::handleReceive, this, std::placeholders::_1, std::placeholders::_2)
+		);
 	}
 
 
@@ -56,13 +44,13 @@ namespace nap
 		if (!error)
 		{
 			// Confirm the ID field for the ArtDmx packet
-			std::string protocolId((char*)mBuffer, 7);
-			if (protocolId == PROTOCOL_ID)
+			std::string protocolId((char*)mBuffer.data(), 7);
+			if (protocolId == artnet::protocolID)
 			{
 				// Check the OpCode and ProtVer fields for the ArtDmx packet
 				uint16_t opCode = (mBuffer[9] << 8) | (mBuffer[8] & 0xff);
 				uint16_t protVer = (mBuffer[10] << 8) | (mBuffer[11] & 0xff);
-				if (opCode == OP_CODE && protVer >= MIN_PROT_VER)
+				if (opCode == artnet::opCode && protVer >= artnet::minProtVer)
 				{
 					// Decode the rest of the Art-Net package
 					uint8_t sequence = mBuffer[12];
@@ -74,7 +62,7 @@ namespace nap
 					ArtNetEventPtr event = std::make_unique<ArtNetEvent>(sequence, physical, portAddress);
 
 					// Set the channel data
-					event->setData(mBuffer + HEADER_LENGTH, dataLength);
+					event->setData(mBuffer.data() + artnet::headerLength, dataLength);
 
 					// Add event to receiver
 					mReceiver.addEvent(std::move(event));
@@ -82,7 +70,7 @@ namespace nap
 			}
 
 			// Reset the buffer
-			std::memset(mBuffer, 0, mBufferSize);
+			std::fill(mBuffer.begin(), mBuffer.end(), 0);
 
 			// Receive more data
 			startReceive();
--- modules/napartnet/src/artnetlistener.h
+++ modules/napartnet/src/artnetlistener.h
@@ -6,6 +6,7 @@
 
 // External Includes
 #include <cstdint>
+#include <nap/numeric.h>
 #include <asio/io_service.hpp>
 #include <asio/error_code.hpp>
 #include <asio/ip/udp.hpp>
@@ -13,33 +14,34 @@
 
 namespace nap
 {
+	namespace artnet
+	{
+		inline constexpr const char* protocolID = "Art-Net";	///< Protocol identifier
+		inline constexpr uint16 opCode = 0x5000;				///< Packet type identifier
+		inline constexpr uint16 minProtVer = 14;				///< Min allowed Art-Net protocol version
+		inline constexpr uint16	dataLength = 512;				///< Length of Art-Net buffer in bytes
+		inline constexpr uint16	headerLength = 18;				///< Length of Art-Net header in bytes
+	}
+
 	class ArtNetReceiver;
 
 	/**
-	* Listens for incoming packets and parses them to ArtDmx packet events
-	*/
-	class ArtNetListener
+	 * Listens for incoming packets and parses them to ArtDmx packet events
+	 */
+	class ArtNetListener final
 	{
-
 	public:
-
 		ArtNetListener(ArtNetReceiver& receiver, asio::io_service& ioService, const std::string& ip, uint16_t port);
-		~ArtNetListener();
 
 	private:
-
 		void startReceive();
 		void handleReceive(const asio::error_code& error, std::size_t size);
 
-		static constexpr const char*	PROTOCOL_ID = "Art-Net";
-		static constexpr uint16_t		OP_CODE = 0x5000;
-		static constexpr uint16_t		MIN_PROT_VER = 14;
-		static constexpr uint16_t		DATA_LENGTH = 512;
-		static constexpr uint8_t		HEADER_LENGTH = 18;
+		// Buffer size is known at compile time
+		using ArtNetBuffer = std::array<uint8, artnet::headerLength + artnet::dataLength>;
 
-		ArtNetReceiver&					mReceiver;		// The Art-Net Receiver that holds the message queue
-		asio::ip::udp::socket			mSocket;		// The UDP socket that is used to receive data
-		size_t							mBufferSize;	// The maximum size for the ArtDmx package in bytes
-		uint8_t*						mBuffer;		// The buffer used for receiving ArtDmx packets
+		ArtNetReceiver&	mReceiver;			// The Art-Net Receiver that holds the message queue
+		asio::ip::udp::socket mSocket;		// The UDP socket that is used to receive data
+		ArtNetBuffer mBuffer;				// The buffer used for receiving ArtDmx packets
 	};
 }
