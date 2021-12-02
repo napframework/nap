e8ba313c4d3f606a3a56db376cf868660d5f5145
Thijs van Hoof <thijs.v.h@gmail.com>
2021-12-02 13:06:50 +0100

    Create portal event in portal websocket component and send to portal service

--- modules/napportal/src/portalwebsocketcomponent.cpp
+++ modules/napportal/src/portalwebsocketcomponent.cpp
@@ -3,6 +3,8 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "portalwebsocketcomponent.h"
+#include "portalutils.h"
+#include "portalevent.h"
 
  // External Includes
 #include <entity.h>
@@ -72,6 +74,39 @@ namespace nap
 		rtti::DeserializeResult result;
 		std::vector<APIMessage*> api_messages;
 		if (!extractMessages(event.mMessage.getPayload(), result, factory, api_messages, error))
+		{
+			if (mVerbose)
+				nap::Logger::warn("%s: %s", mID.c_str(), error.toString().c_str());
+			return;
+		}
+
+		// Get portal event header
+		APIMessage* header_message = api_messages.front();
+		APIEventPtr header_event = header_message->toEvent<APIEvent>();
+		PortalEventHeader header;
+		if (!extractPortalEventHeader(header_event, header, error))
+		{
+			if (mVerbose)
+				nap::Logger::warn("%s: %s", mID.c_str(), error.toString().c_str());
+			return;
+		}
+
+		// Create portal event
+		PortalEventPtr portal_event = std::make_unique<PortalEvent>(header, event.mConnection);
+
+		// Add API events that relate to portal items
+		for (APIMessage* message : api_messages)
+		{
+			// Skip portal event header
+			if (message == header_message)
+				continue;
+
+			APIEventPtr api_event = message->toEvent<APIEvent>();
+			portal_event->addAPIEvent(std::move(api_event));
+		}
+
+		// Send portal event to portal service
+		if (!mPortalService->sendEvent(std::move(portal_event), error))
 		{
 			if (mVerbose)
 				nap::Logger::warn("%s: %s", mID.c_str(), error.toString().c_str());
