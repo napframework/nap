f7def3f3ffe551fbe3d88545f78d4d82c3da1941
cklosters <coen.klosters@gmail.com>
2021-12-03 16:09:24 +0100

    fix webscoketpp cmake include directive

--- cmake/Findwebsocketpp.cmake
+++ cmake/Findwebsocketpp.cmake
@@ -3,18 +3,18 @@ find_path(WEBSOCKETPP_ROOT_DIR
     NAMES COPYING
     HINTS ${THIRDPARTY_DIR}/websocketpp
     )
+set(WEBSOCKETPP_DIST_FILES ${WEBSOCKETPP_ROOT_DIR}/COPYING)
 
-# Websocketpp dist directory
-find_path(WEBSOCKETPP_DIR 
+# Websocketpp dist directory, header only
+find_path(WEBSOCKETPP_DIR
           NAMES include/websocketpp/version.hpp
           HINTS ${WEBSOCKETPP_ROOT_DIR}/install
           )
+set(WEBSOCKETPP_INCLUDE_DIR ${WEBSOCKETPP_DIR}/include)
 
 mark_as_advanced(WEBSOCKETPP_DIR)
 mark_as_advanced(WEBSOCKETPP_ROOT_DIR)
-
-set(WEBSOCKETPP_INCLUDE_DIRS ${WEBSOCKETPP_ROOT_DIR}/include)
-set(WEBSOCKETPP_DIST_FILES ${WEBSOCKETPP_ROOT_DIR}/COPYING)
+mark_as_advanced(WEBSOCKETPP_INCLUDE_DIR)
 
 # promote package for find
 include(FindPackageHandleStandardArgs)
