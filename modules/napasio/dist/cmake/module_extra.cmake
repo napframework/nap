5228a10076a37c43f0194b6f7aa80fdfef1cd40b
Tim Groeneboom <tim@timgroeneboom.com>
2021-12-06 16:26:20 +0100

    - added _WIN32_WINNT=0x0501 flags for mod_napudp packaging and WIN32_LEAN_AND_MEAN for mod_napasio packaging

--- modules/napasio/dist/cmake/module_extra.cmake
+++ modules/napasio/dist/cmake/module_extra.cmake
@@ -6,8 +6,8 @@ add_include_to_interface_target(mod_napasio ${ASIO_INCLUDE_DIRS})
 add_define_to_interface_target(mod_napasio ASIO_STANDALONE)
 
 if(WIN32)
-    # Define _WIN32_WINNT for ASIO
-    add_define_to_interface_target(mod_napasio _WIN32_WINNT=0x0501)
+    # Define WIN32_LEAN_AND_MEAN for ASIO
+    add_define_to_interface_target(mod_napasio WIN32_LEAN_AND_MEAN)
 endif()
 
 # Install asio license into packaged project
--- modules/napudp/dist/cmake/module_extra.cmake
+++ modules/napudp/dist/cmake/module_extra.cmake
@@ -9,5 +9,10 @@ endif()
 add_include_to_interface_target(mod_napudp ${ASIO_INCLUDE_DIRS})
 add_define_to_interface_target(mod_napudp ASIO_STANDALONE)
 
+if(WIN32)
+    # Define _WIN32_WINNT for UDP
+    add_define_to_interface_target(mod_napudp _WIN32_WINNT=0x0501)
+endif()
+
 # Install license into packaged project
 install(FILES ${THIRDPARTY_DIR}/asio/LICENSE_1_0.txt DESTINATION licenses/asio)
