4b9cb63b5b15ff2714189635ec393c0b0ba7268f
Thijs van Hoof <thijs.v.h@gmail.com>
2021-12-27 17:45:09 +0100

    Ensure order of API messages is retained when serializing Portal Event to JSON

--- modules/napportal/src/portalevent.cpp
+++ modules/napportal/src/portalevent.cpp
@@ -16,7 +16,8 @@ namespace nap
 	bool PortalEvent::toAPIMessageJSON(std::string& outJSON, utility::ErrorState& error)
 	{
 		// Objects to write to JSON
-		nap::rtti::ObjectList objects;
+		rtti::JSONWriter writer;
+		rtti::ObjectList objects;
 
 		// Add portal event header
 		APIEventPtr header_event = createPortalEventHeader(mHeader);
@@ -25,16 +26,24 @@ namespace nap
 
 		// Add portal item events
 		std::vector<APIMessagePtr> messages;
-		for (APIEvent* event : getAPIEvents())
+		for (const auto& event : getAPIEvents())
 		{
 			APIMessagePtr message = std::make_unique<APIMessage>(*event);
 			objects.emplace_back(message.get());
 			messages.emplace_back(std::move(message));
 		}
 
-		// Write objects to JSON
-		rtti::JSONWriter writer;
-		if (!serializeObjects(objects, writer, error))
+		// Start writing objects to JSON
+		if (!error.check(writer.start(objects), "Failed to start writing to JSON"))
+			return false;
+
+		// Write object one by one to keep order
+		for (const auto& object : objects)
+			if (!serializeObject(*object, writer, error))
+				return false;
+
+		// Finish writing objects to JSON
+		if (!error.check(writer.finish(), "Failed to finish writing to JSON"))
 			return false;
 
 		// Get result
