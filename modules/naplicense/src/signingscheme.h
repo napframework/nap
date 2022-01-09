5b6f9399e53d93b38ec1a4aa9501802583200a18
Jack Williams <jacwil014@gmail.com>
2022-01-08 17:40:24 -0800

    Extend LicenseGenerator and mod_naplicense to support more crypto schemes

---
+++ modules/naplicense/src/signingscheme.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// External Includes
+#include <nap/resource.h>
+
+namespace nap
+{
+	/**
+	 * Signing schemes. Used by the nap::LicenseService to validate a license.  
+	 * Signing scheme used to generate a license with the 'licensegenerator' tool must
+	 * match the signing scheme passed to the nap::LicenseService.
+	 */
+	enum class NAPAPI SigningScheme
+	{
+		RSASS_PKCS1v15_SHA1,
+		RSASS_PKCS1v15_SHA224,
+		RSASS_PKCS1v15_SHA256,
+		RSASS_PKCS1v15_SHA384,
+		RSASS_PKCS1v15_SHA512,
+	};
+}
