0cff3d21c1d0b0051ba543ae81ff33f5e995976b
cklosters <coen.klosters@gmail.com>
2022-01-24 18:22:49 +0100

    porting facedetection demo to new color scheme

--- demos/facedetection/module/src/renderableclassifycomponent.cpp
+++ demos/facedetection/module/src/renderableclassifycomponent.cpp
@@ -41,7 +41,9 @@ namespace nap
 
 
 	RenderableClassifyComponentInstance::RenderableClassifyComponentInstance(EntityInstance& entity, Component& resource) :
-		RenderableComponentInstance(entity, resource), mRenderService(entity.getCore()->getService<nap::RenderService>())
+		RenderableComponentInstance(entity, resource),
+		mRenderService(entity.getCore()->getService<nap::RenderService>()),
+		mGUIService(entity.getCore()->getService<nap::IMGuiService>())
 	{
 
 	}
@@ -130,9 +132,9 @@ namespace nap
 			return false;
 
 		// Add the colors that are randomly picked for every mesh that is drawn
-		mColors.emplace_back(RGBColor8(0x5D, 0x5E, 0x73).convert<RGBColorFloat>());
-		mColors.emplace_back(RGBColor8(0x8B, 0x8C, 0xA0).convert<RGBColorFloat>());
-		mColors.emplace_back(RGBColor8(0xC8, 0x69, 0x69).convert<RGBColorFloat>());
+		mColors.emplace_back(mGUIService->getColors().mFront1Color.convert<RGBColorFloat>());
+		mColors.emplace_back(mGUIService->getColors().mFront2Color.convert<RGBColorFloat>());
+		mColors.emplace_back(mGUIService->getColors().mHighlightColor1.convert<RGBColorFloat>());
 			
 		return true;
 	}
