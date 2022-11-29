fedfb191c3c570917572d691154faf4157ffa3c2
Chris Heywood <31988069+cheywood@users.noreply.github.com>
2022-11-30 08:43:13 +1100

    App dir shortcuts in Source context

---
+++ demos/dynamicgeo/regenerate.bat
@@ -0,0 +1,8 @@
+@echo off
+set PYTHONPATH=
+set PYTHONHOME=
+set python=%~dp0\..\..\thirdparty\python\python.exe
+if not exist %python% (
+    set python=%~dp0\..\..\..\thirdparty\python\msvc\x86_64\python
+)
+%python% %~dp0\..\..\tools\buildsystem\common\regenerate_app_by_dir.py %~dp0 %*
