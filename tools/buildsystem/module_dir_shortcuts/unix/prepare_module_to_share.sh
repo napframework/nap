310b963b6b38eb5eda315609a9add19278bbdd62
Chris Heywood <31988069+cheywood@users.noreply.github.com>
2022-11-30 10:00:08 +1100

    Add initial cut of module sharing preparation tool

---
+++ tools/buildsystem/module_dir_shortcuts/unix/prepare_module_to_share.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+module_dir=$( cd "$(dirname -- "$0")" ; pwd -P )
+nap_root=$module_dir/../..
+. $nap_root/tools/buildsystem/common/sh_shared.sh
+configure_python $nap_root
+$python $nap_root/tools/buildsystem/prepare_module_to_share/prepare_module_to_share.py $module_dir "$@"
