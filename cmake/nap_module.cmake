57806a557b841e3218b41a2c1417e8fcbdb034e3
Chris Heywood <31988069+cheywood@users.noreply.github.com>
2022-11-29 17:36:31 +1100

    Update nap_module.cmake to work in both contexts
    Then use it in modules

--- cmake/nap_module.cmake
+++ cmake/nap_module.cmake
@@ -1,99 +1,61 @@
-# Enforce GCC on Linux for now
-if(UNIX AND NOT APPLE)
-    if(NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-        message(FATAL_ERROR "NAP only currently supports GCC on Linux")
-    endif()
+if(NOT DEFINED NAP_BUILD_CONTEXT)
+    set(NAP_BUILD_CONTEXT "framework_release")
 endif()
 
-# Support building user modules from their own (build system) project
-if(NOT MODULE_INTO_PROJ)
+# Support building user modules from their own (build system) app (in Framework Release context)
+if(NOT MODULE_INTO_PARENT)
+    # TODO don't duplicate this from Source/CMakeLists.txt, sharing is caring
     get_filename_component(NAP_ROOT ${CMAKE_CURRENT_LIST_DIR}/../ REALPATH)
     message(STATUS "Using NAP root: ${NAP_ROOT}")
     get_filename_component(THIRDPARTY_DIR ${NAP_ROOT}/thirdparty REALPATH)
     message(STATUS "Using thirdparty directory: ${THIRDPARTY_DIR}")
 
-    include(${NAP_ROOT}/cmake/targetarch.cmake)
-    target_architecture(ARCH)
+    include(${NAP_ROOT}/cmake/macros_and_functions.cmake)
 
-    include(${NAP_ROOT}/cmake/dist_shared_native.cmake)
-    include(${NAP_ROOT}/cmake/cross_context_macros.cmake)
+    bootstrap_environment()
 
     # Set our default build type if we haven't specified one (Linux)
     set_default_build_type()
-
-    set(CMAKE_CXX_STANDARD 17)
-    set(CMAKE_CXX_STANDARD_REQUIRED ON)
-    set_property(GLOBAL PROPERTY USE_FOLDERS ON)
-    if (WIN32)
-        if(MSVC)
-            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4244 /wd4305 /wd4996 /wd4267 /wd4018 /wd4251 /MP /bigobj")
-            set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /Zi")
-            set(CMAKE_SHARED_LINKER_FLAGS_RELEASE "${CMAKE_SHARED_LINKER_FLAGS_RELEASE} /DEBUG /OPT:REF /OPT:ICF")
-            set(CMAKE_EXE_LINKER_FLAGS_RELEASE "${CMAKE_EXE_LINKER_FLAGS_RELEASE} /DEBUG /OPT:REF /OPT:ICF")
-        else()
-            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -msse -Wa,-mbig-obj")
-        endif()
-    elseif(UNIX)
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -Wno-format-security -Wno-switch -fvisibility=hidden")
-    endif()
-
-    if(APPLE)
-        set(CMAKE_OSX_DEPLOYMENT_TARGET 10.15)
-    endif()
-
-    cmake_policy(SET CMP0020 NEW)
-    cmake_policy(SET CMP0043 NEW)
-
-    # Restrict to debug and release build types
-    set(CMAKE_CONFIGURATION_TYPES "Debug;Release")
-
-    # Allow extra Find{project}.cmake files to be found by projects
-    list(APPEND CMAKE_MODULE_PATH "${NAP_ROOT}/cmake")
-
-    if(WIN32)
-        if(MINGW)
-            # Copy required MinGW dll's to bin dir
-            get_filename_component(COMPILER_DIR ${CMAKE_CXX_COMPILER} PATH)
-            set(MODULES gcc_s_dw2-1 stdc++-6 winpthread-1)
-            foreach (MOD ${MODULES})
-                find_library(_LIB NAMES ${MOD} HINTS ${COMPILER_DIR})
-                message(STATUS "Copy ${_LIB} to ${BIN_DIR}")
-                file(COPY ${_LIB} DESTINATION ${BIN_DIR})
-                unset(_LIB CACHE)
-            endforeach ()
-        endif()
-    endif()
-
-    # Ensure we have patchelf on Linux, preventing silent failures
-    if(UNIX AND NOT APPLE)
-        ensure_patchelf_installed()
-    endif()
-endif(NOT MODULE_INTO_PROJ)
+endif()
 
 # Fetch our module dependencies
-module_json_in_directory_to_cmake(${CMAKE_CURRENT_SOURCE_DIR})
-fetch_module_dependencies("${DEPENDENT_NAP_MODULES}")
+module_json_to_cmake()
 
-include_directories(${NAP_ROOT}/include/)
+if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty)
+    list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/cmake_find_modules)
+endif()
+
+# Bring in any additional module logic (pre-target definition)
+set(MODULE_EXTRA_PRE_TARGET_CMAKE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/module_extra_pre_target.cmake)
+if (EXISTS ${MODULE_EXTRA_PRE_TARGET_CMAKE_PATH})
+    unset(SKIP_MODULE)
+    include(${MODULE_EXTRA_PRE_TARGET_CMAKE_PATH})
+    if(SKIP_MODULE)
+        message(STATUS "Not generating target for ${PROJECT_NAME}")
+        return()
+    endif()
+endif()
 
 # Add source
 file(GLOB_RECURSE SOURCES src/*.cpp)
 file(GLOB_RECURSE HEADERS src/*.h src/*.hpp)
 
-# Create IDE groups
-create_hierarchical_source_groups_for_files("${SOURCES}" ${CMAKE_CURRENT_SOURCE_DIR}/src "Sources")
-create_hierarchical_source_groups_for_files("${HEADERS}" ${CMAKE_CURRENT_SOURCE_DIR}/src "Headers")
-
 # Compile target as shared lib
 add_library(${PROJECT_NAME} SHARED ${SOURCES} ${HEADERS})
-if(IMPORTING_PROJECT_MODULE)
-    set(MODULE_FOLDER_NAME ProjectModule)
+
+# Set IDE folder
+cmake_path(GET CMAKE_CURRENT_SOURCE_DIR PARENT_PATH parent_path)
+cmake_path(GET parent_path STEM LAST_ONLY parent_dir)     
+if(IMPORTING_APP_MODULE)
+    set(module_folder_name AppModules)
     # Ensure not still declared for dependent module search
-    unset(IMPORTING_PROJECT_MODULE)
+    unset(IMPORTING_APP_MODULE)
+elseif(parent_dir MATCHES "^system_modules$")
+    set(module_folder_name SystemModules)
 else()
-    set(MODULE_FOLDER_NAME UserModules)
+    set(module_folder_name Modules)
 endif()
-set_target_properties(${PROJECT_NAME} PROPERTIES FOLDER ${MODULE_FOLDER_NAME})    
+set_target_properties(${PROJECT_NAME} PROPERTIES FOLDER ${module_folder_name})
 
 # Remove lib prefix on Unix libraries
 set_target_properties(${PROJECT_NAME} PROPERTIES PREFIX "")
@@ -105,74 +67,107 @@ target_include_directories(${PROJECT_NAME} PUBLIC src)
 target_compile_definitions(${PROJECT_NAME} PRIVATE NAP_SHARED_LIBRARY)
 target_compile_definitions(${PROJECT_NAME} PRIVATE MODULE_NAME=${PROJECT_NAME})
 
-if (WIN32)
-    set_target_properties(${PROJECT_NAME} PROPERTIES VS_DEBUGGER_WORKING_DIRECTORY "$(OutDir)")
-    if (NOT MODULE_INTO_PROJ AND ${CMAKE_VERSION} VERSION_GREATER "3.6.0")
-        # Set project as startup project in Visual Studio
-        set_property(DIRECTORY ${CMAKE_SOURCE_DIR} PROPERTY VS_STARTUP_PROJECT ${PROJECT_NAME})
-    endif()
-endif()
-
 # Pull in NAP core
-if (NOT MODULE_INTO_PROJ)
+if (NOT MODULE_INTO_PARENT)
     find_package(napcore REQUIRED)
     find_package(naprtti REQUIRED)
     find_package(naputility REQUIRED)
 endif()
 
-target_link_libraries(${PROJECT_NAME} napcore naprtti naputility RTTR::Core ${PYTHON_LIBRARIES} ${SDL2_LIBRARY})
-if (MODULE_INTO_PROJ)
-    target_include_directories(${PROJECT_NAME} PUBLIC ${pybind11_INCLUDE_DIRS})
+find_rttr()
+target_link_libraries(${PROJECT_NAME} napcore naprtti naputility RTTR::Core ${SDL2_LIBRARY})
+if (NAP_ENABLE_PYTHON)
+    target_link_libraries(${PROJECT_NAME} ${PYTHON_LIBRARIES})
+    if (MODULE_INTO_PARENT)
+        target_include_directories(${PROJECT_NAME} PUBLIC ${pybind11_INCLUDE_DIRS})
+    endif()
 endif()
 
-# Bring in any additional module requirements
+# Bring in any additional module logic
 set(MODULE_EXTRA_CMAKE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/module_extra.cmake)
 if (EXISTS ${MODULE_EXTRA_CMAKE_PATH})
-    include (${MODULE_EXTRA_CMAKE_PATH})
+    include(${MODULE_EXTRA_CMAKE_PATH})
 endif()
 
 # Deploy module.json as MODULENAME.json alongside module post-build
 copy_module_json_to_bin()
 
-# Find each NAP module
-foreach(NAP_MODULE ${NAP_MODULES})
-    find_nap_module(${NAP_MODULE})
-endforeach()
-target_link_libraries(${PROJECT_NAME} ${NAP_MODULES})
-unset(NAP_MODULES)
+# Find each NAP module for Framework Release
+if(NAP_BUILD_CONTEXT MATCHES "framework_release")
+    foreach(NAP_MODULE ${DEEP_DEPENDENT_NAP_MODULES})
+        find_nap_module(${NAP_MODULE})
+    endforeach()
+endif()
+target_link_libraries(${PROJECT_NAME} ${DEEP_DEPENDENT_NAP_MODULES})
 
-# Set our module output directory
-set_module_output_directories()
-
-# On Windows copy over module.json post-build
-if(WIN32)
-    add_custom_command(
-        TARGET ${MODULE_NAME}
-        POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_SOURCE_DIR}/module.json $<TARGET_FILE_DIR:${PROJECT_NAME}>/${MODULE_NAME}.json
-        )
+# Add extra RPATHs from module library search paths
+if(APPLE)
+    set(MACOS_EXTRA_RPATH_RELEASE "")
+    foreach(rpath ${DEEP_DEPENDENT_RPATHS})
+        list(APPEND MACOS_EXTRA_RPATH_RELEASE ${rpath})
+    endforeach()
+    set(MACOS_EXTRA_RPATH_DEBUG ${MACOS_EXTRA_RPATH_RELEASE})
+elseif(UNIX)
+    set(LINUX_EXTRA_RPATH "")
+    foreach(rpath ${DEEP_DEPENDENT_RPATHS})
+        list(APPEND LINUX_EXTRA_RPATH ${rpath})
+    endforeach()
 endif()
 
-# Install module data into packaged project
-if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/data)
-    install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/data DESTINATION modules/${MODULE_NAME} CONFIGURATIONS Release)
-endif()
+if(NAP_BUILD_CONTEXT MATCHES "framework_release")
+    include_directories(${NAP_ROOT}/include)
 
-# On macOS & Linux install module into packaged project
-if (NOT WIN32)
-    install(FILES $<TARGET_FILE:${PROJECT_NAME}> DESTINATION lib CONFIGURATIONS Release)
-    install(FILES $<TARGET_FILE_DIR:${PROJECT_NAME}>/${PROJECT_NAME}.json DESTINATION lib CONFIGURATIONS Release)
+    # Create IDE groups
+    create_hierarchical_source_groups_for_files("${SOURCES}" ${CMAKE_CURRENT_SOURCE_DIR}/src "Sources")
+    create_hierarchical_source_groups_for_files("${HEADERS}" ${CMAKE_CURRENT_SOURCE_DIR}/src "Headers")
 
-    # On Linux set our user modules to use their directory for RPATH when installing
-    if(NOT APPLE)
-        install(CODE "message(\"Setting RPATH on ${CMAKE_INSTALL_PREFIX}/lib/${MODULE_NAME}.so\")
-                      execute_process(COMMAND patchelf 
-                                              --set-rpath 
-                                              $ORIGIN/.
-                                              ${CMAKE_INSTALL_PREFIX}/lib/${MODULE_NAME}.so
-                                      RESULT_VARIABLE EXIT_CODE)
-                      if(NOT \${EXIT_CODE} EQUAL 0)
-                          message(FATAL_ERROR \"Failed to set RPATH on ${MODULE_NAME} using patchelf. Is patchelf installed?\")
-                      endif()")
+    if (WIN32)
+        set_target_properties(${PROJECT_NAME} PROPERTIES VS_DEBUGGER_WORKING_DIRECTORY "$(OutDir)")
+        if (NOT MODULE_INTO_PARENT AND ${CMAKE_VERSION} VERSION_GREATER "3.6.0")
+            # Set module as startup project in Visual Studio
+            set_property(DIRECTORY ${CMAKE_SOURCE_DIR} PROPERTY VS_STARTUP_PROJECT ${PROJECT_NAME})
+        endif()
+    endif()
+
+    # Set our module output directory
+    set_module_output_directories()
+
+    # On Windows copy over module.json post-build
+    # TODO does this duplicate logic in find_nap_module?
+    if(WIN32)
+        add_custom_command(
+            TARGET ${MODULE_NAME}
+            POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_SOURCE_DIR}/module.json $<TARGET_FILE_DIR:${PROJECT_NAME}>/${MODULE_NAME}.json
+            )
+    endif()
+
+    # Install module data into packaged app
+    if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/data)
+        install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/data DESTINATION modules/${MODULE_NAME} CONFIGURATIONS Release)
+    endif()
+
+    # On macOS & Linux install module into packaged app
+    if (NOT WIN32)
+        install(FILES $<TARGET_FILE:${PROJECT_NAME}> DESTINATION lib CONFIGURATIONS Release)
+        install(FILES $<TARGET_FILE_DIR:${PROJECT_NAME}>/${PROJECT_NAME}.json DESTINATION lib CONFIGURATIONS Release)
+
+        # On Linux set our user modules to use their directory for RPATH when installing
+        if(NOT APPLE)
+            install(CODE "message(\"Setting RPATH on ${CMAKE_INSTALL_PREFIX}/lib/${MODULE_NAME}.so\")
+                          execute_process(COMMAND patchelf 
+                                                  --set-rpath 
+                                                  $ORIGIN/.
+                                                  ${CMAKE_INSTALL_PREFIX}/lib/${MODULE_NAME}.so
+                                          RESULT_VARIABLE EXIT_CODE)
+                          if(NOT \${EXIT_CODE} EQUAL 0)
+                              message(FATAL_ERROR \"Failed to set RPATH on ${MODULE_NAME} using patchelf. Is patchelf installed?\")
+                          endif()")
+        endif()
+    endif()
+else()
+    # Package into platform release
+    if(parent_dir MATCHES "^system_modules$")
+        package_module_into_framework_release()
     endif()
 endif()
--- tools/buildsystem/common/module_info_to_cmake.py
+++ tools/buildsystem/common/module_info_to_cmake.py
@@ -5,92 +5,171 @@ import json
 import os
 import sys
 
-MODULE_INFO_FILENAME = 'module.json'
-MODULE_INFO_CMAKE_CACHE_FILENAME = 'cached_module_json.cmake'
+from nap_shared import get_platform_name, eprint
 
-def find_module_json_by_module_name(module_name, nap_root):
-    module_path = None
-    # Check for NAP module in framework release
-    if os.path.exists(os.path.join(nap_root, 'modules', module_name)):
-        module_path = os.path.join(nap_root, 'modules', module_name)
-    # Check for user module in framework release
-    elif os.path.exists(os.path.join(nap_root, 'user_modules', module_name)):
-        module_path = os.path.join(nap_root, 'user_modules', module_name)
-    # Check for NAP module in NAP source
-    elif module_name.startswith('mod_nap') and os.path.exists(os.path.join(nap_root, 'modules', module_name[4:])):
-        module_path = os.path.join(nap_root, 'modules', module_name[4:])
+class ModuleInfoParser():
 
-    if module_path is None:
-        print("Couldn't find module %s" % module_name)
-        return None
-    else:
-        return os.path.join(module_path, MODULE_INFO_FILENAME)
+    MODULE_INFO_FILENAME = 'module.json'
+    MODULE_INFO_CMAKE_CACHE_FILENAME = 'cached_module_json.cmake'
 
-def find_new_dependencies_for_modules(new_dependencies, full_nap_modules, nap_root):
-    output_dependencies = []
-    for module_name in new_dependencies:
-        module_json_path = find_module_json_by_module_name(module_name, nap_root)
-        if module_json_path is None:
-            return (False, None)
-        else:
-            dependencies_for_module = read_single_module_dependencies(module_json_path)
-            for found_module_name in dependencies_for_module:
-                if not found_module_name in full_nap_modules and not found_module_name in output_dependencies:
+    def __init__(self, nap_root, path_mapping_file, arch, path_mapping_element):
+        self.__nap_root = nap_root
+        self.__arch = arch
+        self.__path_mapping_element = path_mapping_element
+
+        # Check our path mapping exists
+        if not os.path.exists(path_mapping_file):
+            raise Exception(f"Path mapping missing at {path_mapping_file}")
+        self.__get_path_relationship_from_path_mapping(path_mapping_file)
+
+        self.__full_nap_modules = []
+        self.__rpaths = []
+        self.__new_dependencies = []
+
+    def __find_module(self, module_name):
+        module_path = None
+        # Check for NAP module in framework release
+        if os.path.exists(os.path.join(self.__nap_root, 'system_modules', module_name)):
+            module_path = os.path.join(self.__nap_root, 'system_modules', module_name)
+        # Check for user module
+        elif os.path.exists(os.path.join(self.__nap_root, 'modules', module_name)):
+            module_path = os.path.join(self.__nap_root, 'modules', module_name)
+        # Check for NAP module in NAP source - TODO remove after path consolidation
+        elif module_name.startswith('mod_nap') and os.path.exists(os.path.join(self.__nap_root, 'system_modules', module_name[4:])):
+            module_path = os.path.join(self.__nap_root, 'system_modules', module_name[4:])
+
+        if module_path is None:
+            raise Exception(f"Couldn't find module {module_name}")
+        return module_path
+
+    def __find_new_dependencies_for_modules(self):
+        output_dependencies = []
+        for module_name in self.__new_dependencies:
+            module_path = self.__find_module(module_name)
+            (module_dependencies, module_rpaths) = self.__read_single_module_dependencies(module_path)
+            for found_module_name in module_dependencies:
+                if found_module_name not in self.__full_nap_modules and found_module_name not in output_dependencies:
                     output_dependencies.append(found_module_name)
+            for rpath in module_rpaths:
+                if rpath not in self.__rpaths:
+                    self.__rpaths.append(rpath)
 
-    return (True, output_dependencies)
+        self.__new_dependencies = output_dependencies
 
-def read_single_module_dependencies(module_json_path):
-    # Read in the JSON
-    with open(module_json_path) as json_file:
-        json_dict = json.load(json_file)
-        if not 'RequiredModules' in json_dict:
-            print("Missing element 'RequiredModules' in %s" % module_json_path)
-            return False
+    def __read_single_module_dependencies(self, module_path):
+        # Read in the JSON
+        module_json_path = os.path.join(module_path, self.MODULE_INFO_FILENAME)
+        with open(module_json_path) as json_file:
+            json_dict = json.load(json_file)
+            if not 'RequiredModules' in json_dict:
+                raise Exception(f"Missing element 'RequiredModules' in {module_json_path}")
 
-        if not type(json_dict['RequiredModules']) is list:
-            print("Element 'RequiredModules' in %s is not an array" % MODULE_INFO_FILENAME)
-            return False
+            if not type(json_dict['RequiredModules']) is list:
+                raise Exception(f"Element 'RequiredModules' in {self.MODULE_INFO_FILENAME} is not an array")
 
-        return json_dict['RequiredModules']
+            rpaths = []
+            if 'LibrarySearchPaths' in json_dict:
+                platform = get_platform_name()
+                if not type(json_dict['LibrarySearchPaths']) is dict:
+                    raise Exception(f"Element 'LibrarySearchPaths' in {self.MODULE_INFO_FILENAME} is not a dict")
 
-def update_module_info_to_cmake(module_path, nap_root):
-    # Check our module exists
-    if not os.path.exists(module_path):
-        return False
+                if platform in json_dict['LibrarySearchPaths']:
+                    if not type(json_dict['LibrarySearchPaths'][platform]) is list:
+                        raise Exception(f"Element 'LibrarySearchPaths' > '{platform}' in {self.MODULE_INFO_FILENAME} is not a dict")
+                    rpaths.extend(json_dict['LibrarySearchPaths'][platform])
 
-    output_filename = os.path.join(module_path, MODULE_INFO_CMAKE_CACHE_FILENAME)
+                    # Make RPATH substitutions
+                    for i in range(len(rpaths)):
+                        rpaths[i] = self.__substitute_in_path_mapping(rpaths[i], module_path)
 
-    # If any existing output file exists remove it.  This ensure that CMake will fail if our JSON parsing etc fails.
-    if os.path.exists(output_filename):
-        os.remove(output_filename)
+            return (json_dict['RequiredModules'], rpaths)
 
-    # Read in the JSON
-    nap_modules = read_single_module_dependencies(os.path.join(module_path, MODULE_INFO_FILENAME))
+    def __get_path_relationship_from_path_mapping(self, path_mapping_path):
+        # Read in the JSON
+        with open(path_mapping_path) as json_file:
+            json_dict = json.load(json_file)
+            if not self.__path_mapping_element in json_dict:
+                raise Exception(f"Missing element '{self.__path_mapping_element}' in {path_mapping_path}")
+            self.__exe_to_root_mapping = json_dict[self.__path_mapping_element]
 
-    # Read full, deep, module dependencies
-    full_nap_modules = []
-    new_dependencies = nap_modules
-    while len(new_dependencies) > 0:
-        full_nap_modules.extend(new_dependencies)
-        (success, new_dependencies) = find_new_dependencies_for_modules(new_dependencies, full_nap_modules, nap_root)
-        if not success:
-            return False
+    def __substitute_in_path_mapping(self, path, module_dir):
+        module_relative_to_root = os.path.relpath(module_dir, start=self.__nap_root)
+        path = path.replace('{MODULE_DIR}', f'{self.__exe_to_root_mapping}/{module_relative_to_root}')
+        path = path.replace('{ROOT}', self.__exe_to_root_mapping)
+        path = path.replace('{BUILD_ARCH}', self.__arch)
+        return path
 
-    # Write out
-    with open(output_filename, 'w') as out_file:
-        out_file.write("# Don't edit this file\n")
-        out_file.write("#\n# It was auto generated by cmake from module.json which should be edited instead\n\n")
-        out_file.write("set(DEPENDENT_NAP_MODULES %s)\n" % ' '.join(nap_modules))
-        out_file.write("set(DEEP_DEPENDENT_NAP_MODULES %s)\n" % ' '.join(full_nap_modules))
+    def run_from_module_json(self, module_path):
+        # Check our module exists
+        if not os.path.exists(module_path):
+            raise Exception(f"Module missing at {module_path}")
 
-    return True
+        self.__output_filename = os.path.join(module_path, self.MODULE_INFO_CMAKE_CACHE_FILENAME)
+
+        # Read in the JSON
+        (top_level_modules, self.__rpaths) = self.__read_single_module_dependencies(module_path)
+
+        self.__run(top_level_modules)
+
+    def run_from_module_list(self, module_list, output_dir):
+        self.__output_filename = os.path.join(output_dir, self.MODULE_INFO_CMAKE_CACHE_FILENAME)
+        module_list = module_list.split(";")
+        self.__new_dependencies = module_list
+        self.__run(module_list)
+
+    def __run(self, initial_modules):
+        # If any existing output file exists remove it.  This ensure that CMake will fail if our JSON parsing etc fails.
+        if os.path.exists(self.__output_filename):
+            os.remove(self.__output_filename)
+
+        self.__new_dependencies = initial_modules
+
+        # Read full, deep, module dependencies
+        while len(self.__new_dependencies) > 0:
+            self.__full_nap_modules.extend(self.__new_dependencies)
+            self.__find_new_dependencies_for_modules()
+
+        # Write out
+        with open(self.__output_filename, 'w') as out_file:
+            out_file.write("# Don't edit this file\n")
+            out_file.write("#\n# It was auto generated by CMake from module.json which should be edited instead\n\n")
+            out_file.write("set(DEPENDENT_NAP_MODULES %s)\n" % ' '.join(initial_modules))
+            out_file.write("set(DEEP_DEPENDENT_NAP_MODULES %s)\n" % ' '.join(self.__full_nap_modules))
+            out_file.write("set(DEEP_DEPENDENT_RPATHS %s)\n" % ' '.join(self.__rpaths))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('MODULE_PATH')
     parser.add_argument('NAP_ROOT')
+    parser.add_argument('PATH_MAPPING_FILE')
+    parser.add_argument('BUILD_ARCH')
+    parser.add_argument('-p', '--path-mapping-element', type=str, default='ProjectExeToRoot')
+    # TODO Update Python to 3.7+ and set required=True here
+    subparsers = parser.add_subparsers(dest='COMMAND')
+
+    parse_json_parser = subparsers.add_parser("parse_json")
+    parse_json_parser.add_argument('MODULE_PATH')
+
+    module_list_parser = subparsers.add_parser("process_module_list")
+    module_list_parser.add_argument('MODULES')
+    module_list_parser.add_argument('OUTPUT_DIR')
+
     args = parser.parse_args()
 
-    if not update_module_info_to_cmake(args.MODULE_PATH, args.NAP_ROOT):
+    # Workaround for Python v3.6 (see above)
+    if args.COMMAND is None:
+        eprint("Error: COMMAND is required")
+        sys.exit(1)
+
+    try:
+        parser = ModuleInfoParser(args.NAP_ROOT, 
+                                  args.PATH_MAPPING_FILE, 
+                                  args.BUILD_ARCH,
+                                  args.path_mapping_element
+                                  )
+        if args.COMMAND == 'parse_json':
+            parser.run_from_module_json(args.MODULE_PATH)
+        else:
+            parser.run_from_module_list(args.MODULES, args.OUTPUT_DIR)
+    except Exception as e:
+        eprint(e)
         sys.exit(1)
