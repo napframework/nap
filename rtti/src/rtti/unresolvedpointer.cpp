aa190726eb1486b5cd9fa7581f023971bdf3fd94
cklosters <coen@nap-labs.tech>
2023-02-01 15:08:22 +0100

    - fix path assignment when adding new component ptr or entity ptr to array - declare and apply names for common rtti methods

--- rtti/src/rtti/unresolvedpointer.cpp
+++ rtti/src/rtti/unresolvedpointer.cpp
@@ -16,7 +16,7 @@ namespace nap
 
 			// If the type that we're processing has a function to translate the ID read from json into a different ID, we call it and use that ID.
 			// This is used for pointers that have a different format in json.
-			rttr::method translate_string_method = findMethodRecursive(resolved_path.getType(), "translateTargetID");
+			rttr::method translate_string_method = findMethodRecursive(resolved_path.getType(), method::translateTargetID);
 			if (translate_string_method.is_valid())
 			{
 				rttr::variant translate_result = translate_string_method.invoke(rttr::instance(), mTargetID);
