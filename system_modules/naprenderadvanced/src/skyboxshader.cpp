4397d0d5fe96de210566ee7095375719cdee87ca
cklosters <coen@nap-labs.tech>
2023-05-19 12:49:29 +0200

    fixing skybox error logging

--- system_modules/naprenderadvanced/src/skyboxshader.cpp
+++ system_modules/naprenderadvanced/src/skyboxshader.cpp
@@ -33,22 +33,22 @@ namespace nap
 		const std::string& name = shader::skybox;
 		std::string relative_path = utility::joinPath({ "shaders", utility::appendFileExtension(name, "vert") });
 		const std::string vertex_shader_path = mRenderAdvancedService->getModule().findAsset(relative_path);
-		if (!errorState.check(!vertex_shader_path.empty(), "%s: Unable to find %s vertex shader %s", mRenderAdvancedService->getModule().getName().c_str(), name, vertex_shader_path.c_str()))
+		if (!errorState.check(!vertex_shader_path.empty(), "%s: Unable to find %s vertex shader %s", mRenderAdvancedService->getModule().getName().c_str(), name.c_str(), vertex_shader_path.c_str()))
 			return false;
 
 		relative_path = utility::joinPath({ "shaders", utility::appendFileExtension(name, "frag") });
 		const std::string fragment_shader_path = mRenderAdvancedService->getModule().findAsset(relative_path);
-		if (!errorState.check(!vertex_shader_path.empty(), "%s: Unable to find %s fragment shader %s", mRenderAdvancedService->getModule().getName().c_str(), name, fragment_shader_path.c_str()))
+		if (!errorState.check(!vertex_shader_path.empty(), "%s: Unable to find %s fragment shader %s", mRenderAdvancedService->getModule().getName().c_str(), name.c_str(), fragment_shader_path.c_str()))
 			return false;
 
 		// Read vert shader file
 		std::string vert_source;
-		if (!errorState.check(utility::readFileToString(vertex_shader_path, vert_source, errorState), "Unable to read %s vertex shader file", name))
+		if (!errorState.check(utility::readFileToString(vertex_shader_path, vert_source, errorState), "Unable to read %s vertex shader file", name.c_str()))
 			return false;
 
 		// Read frag shader file
 		std::string frag_source;
-		if (!errorState.check(utility::readFileToString(fragment_shader_path, frag_source, errorState), "Unable to read %s fragment shader file", name))
+		if (!errorState.check(utility::readFileToString(fragment_shader_path, frag_source, errorState), "Unable to read %s fragment shader file", name.c_str()))
 			return false;
 
 		// Copy data search paths
