b8475ee7bd81512880640530bf5bf6c12cb31e57
Coen Klosters <coen@naivi.nl>
2023-08-07 18:13:48 +0200

    look for MoltenVK ICD relative to root instead of executable

--- core/src/nap/osx/core_env.cpp
+++ core/src/nap/osx/core_env.cpp
@@ -8,22 +8,25 @@
 // External Includes
 #include <utility/fileutils.h>
 #include <stdlib.h>
+#include <nap/logger.h>
 
 namespace nap
 {
+    constexpr const char* moltenvk_icd = "MoltenVK_icd.json";
+
     void Core::setupPlatformSpecificEnvironment()
     {
         // Build Vulkan ICD path environment variable. We're currently blatting this blindly, which
         // won't do any harm, even if the project isn't using graphics.
-
-        // These three in order represent the three contexts: Source, Framework Release and 
-        // Packaged App
         std::vector<std::string> paths({
-            "{ROOT}/system_modules/naprender/thirdparty/vulkansdk/macos/x86_64/share/vulkan/icd.d/MoltenVK_icd.json",
-            "{EXE_DIR}/lib/MoltenVK_icd.json"
+            nap::utility::stringFormat("{ROOT}/system_modules/naprender/thirdparty/vulkansdk/macos/x86_64/share/vulkan/icd.d/%s", moltenvk_icd),
+            nap::utility::stringFormat("{ROOT}/lib/%s", moltenvk_icd)
         });
-        mProjectInfo->patchPaths(paths);
         
+        // Path the paths
+        mProjectInfo->patchPaths(paths);
+
+        // Set environment variable
         std::string icd_paths = utility::joinString(paths, ":");
         setenv("VK_ICD_FILENAMES", icd_paths.c_str(), 1);
     }
