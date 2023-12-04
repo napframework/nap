38e86682465c12bd54fd3d1b787e6d553193aef4
lshoek <lesleyvanhoek@hotmail.com>
2023-12-04 15:05:40 +0100

    compile kissfft from source

--- system_modules/napfft/module_extra.cmake
+++ system_modules/napfft/module_extra.cmake
@@ -3,21 +3,16 @@ if(NOT TARGET kissfft)
 endif()
 
 if(NAP_BUILD_CONTEXT MATCHES "source")
-    target_include_directories(${PROJECT_NAME} PUBLIC src ${KISSFFT_INCLUDE_DIR})
-    target_link_libraries(${PROJECT_NAME} ${KISSFFT_RELEASE_LIB})
+    file(GLOB_RECURSE KISS ${KISSFFT_INCLUDE_DIR}/*.c ${KISSFFT_INCLUDE_DIR}/*.h)
+    source_group("kissfft" FILES ${KISS})
+    target_sources(${PROJECT_NAME} PUBLIC ${KISS})
+
+    target_include_directories(${PROJECT_NAME} PUBLIC ${KISSFFT_INCLUDE_DIR})
+    target_compile_definitions(${PROJECT_NAME} PUBLIC KISSFFT_DATATYPE=float KISSFFT_STATIC=OFF)
 
     # additional definitions
     if(WIN32)
         target_compile_definitions(${PROJECT_NAME} PUBLIC WIN32_LEAN_AND_MEAN _WIN32_WINNT=0x0A00)
-
-        add_custom_command(
-            TARGET ${PROJECT_NAME}
-            POST_BUILD
-            COMMAND ${CMAKE_COMMAND}
-                    -E copy_if_different
-                    "$<$<CONFIG:Debug>:${KISSFFT_DEBUG_DLL}>$<$<CONFIG:Release>:${KISSFFT_RELEASE_DLL}>"
-                    $<TARGET_FILE_DIR:${PROJECT_NAME}>
-            COMMENT "Copying ${KISSFFT_DLL_FILENAME} -> bin dir")
     endif()
 
     # Package kissfft into platform release
@@ -25,9 +20,10 @@ if(NAP_BUILD_CONTEXT MATCHES "source")
     install(DIRECTORY ${KISSFFT_INCLUDE_DIR} DESTINATION ${KISSFFT_DIR})
 else()
     add_include_to_interface_target(napfft ${KISSFFT_INCLUDE_DIR})
+    add_define_to_interface_target(napfft KISSFFT_DATATYPE=float)
+    add_define_to_interface_target(napfft KISSFFT_STATIC=OFF)
 
     if(WIN32)
-        # Define _WIN32_WINNT for KISSFFT
         add_define_to_interface_target(napfft WIN32_LEAN_AND_MEAN)
         add_define_to_interface_target(napfft _WIN32_WINNT=0x0A00)
     endif()
