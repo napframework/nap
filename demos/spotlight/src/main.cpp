6259e6aba308222afa4405e75bb4bec882b91a54
cklosters <coen@naivi.nl>
2024-01-29 15:31:26 +0100

    initial spotlight demo

---
+++ demos/spotlight/src/main.cpp
@@ -0,0 +1,32 @@
+// main.cpp : Defines the entry point for the console application.
+//
+// Local Includes
+#include "spotlightapp.h"
+
+// Nap includes
+#include <apprunner.h>
+#include <nap/logger.h>
+#include <guiappeventhandler.h>
+
+// Main loop
+int main(int argc, char *argv[])
+{
+    // Create core
+    nap::Core core;
+
+    // Create the application runner, based on the app to run
+	// and event handler that is used to forward information into the app.
+    nap::AppRunner<nap::CoreApp, nap::GUIAppEventHandler> app_runner(core);
+
+    // Start running
+    nap::utility::ErrorState error;
+    if (!app_runner.start(error))
+    {
+        nap::Logger::fatal("error: %s", error.toString().c_str());
+        return -1;
+    }
+
+    // Return if the app ran successfully
+    return app_runner.exitCode();
+}
+
---
+++ demos/spotlight/src/spotlightapp.cpp
@@ -0,0 +1,120 @@
+// Local Includes
+#include "spotlightapp.h"
+
+// External Includes
+#include <utility/fileutils.h>
+#include <nap/logger.h>
+#include <inputrouter.h>
+#include <rendergnomoncomponent.h>
+#include <perspcameracomponent.h>
+
+namespace nap 
+{    
+    bool CoreApp::init(utility::ErrorState& error)
+    {
+		// Retrieve services
+		mRenderService	= getCore().getService<nap::RenderService>();
+		mSceneService	= getCore().getService<nap::SceneService>();
+		mInputService	= getCore().getService<nap::InputService>();
+		mGuiService		= getCore().getService<nap::IMGuiService>();
+
+		// Fetch the resource manager
+        mResourceManager = getCore().getResourceManager();
+
+		// Get the render window
+		mRenderWindow = mResourceManager->findObject<nap::RenderWindow>("Window");
+		if (!error.check(mRenderWindow != nullptr, "unable to find render window with name: %s", "Window"))
+			return false;
+
+		// Get the scene that contains our entities and components
+		mScene = mResourceManager->findObject<Scene>("Scene");
+		if (!error.check(mScene != nullptr, "unable to find scene with name: %s", "Scene"))
+			return false;
+
+		// Get the camera and origin Gnomon entity
+		mCameraEntity = mScene->findEntity("CameraEntity");
+		mGnomonEntity = mScene->findEntity("GnomonEntity");
+
+		// All done!
+        return true;
+    }
+
+
+    // Render app
+    void CoreApp::render()
+    {
+		// Signal the beginning of a new frame, allowing it to be recorded.
+		// The system might wait until all commands that were previously associated with the new frame have been processed on the GPU.
+		// Multiple frames are in flight at the same time, but if the graphics load is heavy the system might wait here to ensure resources are available.
+		mRenderService->beginFrame();
+
+		// Begin recording the render commands for the main render window
+		if (mRenderService->beginRecording(*mRenderWindow))
+		{
+			// Begin render pass
+			mRenderWindow->beginRendering();
+
+			// Get Perspective camera to render with
+			auto& perp_cam = mCameraEntity->getComponent<PerspCameraComponentInstance>();
+
+			// Add Gnomon
+			std::vector<nap::RenderableComponentInstance*> components_to_render
+			{
+				&mGnomonEntity->getComponent<RenderGnomonComponentInstance>()
+			};
+
+			// Render Gnomon
+			mRenderService->renderObjects(*mRenderWindow, perp_cam, components_to_render);
+
+			// Draw GUI elements
+			mGuiService->draw();
+
+			// Stop render pass
+			mRenderWindow->endRendering();
+
+			// End recording
+			mRenderService->endRecording();
+		}
+
+		// Proceed to next frame
+		mRenderService->endFrame();
+    }
+
+
+    void CoreApp::windowMessageReceived(WindowEventPtr windowEvent)
+    {
+		mRenderService->addEvent(std::move(windowEvent));
+    }
+
+
+    void CoreApp::inputMessageReceived(InputEventPtr inputEvent)
+    {
+		// If we pressed escape, quit the loop
+		if (inputEvent->get_type().is_derived_from(RTTI_OF(nap::KeyPressEvent)))
+		{
+			nap::KeyPressEvent* press_event = static_cast<nap::KeyPressEvent*>(inputEvent.get());
+			if (press_event->mKey == nap::EKeyCode::KEY_ESCAPE)
+				quit();
+
+			if (press_event->mKey == nap::EKeyCode::KEY_f)
+				mRenderWindow->toggleFullscreen();
+		}
+		mInputService->addEvent(std::move(inputEvent));
+    }
+
+
+    int CoreApp::shutdown()
+    {
+		return 0;
+    }
+
+
+	// Update app
+    void CoreApp::update(double deltaTime)
+    {
+		// Use a default input router to forward input events (recursively) to all input components in the scene
+		// This is explicit because we don't know what entity should handle the events from a specific window.
+		nap::DefaultInputRouter input_router(true);
+		mInputService->processWindowEvents(*mRenderWindow, input_router, { &mScene->getRootEntity() });
+    }
+}
