24bee0a019b2035d0e3dee15c7ef2a682206b7bf
Stijn van Beek <stijn.v.beek@gmail.com>
2024-02-05 16:33:11 +0100

    spirv-cross macos universal build

---
+++ system_modules/naprender/thirdparty/SPIRV-cross/macos/universal/share/spirv_cross_core/cmake/spirv_cross_coreConfig-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "spirv-cross-core" for configuration "Release"
+set_property(TARGET spirv-cross-core APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(spirv-cross-core PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libspirv-cross-core.a"
+  )
+
+list(APPEND _cmake_import_check_targets spirv-cross-core )
+list(APPEND _cmake_import_check_files_for_spirv-cross-core "${_IMPORT_PREFIX}/lib/libspirv-cross-core.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
