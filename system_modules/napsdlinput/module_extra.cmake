c2db504c14d6ec4b693d3f649cf507ec8c30b630
Stijn van Beek <stijnvanbeek@users.noreply.github.com>
2024-03-20 22:39:47 +0100

    Update module_extra.cmake

--- system_modules/napsdlinput/module_extra.cmake
+++ system_modules/napsdlinput/module_extra.cmake
@@ -1,4 +1,2 @@
-list(APPEND CMAKE_MODULE_PATH ${NAP_ROOT}/system_modules/naprender/thirdparty/cmake_find_modules)
-
 include(${NAP_ROOT}/cmake/module_utilities.cmake)
 target_link_import_library(${PROJECT_NAME} SDL2)
