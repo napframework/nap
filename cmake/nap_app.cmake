aa6504d8227f76ba95e88595159dc423f7e49fdf
Stijn van Beek <stijn.v.beek@gmail.com>
2024-03-28 12:43:48 +0100

    Config file support

--- cmake/nap_app.cmake
+++ cmake/nap_app.cmake
@@ -81,7 +81,7 @@ endif()
 
 # Set the path mapping variable in patched app.json to copy of path mapping in project root.
 cmake_path(GET path_mapping_path FILENAME path_mapping_filename)
-string(JSON patched_path_mapping_app_json SET ${app_json} "PathMapping" \"${path_mapping_filename}\")
+string(JSON install_app_json SET ${app_json} "PathMapping" \"${path_mapping_filename}\")
 
 # Copy path mapping to app specific install data directory in bin
 set(app_install_data_dir ${BIN_DIR}/app_install_data/${PROJECT_NAME})
@@ -91,35 +91,45 @@ add_custom_command(
         COMMAND ${CMAKE_COMMAND} -E copy_if_different
         ${path_mapping_abs_path} ${app_install_data_dir}/${path_mapping_filename})
 
-# Write app json with patched path mapping to source cache
-file(WRITE ${cache_dir}/patched_path_mapping_app.json ${patched_path_mapping_app_json})
-# Write app json with patched path mapping to bin for installation
+# Check if a config file is specified
+string(JSON config_path GET ${app_json} ServiceConfig)
+if (NOT "${config_path}" STREQUAL "")
+    set(config_abs_path ${CMAKE_CURRENT_SOURCE_DIR}/${config_path})
+    if(EXISTS ${config_abs_path})
+        # Patch the config file path in patched app.json to config file path in root
+        cmake_path(GET config_abs_path FILENAME config_filename)
+        string(JSON install_app_json SET ${install_app_json} "ServiceConfig" \"${config_filename}\")
+        add_custom_command(
+                TARGET ${PROJECT_NAME} POST_BUILD
+                COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                ${config_abs_path} ${app_install_data_dir}/${config_filename})
+        install(FILES ${app_install_data_dir}/${config_filename} TYPE BIN OPTIONAL)
+    endif()
+endif ()
+
+# Write app json patched for installation to cache directory
+file(WRITE ${cache_dir}/install_app_json.json ${install_app_json})
+# Write app json patched for installation to app install directory in bin
 add_custom_command(
         TARGET ${PROJECT_NAME} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_if_different
-        ${cache_dir}/patched_path_mapping_app.json
+        ${cache_dir}/install_app_json.json
         ${app_install_data_dir}/app.json)
 
-# Patch data path in app json
+# Create another patched version of App.json in the cache that is patched to function in the bin directory build
 string(JSON data_file_path GET ${app_json} Data)
 set(absolute_data_file_path ${CMAKE_CURRENT_SOURCE_DIR}/${data_file_path})
-string(JSON patched_data_app_json SET ${app_json} "Data" \"${absolute_data_file_path}\")
-string(JSON patched_data_app_json SET ${patched_data_app_json} "PathMapping" \"app_install_data/${PROJECT_NAME}/${path_mapping_filename}\")
+string(JSON build_app_json SET ${app_json} "Data" \"${absolute_data_file_path}\")
+string(JSON build_app_json SET ${build_app_json} "PathMapping" \"app_install_data/${PROJECT_NAME}/${path_mapping_filename}\")
+string(JSON build_app_json SET ${build_app_json} "ServiceConfig" \"app_install_data/${PROJECT_NAME}/${config_filename}\")
+file(WRITE ${cache_dir}/build_app.json ${build_app_json})
 
-# Write app json with patched data path and path mapping to bin for running apps from source
-file(WRITE ${cache_dir}/patched_data_app.json ${patched_data_app_json})
+# Copy the patched App.json from cache to bin for running apps from source
 add_custom_command(
         TARGET ${PROJECT_NAME} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_if_different
-        ${cache_dir}/patched_data_app.json
+        ${cache_dir}/build_app.json
         ${BIN_DIR}/${PROJECT_NAME}.json)
-
-# Update executable rpath
-if(APPLE)
-    add_custom_command(TARGET ${PROJECT_NAME}
-            POST_BUILD COMMAND
-            ${CMAKE_INSTALL_NAME_TOOL} -add_rpath "@executable_path/${LIB_RPATH}/." $<TARGET_FILE:${PROJECT_NAME}>)
-endif()
 
 # Copy data directory to app specific bin
 set(bin_data_dir ${app_install_data_dir}/data)
@@ -141,6 +151,13 @@ add_custom_command(
         TARGET ${PROJECT_NAME} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_directory
         ${NAP_ROOT}/docs/license ${bin_license_dir}/NAP)
+
+# Update executable rpath
+if(APPLE)
+    add_custom_command(TARGET ${PROJECT_NAME}
+            POST_BUILD COMMAND
+            ${CMAKE_INSTALL_NAME_TOOL} -add_rpath "@executable_path/${LIB_RPATH}/." $<TARGET_FILE:${PROJECT_NAME}>)
+endif()
 
 # Install to packaged app
 install(FILES $<TARGET_FILE:${PROJECT_NAME}> PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE TYPE BIN OPTIONAL)
