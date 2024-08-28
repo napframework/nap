1e0d0a3cdc83f6265288f51689eee084ea3bf490
cklosters <coen@nap-labs.tech>
2024-08-28 19:35:08 +0200

    port NAP to Qt6

--- cmake/qtconfig.cmake
+++ cmake/qtconfig.cmake
@@ -14,14 +14,14 @@ macro(nap_qt_pre)
         elseif(DEFINED NAP_PACKAGED_BUILD)
             # If we're doing a platform release let's enforce the an explicit Qt path so that we're
             # certain what we're bundling with the release
-            message(FATAL_ERROR "Please set the QT_DIR environment variable to define the Qt5 version"
-                                "to be installed with the platform release, eg. \"C:/dev/Qt/5.9.1/msvc2015_64\"")
+            message(FATAL_ERROR "Please set the QT_DIR environment variable to define the Qt6 version"
+                                "to be installed with the platform release, eg. \"C:/dev/Qt/6.5.3/msvc2015_64\"")
         endif()
     endif()
 
     # Add possible Qt installation paths to the HINTS section
     # The version probably doesn't have to match exactly (5.8.? is probably fine)
-    find_path(QT_DIR lib/cmake/Qt5/Qt5Config.cmake
+    find_path(QT_DIR lib/cmake/Qt6/Qt6Config.cmake
               HINTS
               ${QTDIR}
               )
@@ -38,36 +38,36 @@ macro(nap_qt_pre)
 
         # TODO Ensure we're not packaging system Qt on Linux, we only want to use a download from qt.io
 
-        # Find_package for Qt5 will pick up the Qt installation from CMAKE_PREFIX_PATH
+        # Find_package for Qt6 will pick up the Qt installation from CMAKE_PREFIX_PATH
         set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ${QT_DIR})
     else()
         message(FATAL_ERROR
-                "Qt5 could not be found, please set the QT_DIR environment variable, eg.:"
-                "\n Win64 - \"C:/dev/Qt/5.11.3/msvc2015_64\""
-                "\n macOS - \"/Users/username/dev/Qt/Qt5.11.3/5.11.3/clang_64\""
-                "\n Linux - \"/home/username/dev/Qt/Qt5.11.3/5.11.3/gcc_64\"")
+                "Qt6 could not be found, please set the QT_DIR environment variable, eg.:"
+                "\n Win64 - \"C:/dev/Qt/6.5.3/msvc2015_64\""
+                "\n macOS - \"/Users/username/dev/Qt/6.5.3/clang_64\""
+                "\n Linux - \"/home/username/dev/Qt/6.5.3/gcc_64\"")
     endif()
 
     # TODO Update to CMake 3.24+ and use global scope here to avoid redefining
-    find_package(Qt5Core REQUIRED)
-    find_package(Qt5Widgets REQUIRED)
-    find_package(Qt5Gui REQUIRED)
-    find_package(Qt5OpenGL REQUIRED)
+    find_package(Qt6Core REQUIRED)
+    find_package(Qt6Widgets REQUIRED)
+    find_package(Qt6Gui REQUIRED)
+    find_package(Qt6OpenGL REQUIRED)
 
     set(CMAKE_AUTOMOC ON)
     set(CMAKE_AUTORCC ON)
     add_definitions(-DQT_NO_KEYWORDS)
 
-    set(QT_LIBS Qt5::Widgets Qt5::Core Qt5::Gui Qt5::OpenGL)
+    set(QT_LIBS Qt6::Widgets Qt6::Core Qt6::Gui Qt6::OpenGL)
 endmacro()
 
 macro(nap_qt_post PROJECTNAME)
     if(WIN32)
         add_custom_command(TARGET ${PROJECTNAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                           $<TARGET_FILE:Qt5::Widgets>
-                           $<TARGET_FILE:Qt5::Core>
-                           $<TARGET_FILE:Qt5::Gui>
-                           $<TARGET_FILE:Qt5::OpenGL>
+                           $<TARGET_FILE:Qt6::Widgets>
+                           $<TARGET_FILE:Qt6::Core>
+                           $<TARGET_FILE:Qt6::Gui>
+                           $<TARGET_FILE:Qt6::OpenGL>
                            $<TARGET_FILE_DIR:${PROJECTNAME}>
                            COMMENT "Copy Qt DLLs")
     endif()
